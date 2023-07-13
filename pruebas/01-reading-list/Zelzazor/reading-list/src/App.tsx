5b3f0e81334c188883905d1811f3b90d5ea45aac
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2023-07-13 17:40:35 -0400

    add zustand store and logic

--- pruebas/01-reading-list/Zelzazor/reading-list/src/App.tsx
+++ pruebas/01-reading-list/Zelzazor/reading-list/src/App.tsx
@@ -3,7 +3,7 @@ function App () {
   return (
     <>
       <div className="container mx-auto mt-11">
-        <h1 className="text-4xl font-bold">Reading List</h1>
+        <h1 className="text-4xl font-bold mb-4">Reading List</h1>
       </div>
     </>
   )
