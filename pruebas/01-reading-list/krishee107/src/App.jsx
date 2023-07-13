c7a6c0834f64a3f04b73fe89de4163bcf1251287
krishee107 <krishee107@gmail.com>
2023-07-13 21:32:55 +0200

    Mostrar un libro aleatorio en portada

--- pruebas/01-reading-list/krishee107/src/App.jsx
+++ pruebas/01-reading-list/krishee107/src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import books from '../../books.json';
 import { BookGridComponent } from './components/BookGridComponent/BookGridComponent';
+import { RandomBookComponent } from './components/RandomBookComponent/RandomBookComponent';
 
 export const App = () => {
   const [bookList, setBookList] = useState(books['library']);
 
   return (
     <div>
-
+      <RandomBookComponent bookList={bookList} />
       <BookGridComponent bookList={bookList} />
 
 
---
+++ pruebas/01-reading-list/krishee107/src/components/RandomBookComponent/RandomBookComponent.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import { BookItemComponent } from '../BookItemComponent/BookItemComponent';
+import './RandomBookComponent.css';
+
+export const RandomBookComponent = ({ bookList }) => {
+    const [randomBook, setRandomBook] = useState(bookList[0].book);
+
+    useEffect(() => {
+        //Mostrar un libro aleatorio de bookList en randomBook cada 5 segundos
+        const interval = setInterval(() => {
+            const randomIndex = Math.floor(Math.random() * bookList.length);
+            setRandomBook(bookList[randomIndex].book);
+        }
+            , 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+
+    return (
+        <div>
+            <h1>Random Book</h1>
+            <div className="bookInspiration">
+                {randomBook &&
+                    <BookItemComponent book={randomBook} />}
+            </div>
+        </div>
+    )
+}
