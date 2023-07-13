a54444ffe20277ffbb156a6ea4fc3017772f2b4d
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2023-07-13 17:39:46 -0400

    add favicon and add JSON example

--- pruebas/01-reading-list/Zelzazor/reading-list/src/main.tsx
+++ pruebas/01-reading-list/Zelzazor/reading-list/src/main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -6,5 +7,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
