ed7803ab09c90ac8f672df687ae76542976b59d9
Alvaro <alvaromagu2002@gmail.com>
2023-07-13 22:20:18 +0200

    initial commit

---
+++ pruebas/01-reading-list/aIv4ro/.eslintrc.cjs
@@ -0,0 +1,28 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended',
+    './node_modules/ts-standard/eslintrc.json'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
+}
---
+++ pruebas/01-reading-list/aIv4ro/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App () {
+  return (
+    <>
+      HGolla
+    </>
+  )
+}
+
+export default App
