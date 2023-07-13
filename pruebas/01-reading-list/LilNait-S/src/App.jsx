f63b2e55d815fe5f17bca0e549d7e36a9e66f319
LilNait-S <style.damn.falling@gmail.com>
2023-07-13 16:53:08 -0500

    added list of readbooks

--- pruebas/01-reading-list/LilNait-S/src/App.jsx
+++ pruebas/01-reading-list/LilNait-S/src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import books from "./mocks/books.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [listOfBooks, setlistOfBooks] = useState(books.library);
+  const [readList, setReadList] = useState([]);
+
+  const handleAddedBookToReadList = (ISBN) => {
+    const selectBook = listOfBooks.find((item) => item.book.ISBN === ISBN);
+    const NewBook = listOfBooks.filter((item) => item.book.ISBN !== ISBN);
+    setlistOfBooks(NewBook);
+    setReadList([...readList, selectBook]);
+  };
+
+  const handleRemoveFromReadList = (ISBN) => {
+    const selectBook = readList.find((item) => item.book.ISBN === ISBN);
+    const newReadList = readList.filter((item) => item.book.ISBN !== ISBN);
+    setReadList(newReadList);
+    setlistOfBooks([...listOfBooks, selectBook]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container m-auto px-3">
+      <div className="flexCenter flex-col py-20 gap-8 ">
+        <h1 className="text-5xl font-bold">Lista de libros</h1>
+        <div className="flex justify-between w-full">
+          <div className="flex flex-col">
+            <span>libros disponible : 8</span>
+            <span>En la lista de lectura : 2</span>
+          </div>
+          <div className="flex gap-6">
+            <span>filtrar por páginas</span>
+            <span>filtrar por género</span>
+          </div>
+        </div>
+
+        <div className="flex gap-8">
+          <div className="flex flex-col gap-10">
+            <h2 className="text-2xl font-bold">Libros disponibles</h2>
+            <div className="custom-grid">
+              {listOfBooks.map(({ book }) => (
+                <div
+                  onClick={() => handleAddedBookToReadList(book.ISBN)}
+                  key={book.ISBN}
+                  className="flexCenter relative flex-col max-w-xs gap-3 group whitespace-nowrap"
+                >
+                  <span>{book.title}</span>
+                  <img
+                    src={book.cover}
+                    className="w-full h-full object-cover rounded-2xl "
+                  />
+                  <div className="justify-end items-end hidden group-hover:flex w-full h-1/3 bg-gradient-to-b from-transparent to-black/90 rounded-b-2xl gap-2 absolute bottom-0 right-0 font-semibold text-lg text-white p-4">
+                    <p className="w-full">Año : {book.year}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {readList.length !== 0 && (
+            <div className="flex flex-col gap-10">
+              <h2 className="text-2xl font-bold">Lista de lectura</h2>
+              <div className="custom-grid">
+                {readList.map(({ book }) => (
+                  <div
+                    onClick={() => handleRemoveFromReadList(book.ISBN)}
+                    key={book.ISBN}
+                    className="flexCenter relative flex-col max-w-xs gap-3 group whitespace-nowrap"
+                  >
+                    <span>{book.title}</span>
+                    <img
+                      src={book.cover}
+                      className="w-full h-full object-cover rounded-2xl "
+                    />
+                    <div className="justify-end items-end hidden group-hover:flex w-full h-1/3 bg-gradient-to-b from-transparent to-black/90 rounded-b-2xl gap-2 absolute bottom-0 right-0 font-semibold text-lg text-white p-4">
+                      <p className="w-full">Año : {book.year}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
