12c2d8db04a88eaeb21a928542eac58379a15ede
krishee107 <krishee107@gmail.com>
2023-07-13 20:38:17 +0200

    WIP diseño de BookItemComponent

--- pruebas/01-reading-list/krishee107/src/components/BookItemComponent/BookItemComponent.jsx
+++ pruebas/01-reading-list/krishee107/src/components/BookItemComponent/BookItemComponent.jsx
@@ -3,11 +3,12 @@ import './BookItemComponent.css';
 
 export const BookItemComponent = ({ book }, key) => {
     return (
-        <div key={key}>
+        <div key={key} className='bookItem'>
             <img className="bookCover" src={book.cover} alt={book.title} />
-            <h2>{book.title}</h2>
-            <p>{book.pages}</p>
-            <p>{book.genre}</p>
+            <h2 className='bookTitle'>{book.title}</h2>
+            <div className='bookAuthor'>
+                Por <span className='bookAuthorName'>{book.author.name}</span>
+            </div>
         </div>
     )
 }
