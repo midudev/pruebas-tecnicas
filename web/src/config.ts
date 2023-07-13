3eae20d66e287a2c17d8bdbf1d51a07c587655f1
Miguel Ángel Durán <miduga@gmail.com>
2023-07-13 18:04:59 +0200

    Change meta

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,11 +1,8 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Pruebas Técnicas de Programación";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "Practica tus habilidades en programación con pruebas técnicas de empresas reales";
+export const TWITTER_HANDLE = "@midudev";
+export const MY_NAME = "Miguel Ángel Durán";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
