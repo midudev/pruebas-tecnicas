5b860eba125b4170de25647974ce5678d9587479
brus2099 <indiebrus@gmail.com>
2023-07-14 00:04:23 -0600

    first commit by brus2099

---
+++ pruebas/01-reading-list/brus2099/src/App.jsx
@@ -0,0 +1,30 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Prueba tecnica 01</h1>
+      <div className="card">
+        <p>
+          Por <code>brus2099</code>
+        </p>
+        <p>
+          test
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
