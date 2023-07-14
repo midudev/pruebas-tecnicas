77bef5707c95b4d802705959485541b8f78c6f17
Roibert David Peñaloza Valencia <roibert.penalosa@infinitytech.com.co>
2023-07-13 21:38:19 -0500

    @feat added book list and book component

--- pruebas/01-reading-list/rpenlaoza080222/app-list-books-learn/src/routes/+page.svelte
+++ pruebas/01-reading-list/rpenlaoza080222/app-list-books-learn/src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import booksLibrary from "$lib/books.json";
+  import Book from "../components/Book.svelte"; 
+  let { library: books } = booksLibrary;
+  
+</script>
+
+<h1>Lista de libros</h1>
+<div class="container">
+  {#each books as { book }, i}
+    <Book {book} />
+  {/each}
+</div>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+
+  .container {
+    width: 80%;
+    margin: auto;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+    gap: 10px;
+    
+  }
+</style>
