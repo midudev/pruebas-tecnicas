3ef7a5c6a6357cbccee9bd79b5954e9e1d3b0397
Foleas <sebastianfilipetto@gmail.com>
2023-07-14 16:58:45 -0300

    Add books.json & books types

---
+++ pruebas/01-reading-list/foleas/src/types.d.ts
@@ -0,0 +1,20 @@
+export type Author = {
+  name: string;
+  otherBooks: Array[string];
+};
+
+export type Book = {
+  book: {
+    title: string;
+    pages: number;
+    genre: string;
+    cover: string;
+    synopsis: string;
+    ISBN: string;
+    author: Author;
+  };
+};
+
+export type Library = {
+  library: Array<Book>;
+};
