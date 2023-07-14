72be1daec93a8c254ce96cb189ded9d2c999b2b8
aitorjs <aitiba@gmail.com>
2023-07-14 19:42:28 +0200

    add: ts-standard instead of ESLint

--- pruebas/01-reading-list/aitorjs/src/store/booksStore.ts
+++ pruebas/01-reading-list/aitorjs/src/store/booksStore.ts
@@ -1,14 +1,13 @@
-import { create } from "zustand";
-import booksJson from '../books.json';
+import { create } from 'zustand'
+import booksJson from '../books.json'
 import { BooksState } from '../types'
 
 export const useBooksStore = create<BooksState>((set) => ({
-    books: [],
-    getBooks: () => {
-        console.log("json", booksJson.library)
-        const books = booksJson.library
+  books: [],
+  getBooks: () => {
+    console.log('json', booksJson.library)
+    const books = booksJson.library
 
-        set((state) => ({ ...state, books }))
-    }
-}));
-
+    set((state) => ({ ...state, books }))
+  }
+}))
