ec404afc33679f26fe8a906428e2bfa76bcf4065
Martiks38 <cappa.giuliano@gmail.com>
2023-07-14 20:33:02 -0300

    Initial commit

---
+++ pruebas/01-reading-list/martiks38/src/app/layout.tsx
@@ -0,0 +1,15 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Readling list',
+  description: 'Prueba técnica frontend junior'
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='es'>
+      <body>{children}</body>
+    </html>
+  )
+}
