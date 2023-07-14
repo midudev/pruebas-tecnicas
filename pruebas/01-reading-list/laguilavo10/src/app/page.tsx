fd30751f3a5ef96f628865632f0ea80b501dc06a
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-07-14 16:03:39 -0500

    add first mockup

--- pruebas/01-reading-list/laguilavo10/src/app/page.tsx
+++ pruebas/01-reading-list/laguilavo10/src/app/page.tsx
@@ -1,7 +1,47 @@
+'use client'
+import Image from 'next/image'
+import books from '../../../books.json'
+import { useState } from 'react'
 export default function Home() {
+  const { library } = books
+  const [isShowReadingList, setIsShowReadingList] = useState<boolean>(true)
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Hello World</h1>
-    </main>
+    <>
+      <header>
+        <button onClick={() => setIsShowReadingList((prev) => !prev)}>
+          Reading List
+        </button>
+      </header>
+      <main
+        className={` ${
+          isShowReadingList ? 'w-4/6' : 'w-full'
+        } flex transition-all delay-100 duration-300 ease-out overflow-x-hidden gap-5`}
+      >
+        <ul className='grid grid-cols-auto-fit w-full gap-5'>
+          {library.map(({ book }) => (
+            <li key={book.title}>
+              <Image
+                src={book.cover}
+                alt={book.title}
+                width={200}
+                height={300}
+                className='h-80 w-56 m-auto'
+              />
+            </li>
+          ))}
+        </ul>
+        {/* <section className='relative w-1/3'> */}
+        <div
+          className={`w-[32%] h-full fixed bg-red-500 left-full ${
+            isShowReadingList
+              ? '-translate-x-full'
+              : 'translate-x-full invisible'
+          } duration-500 ease-in-out fixed`}
+        >
+          dasjdhas
+        </div>
+        {/* </section> */}
+      </main>
+    </>
   )
 }
