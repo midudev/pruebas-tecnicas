fd8a873c018fdc7b9e8aa9c19b41d9670a04612f
Foleas <sebastianfilipetto@gmail.com>
2023-07-14 16:59:38 -0300

    Feature: get books

--- pruebas/01-reading-list/foleas/src/App.tsx
+++ pruebas/01-reading-list/foleas/src/App.tsx
@@ -1,7 +1,29 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import { Library, Book } from "./types";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [books, setBooks] = useState<Array<Book>>([]);
+
+  const getBooks = async (endpoint: string): Promise<void> => {
+    const response = await fetch(endpoint);
+    const data: Library = (await response.json()) as Library;
+    setBooks(data.library);
+  };
+
+  useEffect(() => {
+    getBooks("./books.json")
+      .then(() => console.log("In then block"))
+      .catch((err) => console.log("ERROR", err));
+  }, []);
+
+  return (
+    <main>
+      <h1 className="text-3xl font-bold">{`${books.length} libros disponibles`}</h1>
+      {books?.map(({ book }) => {
+        return <p>{book.title}</p>;
+      })}
+    </main>
+  );
 }
 
 export default App;
