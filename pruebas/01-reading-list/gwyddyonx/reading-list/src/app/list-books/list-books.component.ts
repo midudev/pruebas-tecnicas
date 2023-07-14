7cc54f531e1491e4ef663bcc07f468df5950bae3
Gwyddyon <jkmlopez23@gmail.com>
2023-07-14 00:32:30 -0500

    getting books

--- pruebas/01-reading-list/gwyddyonx/reading-list/src/app/list-books/list-books.component.ts
+++ pruebas/01-reading-list/gwyddyonx/reading-list/src/app/list-books/list-books.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { BooksService } from '../books.service';
+
+type Author = {
+  name: string,
+  otherBooks: string[]
+}
+
+type Book = {
+  title: string,
+  pages: number,
+  genre: string,
+  cover: string,
+  synopsis: string,
+  year: number,
+  ISBN: string,
+  author: Author
+}
+
+/*
+"book": {
+  "title": "El Señor de los Anillos",
+  "pages": 1200,
+  "genre": "Fantasía",
+  "cover": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1566425108i/33.jpg",
+  "synopsis": "Una aventura épica en un mundo de fantasía llamado la Tierra Media.",
+  "year": 1954,
+  "ISBN": "978-0618640157",
+  "author": {
+      "name": "J.R.R. Tolkien",
+      "otherBooks": [
+          "El Hobbit",
+          "El Silmarillion"
+      ]
+  }
+}*/
 
 @Component({
   selector: 'app-list-books',
   templateUrl: './list-books.component.html',
   styleUrls: ['./list-books.component.css']
 })
+
 export class ListBooksComponent {
 
+  books: Book[] = [];
+
+  constructor(private bookService: BooksService) {
+  }
+
+  ngOnInit(): void {
+    //init array books
+    this.books = this.bookService.getBooks();
+  }
+
 }
+
