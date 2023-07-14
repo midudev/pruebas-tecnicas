ce2156fb16f6f70190649aef9671b3ca9977fa45
genaroibc <bonavitaismael@gmail.com>
2023-07-14 18:28:40 -0300

    [FEAT]: show books list

---
+++ pruebas/01-reading-list/genaroibc/src/components/BooksList.tsx
@@ -0,0 +1,27 @@
+import { component$ } from "@builder.io/qwik"
+import type { Book } from "~/types"
+
+type Props = {
+  books: Book[]
+}
+
+export const BooksList = component$(({ books }: Props) => {
+  return (
+    <section class="grid grid-cols-[repeat(auto-fill,minmax(min(100%,200px),1fr))] gap-6">
+      {books.map(({ cover, title, ISBN }) => (
+        <article
+          key={ISBN}
+          class="aspect-[9:16] flex flex-col justify-center items-center group brightness-95 grayscale-[0.3] hover:grayscale-0 hover:brightness-105 hover:shadow-lg transition-all duration-300 ease-in-out hover:scale-105"
+        >
+          <img
+            src={cover}
+            width={300}
+            height={500}
+            class="w-full h-full rounded-md"
+          />
+          <h6 class="whitespace-nowrap truncate max-w-full py-4">{title}</h6>
+        </article>
+      ))}
+    </section>
+  )
+})
--- pruebas/01-reading-list/genaroibc/src/routes/index.tsx
+++ pruebas/01-reading-list/genaroibc/src/routes/index.tsx
@@ -1,8 +1,18 @@
+import type { Book } from "~/types"
+
 import { component$ } from "@builder.io/qwik"
 import type { DocumentHead } from "@builder.io/qwik-city"
+import { BooksList } from "~/components/BooksList"
+import books from "../../../books.json"
 
 export default component$(() => {
-  return <h1>Hello world</h1>
+  const mappedBooks: Book[] = books.library.map(({ book }) => book)
+
+  return (
+    <>
+      <BooksList books={mappedBooks} />
+    </>
+  )
 })
 
 export const head: DocumentHead = {
