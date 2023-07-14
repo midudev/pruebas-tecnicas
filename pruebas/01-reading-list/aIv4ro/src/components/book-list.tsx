40f24231c7ba8cc2e87fb2809d87fbfc7c4936e0
Alvaro <alvaromagu2002@gmail.com>
2023-07-14 12:45:03 +0200

    fix: add genre books count

--- pruebas/01-reading-list/aIv4ro/src/components/book-list.tsx
+++ pruebas/01-reading-list/aIv4ro/src/components/book-list.tsx
@@ -7,7 +7,7 @@ export function BookList () {
 
   return (
     <div className='p-2'>
-      <h3 className='text-xl font-semibold mb-2'>Library books ({library.remainingBooks} left)</h3>
+      <h3 className='text-xl font-semibold mb-2'>Library books ({library.filteredBooks.length}/{library.remainingBooks})</h3>
       <ul className='flex flex-wrap gap-2'>
         {library.filteredBooks.map(book => (
           <li className='max-w-[10rem]' key={book.ISBN}><Book book={book} /></li>
