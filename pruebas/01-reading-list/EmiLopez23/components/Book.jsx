e94ea1acb77141fd7598a019644980273eb97584
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-07-14 15:25:24 -0300

    fix book width & height

--- pruebas/01-reading-list/EmiLopez23/components/Book.jsx
+++ pruebas/01-reading-list/EmiLopez23/components/Book.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Book({title,cover,onClick}){
-    return <Image src={cover} alt={title} onClick={onClick} height={50} width={50}/>
+    return <Image src={cover} alt={title} onClick={onClick} width={200} height={320}/>
 }
