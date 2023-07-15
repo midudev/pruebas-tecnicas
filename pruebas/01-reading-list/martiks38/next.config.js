54ac2188f21acfab9880e122f21d5df108a03c00
Martiks38 <cappa.giuliano@gmail.com>
2023-07-15 13:51:52 -0300

    chore: add Next configuration for external images

--- pruebas/01-reading-list/martiks38/next.config.js
+++ pruebas/01-reading-list/martiks38/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images-na.ssl-images-amazon.com',
+        port: '',
+        pathname: '/images/S/compressed.photo.goodreads.com/books/**'
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
