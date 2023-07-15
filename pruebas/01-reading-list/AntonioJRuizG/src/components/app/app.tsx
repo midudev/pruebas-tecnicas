e9ce315204547bfd0207fab00c1de69f6b3459e3
AntonioJesusRG <antoniogar129@gmail.com>
2023-07-16 01:06:29 +0200

    Delete unused files and code and start app

---
+++ pruebas/01-reading-list/AntonioJRuizG/src/components/app/app.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+	return (
+		<>
+			<h1>Reading list</h1>
+		</>
+	);
+}
--- pruebas/01-reading-list/AntonioJRuizG/src/main.tsx
+++ pruebas/01-reading-list/AntonioJRuizG/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/app/app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<App></App>
+	</React.StrictMode>
+);
