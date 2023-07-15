8c0c3c242b2ad73bd2f7adfbbacc3197c322bfaf
krishee107 <krishee107@gmail.com>
2023-07-15 16:51:42 +0200

    Efecto de apilado de libros en el readingList

--- pruebas/01-reading-list/krishee107/src/components/ReadingListComponent/ReadingListComponent.jsx
+++ pruebas/01-reading-list/krishee107/src/components/ReadingListComponent/ReadingListComponent.jsx
@@ -1,41 +1,41 @@
-import React from 'react'
+import React from 'react';
 import './ReadingListComponent.css';
 
 export const ReadingListComponent = ({ readingListVisible, handleReadingList, selectedBooks }) => {
-
     return (
         <>
-            {readingListVisible &&
+            {readingListVisible && (
                 <div className="readingListContainer">
                     <div className="readingList">
                         <div className="readingListHeader">
                             <h1>Mi lista de lectura</h1>
-                            <div className="closeButton"
+                            <div
+                                className="closeButton"
                                 onClick={() => {
                                     handleReadingList();
                                 }}
-                            >X</div>
+                            >
+                                X
+                            </div>
                         </div>
                         <div className="readingListContent">
                             <h2>Reading List</h2>
                             <div className="readingListBooks">
-                                {selectedBooks.length > 0 ?
-                                    selectedBooks.map((book, index) => {
-                                        return (
-                                            <div key={index} className="readingListBook">
-                                                <img src={book.book.cover} alt={book.book.title} />
-                                            </div>
-                                        )
-                                    })
-                                    :
-                                    <div className="readingListBook">No hay libros en tu lista de lectura</div>
-                                }
+                                {selectedBooks.map((selectedBook, index) => (
+                                    <img
+                                        key={selectedBook.book.title}
+                                        className={`stackedBook cover${index + 1}`}
+                                        style={{ marginTop: `${index * 10}%` }}
+                                        src={selectedBook.book.cover}
+                                        alt={selectedBook.book.title}
+                                    />
+                                ))}
                             </div>
                         </div>
-
                     </div>
                 </div>
-            }
+            )}
         </>
-    )
-}
+    );
+};
+
