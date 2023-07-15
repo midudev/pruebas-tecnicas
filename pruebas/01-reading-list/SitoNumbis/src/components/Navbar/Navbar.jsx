aacf4cc5520a575b8b1a6ea4717c4a71745a97bd
Sito8943 <sito8943@gmail.com>
2023-07-15 09:00:51 -0700

    reading list button for navbar

--- pruebas/01-reading-list/SitoNumbis/src/components/Navbar/Navbar.jsx
+++ pruebas/01-reading-list/SitoNumbis/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import { faBookmark as faBookmarkR } from "@fortawesome/free-regular-svg-icons";
+
 // contexts
 import { useLanguage } from "../../contexts/LanguageProvider";
+import { useLibrary } from "../../contexts/LibraryProvider";
 
 // styles
 import styles from "./styles.module.css";
@@ -15,6 +20,8 @@ import noPhoto from "../../assets/images/no-photo.webp";
 function Navbar() {
   const { languageState } = useLanguage();
 
+  const { libraryState } = useLibrary();
+
   return (
     <header className={styles.main}>
       <div className={styles.navContainer}>
@@ -26,8 +33,17 @@ function Navbar() {
             {languageState.texts.logo}
           </h1>
         </Link>
-        <div className="flex items-center gap-3">
+        <div className="flex items-center gap-5">
           <SearchWrapper />
+          <Link
+            to="/reading-list"
+            className="text-xl"
+            aria-label={languageState.texts.ariaLabels.toReadingList}
+          >
+            <FontAwesomeIcon
+              icon={libraryState.readingList.length ? faBookmark : faBookmarkR}
+            />
+          </Link>
           <img
             className="w-10 h-10 rounded-full"
             loading="lazy"
