8f265156e0fd755653e486201e5128f07f3f81ae
Yuri <jdsanchez19@esei.uvigo.es>
2023-07-15 17:18:58 +0200

    Added Book FIltering by Genre

--- pruebas/01-reading-list/juanyuri/src/components/BookCatalogue.vue
+++ pruebas/01-reading-list/juanyuri/src/components/BookCatalogue.vue
@@ -1,19 +1,55 @@
 <template>
   <h1>Book Catalogue</h1>
 
-  <ul v-for="book in books" :key="book.title">
-    <li>{{ book.title }}</li>
-  </ul>
+  <select name="genre-selector" id="genreSelector" @change="onSelectedGenre">
+    <option :value="genre" v-for="genre in genres" :key="genre">{{ genre }}</option>
+  </select>
 
+  <button @click="filterCatalogue">Filtrar</button>
+  <button @click="resetFilters">Reset Filters</button>
+
+  <ul v-for="book in books" :key="book.title">
+    <li>{{ book.title }}, {{ book.genre }}</li>
+  </ul>
 </template>
 
 <script setup>
-/* import {ref} from 'vue' */
-import { useBooks } from '@/composables/books';
 
-const { books } = useBooks();
+import { ref } from 'vue'
+import { useBooks } from '@/composables/books.js'
+
+/* Call to composable to get data */
+const { books } = useBooks()
+const booksOriginal = [...books.value]
+
+const genres = [...new Set(books.value.map(book => book.genre))]
+let selectedGenre = ref(genres[0])
+
+const onSelectedGenre = (event) => {
+  selectedGenre.value = event.target.value;
+}
+
+
+/**
+ * Filters the catalogue based on the selected genre and the assigns it to the book reactive reference
+ */
+const filterCatalogue = () => {
+  let tempCatalogue = booksOriginal
+
+  if (selectedGenre.value != '')
+    tempCatalogue = tempCatalogue.filter((book) => book.genre == selectedGenre.value)
+
+  books.value = tempCatalogue
+}
+
+/**
+ * Reset all filters. 
+ */
+const resetFilters = () => {
+  books.value = booksOriginal
+}
+
 </script>
 
 <style scoped>
-
 </style>
