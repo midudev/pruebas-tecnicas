66d05771ed484f57114df3f6efd4709e8f98bb48
Martiks38 <cappa.giuliano@gmail.com>
2023-07-14 23:09:46 -0300

    feat: modify layout of Home

--- pruebas/01-reading-list/martiks38/src/app/layout.tsx
+++ pruebas/01-reading-list/martiks38/src/app/layout.tsx
@@ -1,4 +1,9 @@
+import Image from 'next/image'
+
 import './globals.css'
+import headerStyles from '../assets/styles/HeaderPage.module.css'
+import logo from './icon.svg'
+
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -9,7 +14,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='es'>
-      <body>{children}</body>
+      <body>
+        <header className={headerStyles.headerPage}>
+          <Image
+            src={logo}
+            alt='Reading List'
+            width={36}
+            height={36}
+            className={headerStyles.headerPage__logo}
+          />
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
