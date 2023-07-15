2708befffddba762f1d625ae2f3805b45071327a
Mauro Brandan <maurobrandan.mb@gmail.com>
2023-07-15 15:25:05 -0300

    Setup

---
+++ pruebas/01-reading-list/MauroBrandan/ReadingList/.eslintrc.cjs
@@ -0,0 +1,28 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended',
+    './node_modules/standard/eslintrc.json'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: './tsconfig.json'
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  }
+}
---
+++ pruebas/01-reading-list/MauroBrandan/ReadingList/src/App.tsx
@@ -0,0 +1,9 @@
+function App () {
+  return (
+    <>
+      <h1 className='text-5xl font-bold text-center'>Reading List</h1>
+    </>
+  )
+}
+
+export default App
