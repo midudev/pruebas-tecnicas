e15cac53a0b0b7a2f16261297d07bce5ebae6c68
AntonioJesusRG <antoniogar129@gmail.com>
2023-07-16 01:26:40 +0200

    Update root client rendering

--- pruebas/01-reading-list/AntonioJRuizG/src/main.tsx
+++ pruebas/01-reading-list/AntonioJRuizG/src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './components/app/app';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root')!;
+
+const root = createRoot(container);
+
+root.render(
 	<React.StrictMode>
 		<App></App>
 	</React.StrictMode>
