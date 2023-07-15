a076608c2829883756608459ccfa088712b34483
Sito8943 <sito8943@gmail.com>
2023-07-15 09:00:42 -0700

    cta and more styles

--- pruebas/01-reading-list/SitoNumbis/tailwind.config.js
+++ pruebas/01-reading-list/SitoNumbis/tailwind.config.js
@@ -3,12 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      primary: "crimson",
+      primary: "#dc143c",
+      "primary-dark": "#9a0e2a",
       secondary: "dodgerblue",
       "dark-bg": "#1b1b1b",
       "dark-bg-opacity": "#1b1b1b8a",
       "dark-alt-bg": "#222222",
-      "dark-alt-bg-opacity": "#2222228a",
+      "dark-alt-bg-opacity": "#222222ee",
       "dark-text": "#fbfbfb",
       "dark-alt-text": "#ababab",
     },
