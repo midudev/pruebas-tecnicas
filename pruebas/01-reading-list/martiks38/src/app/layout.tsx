f9166e607bb9167ae24a8b7d18900fb941748cba
Martiks38 <cappa.giuliano@gmail.com>
2023-07-15 13:48:58 -0300

    feat: make home interface and list of genres

--- pruebas/01-reading-list/martiks38/src/app/layout.tsx
+++ pruebas/01-reading-list/martiks38/src/app/layout.tsx
@@ -1,8 +1,4 @@
-import Image from 'next/image'
-
 import './globals.css'
-import headerStyles from '../assets/styles/HeaderPage.module.css'
-import logo from './icon.svg'
 
 import type { Metadata } from 'next'
 
@@ -14,18 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='es'>
-      <body>
-        <header className={headerStyles.headerPage}>
-          <Image
-            src={logo}
-            alt='Reading List'
-            width={36}
-            height={36}
-            className={headerStyles.headerPage__logo}
-          />
-        </header>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
--- pruebas/01-reading-list/martiks38/src/app/page.tsx
+++ pruebas/01-reading-list/martiks38/src/app/page.tsx
@@ -1,3 +1,65 @@
+import Image from 'next/image'
+import { useLibrary } from '@/hooks/useLibrary'
+
+import homeStyles from '@/assets/styles/Home.module.css'
+import headerStyles from '@/assets/styles/HeaderPage.module.css'
+
+import logo from './icon.svg'
+
 export default function Home() {
-  return <main></main>
+  const [books, genres] = useLibrary()
+
+  return (
+    <>
+      <header className={headerStyles.headerPage}>
+        <h1 aria-label='Reading List'>
+          <Image
+            src={logo}
+            alt='Reading List'
+            width={36}
+            height={36}
+            className={headerStyles.headerPage__logo}
+          />
+        </h1>
+      </header>
+      <main className={homeStyles.homeMain}>
+        <section
+          className={homeStyles.homeMain__searchSection}
+          style={{ width: '100%', height: 200 }}
+        >
+          <form>
+            <h2>Buscar libros</h2>
+            <div>
+              <label htmlFor='author'>Autor</label>
+              <input type='text' name='author' id='author' />
+            </div>
+            <div>
+              <label htmlFor='book'>Libro</label>
+              <input type='text' name='book' id='book' />
+            </div>
+          </form>
+          <figure>
+            {/* <Image
+              src='https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1669159060i/63631742.jpg'
+              alt=''
+              width={200}
+              height={360}
+            /> */}
+            <figcaption></figcaption>
+          </figure>
+        </section>
+        <section className={homeStyles.homeMain__bookGenresSection}>
+          <h3 className={homeStyles.booksGenresSection__title}>Género</h3>
+          <ul className={homeStyles.booksGenresSection__list}>
+            {genres.map((genre) => (
+              <li key={genre} className={homeStyles.booksGenresSection__listItem}>
+                {genre}
+              </li>
+            ))}
+          </ul>
+        </section>
+        <article className={homeStyles.homeMain__booksSection}></article>
+      </main>
+    </>
+  )
 }
