6c8ea9adea7957532611ce6be601f20678dfbda7
jmvaliente <valiente_jose2@hotmail.com>
2023-07-16 23:50:38 +0200

    change extension name

---
+++ pruebas/01-reading-list/jmBrave/book-front/src/app/page.jsx
@@ -0,0 +1,9 @@
+import Card from '../components/card/Card'
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Card />
+        </main>
+    )
+}
