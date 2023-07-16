52275ded21125eae64049bbaf339a35b2e55feff
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-16 00:53:31 -0400

    Add component Header and call useFilters

--- pruebas/01-reading-list/patriciotrujilllo/src/App.js
+++ pruebas/01-reading-list/patriciotrujilllo/src/App.js
@@ -1,22 +1,40 @@
 import {useEffect,useState} from 'react'
 import library from './mocks/books.json'
 import { Books } from './component/Books'
+import { Header } from './component/Header'
+import { useFilters } from './hook/useFilters'
 import './styles.css'
+import './header.css'
 
 function App() {
 
   const [books,setBooks] = useState([])
+  
 
   useEffect(()=>{
     setBooks(library.library)
   },[])
+
+  const {filterBooks} = useFilters()
+
+  const filteredbooks = filterBooks(books)
   
   return (
     <>
-    <Books books={books}/>
+    <Header />
+    <Books books={filteredbooks}/>
     </>
+    
+    
     
   )
 }
 
 export default App;
+/*
+Generos:
+Fantasía
+Ciencia ficción
+Zombies
+Terror
+*/
