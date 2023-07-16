1c837b1dc395b53279bb242dae48e501cc0f13d1
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-07-16 03:57:01 -0400

    chore: move app to screens module

--- pruebas/01-reading-list/SergioRibera/src/app.rs
+++
@@ -1,9 +0,0 @@
-use yew::prelude::*;
-
-#[function_component(App)]
-pub fn app() -> Html {
-    html! {
-        <main>
-        </main>
-    }
-}
---
+++ pruebas/01-reading-list/SergioRibera/src/screens/app.rs
@@ -0,0 +1,110 @@
+use yew::prelude::*;
+use yew_hooks::{use_async_with_options, use_local_storage, UseAsyncOptions};
+
+use crate::{
+    components::NavBar,
+    layout::{ErrorType, LayoutError, Library as LibraryComponent},
+    models::Book,
+    services::get_data,
+};
+
+#[function_component(App)]
+pub fn app() -> Html {
+    let load_data = use_async_with_options(
+        async move { get_data().await },
+        UseAsyncOptions::enable_auto(),
+    );
+
+    let search = use_state(String::new);
+    let data = use_state(Vec::<Book>::new);
+    let reading_list = use_local_storage::<Vec<Book>>("saved_books".to_string());
+    let filtered_data = use_state(Vec::<Book>::new);
+
+    let onsearch = {
+        let search = search.clone();
+        Callback::from(move |txt: String| {
+            search.set(txt);
+        })
+    };
+
+    let _onaddbook = {
+        let reading_list = reading_list.clone();
+        Callback::from(move |b: &Book| {
+            if let Some(r) = reading_list.as_ref().and_then(|r| {
+                let mut r = r.clone();
+                if !r.contains(b) {
+                    r.push(b.clone());
+                }
+                Some(r)
+            }) {
+                reading_list.set(r);
+            }
+        })
+    };
+
+    use_effect_with_deps(
+        move |(search, data, filtered_data)| {
+            if !search.is_empty() {
+                let search = search.to_lowercase();
+                filtered_data.set(
+                    data.iter()
+                        .filter(|d| {
+                            d.title.to_lowercase().contains(&search)
+                                || d.genre.to_lowercase().contains(&search)
+                                || d.author.name.to_lowercase().contains(&search)
+                                || d.synopsis.to_lowercase().contains(&search)
+                                || d.year.to_string().contains(&search)
+                                || d.pages.to_string().contains(&search)
+                        })
+                        .cloned()
+                        .collect::<Vec<Book>>(),
+                );
+            } else {
+                filtered_data.set(Vec::<Book>::new());
+            }
+        },
+        (search, data.clone(), filtered_data.clone()),
+    );
+
+    {
+        let data = data.clone();
+        let load_data = load_data.clone();
+        use_effect_with_deps(
+            move |d| {
+                if let Some(d) = &d.data {
+                    data.set(d.library.iter().map(|l| l.book.clone()).collect());
+                }
+            },
+            load_data,
+        );
+    }
+
+    html! {
+        <>
+            <NavBar {onsearch} />
+            <main>
+                // If searching
+                if !filtered_data.is_empty() {
+                    <LibraryComponent books={(*filtered_data).clone()} />
+                } else {
+                    <h1 class={classes!("text-gray-700","font-bold","text-3xl","mt-3","mb-2")}>{"Tus Lista de Lectura"}</h1>
+                    // if not have saved books
+                    if reading_list.is_none() || reading_list.as_ref().is_some_and(|r| r.is_empty()) {
+                        <LayoutError errtype={ErrorType::EmptyReadingList} />
+                    // Show saved books
+                    } else {
+                        <LibraryComponent books={((*reading_list).clone()).unwrap_or_default()} />
+                    }
+                    <h1 class={classes!("text-gray-700","font-bold","text-3xl","mt-3","mb-2")}>{"Nuestros Libros"}</h1>
+                    if load_data.loading {
+                        <LayoutError errtype={ErrorType::LoadingBooks} />
+                    } else if load_data.error.is_some() || data.is_empty() {
+                        <LayoutError errtype={ErrorType::Empty} />
+                    } else {
+                        <LibraryComponent books={(*data).clone()} />
+                    }
+                }
+            </main>
+        </>
+    }
+}
