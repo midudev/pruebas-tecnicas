9cd5d200b4a39d94537d0e787115a966a2c42c39
AntonioJesusRG <antoniogar129@gmail.com>
2023-07-16 02:45:01 +0200

    Style list & improve performace

--- pruebas/01-reading-list/AntonioJRuizG/src/components/list/list.tsx
+++ pruebas/01-reading-list/AntonioJRuizG/src/components/list/list.tsx
@@ -1,4 +1,5 @@
 import useBooks from '../hooks/useBooks';
+import styles from './list.module.scss';
 
 export default function List() {
 	const { books } = useBooks();
@@ -6,10 +7,12 @@ export default function List() {
 	return (
 		<>
 			<h2>Avaible Books</h2>
-			<ul>
+			<ul className={styles.listContainer}>
 				{books.map((book) => (
-					<li key={book.ISBN}>
+					<li key={book.ISBN} className={styles.bookCover}>
 						<img
+							width={240}
+							height={380}
 							src={book.cover}
 							alt={'Cover of the book ' + book.title + ' by ' + book.author}
 						></img>
