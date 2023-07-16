69e8fc3ec3b9d57652a46e3035de77e60aaab2bc
Maria Soledad Lanfranconi <pennymsl@gmail.com>
2023-07-16 17:44:25 -0300

    initial configuration

---
+++ pruebas/01-reading-list/saerwenlis/src/app/components/Header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="">//</div>
+      </main>
+    )
+  }
+  
---
+++ pruebas/01-reading-list/saerwenlis/src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="">Explore Books</h1>
+    </main>
+  )
+}
