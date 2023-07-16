9b44e064846c6deced5dbaceec7ad7cf017509bf
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-16 00:52:06 -0400

    Fix use correct provider

--- pruebas/01-reading-list/patriciotrujilllo/src/index.js
+++ pruebas/01-reading-list/patriciotrujilllo/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { FilterProvider } from './context/filtersContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <FilterProvider>
     <App />
-  </React.StrictMode>
+  </FilterProvider>
 );
 
