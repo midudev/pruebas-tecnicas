8a1a9c96b2fc56807720ce667665fbe1e75a77e8
LuisMFG <luismiguelfloresgr@gmail.com>
2023-07-16 18:29:55 +0200

    Base styles configured

--- pruebas/01-reading-list/LuisMFG1992/src/App.jsx
+++ pruebas/01-reading-list/LuisMFG1992/src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 
 function App() {
-  return <div className="text-red-700">Hello World!</div>
+  return (
+    <div className="w-full bg-gray-001 flex justify-center items-center min-h-screen">
+      <div className="">Hello World!</div>
+    </div>
+  )
 }
 
 export default App
