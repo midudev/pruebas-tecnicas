76945686afb6cc138993c34de683b1cb7a8eece4
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-16 17:00:51 -0400

    Add or Remove: dependiendo si el libro esta en el contexto

--- pruebas/01-reading-list/patriciotrujilllo/src/component/Books.js
+++ pruebas/01-reading-list/patriciotrujilllo/src/component/Books.js
@@ -1,4 +1,10 @@
-export const Books = ({books}) =>{
+import { useListBook } from "../hook/useListBook"
+
+export const Books = ({books}) =>{ 
+    const {read,addtoToListRead, removeToListRead} = useListBook()
+
+    const isInList =(book)=>(read.some(item=>item.ISBN===book.ISBN)) 
+    
     return (
 
     <div className='container'>
@@ -8,7 +14,11 @@ export const Books = ({books}) =>{
             <div key={book.ISBN} className='book'>
 
                 <figure className='img-container'>
-                <img src={book.cover} alt={book.title} />
+                <img src={book.cover} alt={book.title} onClick={()=>(
+
+                        isInList(book) ? removeToListRead(book): addtoToListRead(book)
+
+                )}/>
                 </figure>
 
             </div>
