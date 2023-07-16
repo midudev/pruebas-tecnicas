a984816f5ab1d0a041c68511a70ebdcaa6072d33
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-07-16 01:00:27 -0400

    feat: add basic nav bar

---
+++ pruebas/01-reading-list/SergioRibera/src/components/nav.rs
@@ -0,0 +1,23 @@
+use crate::components::InputText;
+use yew::prelude::*;
+use yew_icons::IconId;
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub onsearch: Callback<InputEvent>,
+}
+
+#[function_component(NavBar)]
+pub fn navbar(props: &Props) -> Html {
+    let Props { onsearch } = props;
+    html! {
+        <nav class={classes!("w-full", "py-4", "flex", "flex-row", "items-center", "justify-between")}>
+            <InputText
+                onchange={onsearch}
+                placeholder={"Buscar por Nombre, author, genero, sinopsis"}
+                icon={IconId::BootstrapSearch}
+            />
+            <span class={classes!("text-black")}>{"Search book name, author, edition"}</span>
+        </nav>
+    }
+}
