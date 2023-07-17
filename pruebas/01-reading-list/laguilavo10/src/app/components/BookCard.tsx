dd661d5b1899fdf980d72ce91f5a42591672f803
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-07-17 14:16:36 -0500

    add more info of book on hover

--- pruebas/01-reading-list/laguilavo10/src/app/components/BookCard.tsx
+++ pruebas/01-reading-list/laguilavo10/src/app/components/BookCard.tsx
@@ -1,10 +1,7 @@
 import Image from 'next/image'
 import type { Book } from '../types'
-import {
-  PlusCircleIcon,
-  InformationCircleIcon
-} from '@heroicons/react/24/outline'
-import Link from 'next/link'
+import { PlusCircleIcon } from '@heroicons/react/24/outline'
+import { BookOpenIcon } from '@heroicons/react/24/solid'
 interface Props {
   book: Book
   setReadList?: React.Dispatch<React.SetStateAction<Book[]>>
@@ -20,19 +17,31 @@ export default function BookCard({ book, setReadList }: Props) {
   }
 
   return (
-    <li className='relative m-auto h-80 w-56 overflow-hidden text-white [&:hover>article]:opacity-80 [&:hover>img]:scale-125'>
-      <article className='ease-linear absolute left-0 top-0 z-20 flex h-full w-full flex-col justify-evenly bg-black opacity-0 duration-150'>
+    <li className='relative m-auto mt-2 h-80 w-56 overflow-hidden rounded-lg text-white outline outline-white [&:hover>article]:opacity-90 [&:hover>img]:scale-125'>
+      <article className='absolute left-0 top-0 z-20 flex h-full w-full flex-col justify-evenly bg-black p-3 text-sm opacity-0 duration-300 ease-linear'>
+        <h3 className='mt-2 text-lg font-extrabold'>{book.title}</h3>
+        <p className='flex items-center gap-2'>
+          <BookOpenIcon className='z-50 h-5 fill-[#596886]'>hola</BookOpenIcon>
+          {book.pages} pages
+        </p>
+        <p>{book.synopsis}</p>
+        <p>
+          {' '}
+          <span className='font-semibold  tracking-wide'>Genero:</span>{' '}
+          {book.genre}
+        </p>
+        <p>
+          {' '}
+          <span className='font-semibold  tracking-wide'>Author:</span>{' '}
+          {book.author?.name}
+        </p>
+
         <button
-          className='flex cursor-pointer flex-col items-center justify-around text-sm '
+          className='absolute right-3 top-3 flex h-8 cursor-pointer flex-col items-center justify-around text-sm'
           onClick={addToReadingList}
         >
-          <PlusCircleIcon className='h-10' />
-          <p>ADD TO READING LIST</p>
+          <PlusCircleIcon className='h-full' title='Add To Reading List' />
         </button>
-        <Link href={`/book/${book.title}`} className='flex cursor-pointer flex-col items-center justify-center text-sm'>
-          <InformationCircleIcon className='h-10' />
-          <p>GET MORE INFO</p>
-        </Link>
       </article>
 
       <Image
@@ -40,7 +49,7 @@ export default function BookCard({ book, setReadList }: Props) {
         alt={book.title}
         width={200}
         height={300}
-        className='z-10 h-full w-full duration-500 ease-in-out'
+        className='z-10 h-full w-full duration-300 ease-in-out'
       />
     </li>
   )
