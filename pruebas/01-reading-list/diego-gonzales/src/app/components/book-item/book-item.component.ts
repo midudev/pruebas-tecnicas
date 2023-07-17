3f7d5a602b46d0f42ffa9655e6478d322bf43736
Diego Gonzales <diegoegj14@gmail.com>
2023-07-17 16:30:29 -0500

    new improvements in book-item

--- pruebas/01-reading-list/diego-gonzales/src/app/components/book-item/book-item.component.ts
+++ pruebas/01-reading-list/diego-gonzales/src/app/components/book-item/book-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import type { Book } from '~/interfaces/books.interface';
 
@@ -7,7 +7,14 @@ import type { Book } from '~/interfaces/books.interface';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './book-item.component.html',
+  styleUrls: ['./book-item.component.css'],
 })
 export class BookItemComponent {
   @Input({ required: true }) book!: Book;
+  isFavorite = signal(false);
+
+  toggleFavorite() {
+    this.isFavorite.update((value) => !value);
+    console.log(this.isFavorite());
+  }
 }
--- pruebas/01-reading-list/diego-gonzales/src/app/pages/books-home/books-home.component.ts
+++ pruebas/01-reading-list/diego-gonzales/src/app/pages/books-home/books-home.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import BookListComponent from '~/components/book-list/book-list.component';
 import { BookFiltersComponent } from '~/components/book-filters/book-filters.component';
 import { ReadingListComponent } from '~/components/reading-list/reading-list.component';
+import { BooksService } from '~/services/books.service';
 
 @Component({
   selector: 'app-books-home',
@@ -15,4 +16,7 @@ import { ReadingListComponent } from '~/components/reading-list/reading-list.com
   ],
   templateUrl: './books-home.component.html',
 })
-export default class BooksHomeComponent {}
+export default class BooksHomeComponent {
+  private _booksService = inject(BooksService);
+  readingList = this._booksService.readingList;
+}
