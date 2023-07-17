6784873acaf130c55270882490ed59744601dba8
Foleas <sebastianfilipetto@gmail.com>
2023-07-17 13:46:10 -0300

    Genre Filter name change

--- pruebas/01-reading-list/foleas/src/components/GenreFilter/index.test.tsx
+++ pruebas/01-reading-list/foleas/src/components/GenreFilter/index.test.tsx
@@ -3,9 +3,11 @@ import { render, screen } from "@testing-library/react";
 import PageFilter from ".";
 
 describe("Genre Filter", () => {
-  test("Should render genre filter input", () => {
+  test("Should render genre filter select", () => {
     render(<PageFilter />);
 
     screen.getByLabelText("Filtrar por Género");
+
+    console.log("1112", screen.getByLabelText("Filtrar por Género"));
   });
 });
--- pruebas/01-reading-list/foleas/src/components/GenreFilter/index.tsx
+++ pruebas/01-reading-list/foleas/src/components/GenreFilter/index.tsx
@@ -11,13 +11,13 @@ const GenderFilter = () => {
   return (
     <div>
       <label
-        htmlFor="gender"
+        htmlFor="genre"
         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
       >
         Filtrar por Género
       </label>
       <select
-        id="gender"
+        id="genre"
         value={currentGenre}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         onChange={({ target }) => selectChangeHandler(target.value)}
