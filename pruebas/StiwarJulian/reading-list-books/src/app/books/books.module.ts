de337a0fceb07576387bb1f94c5db22c8e7ac072
Stiwar Julian <stiwarospina6@gmail.com>
2023-07-17 16:29:21 -0500

    add initial structure

---
+++ pruebas/StiwarJulian/reading-list-books/src/app/books/books.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class BooksModule { }
