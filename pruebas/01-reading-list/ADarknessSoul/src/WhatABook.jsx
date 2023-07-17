1cb4c467179368f24be6a597020d039dc209f808
Gerardo <a21110179@ceti.mx>
2023-07-16 18:54:23 -0600

    Add fonts, colors and styles for the project. Also add the name of the app

---
+++ pruebas/01-reading-list/ADarknessSoul/src/WhatABook.jsx
@@ -0,0 +1,10 @@
+
+export const WhatABook = () => {
+  return (
+    <>
+    
+        
+    
+    </>
+  )
+}
--- pruebas/01-reading-list/ADarknessSoul/src/main.jsx
+++ pruebas/01-reading-list/ADarknessSoul/src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import { WhatABook } from './WhatABook';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-  </React.StrictMode>,
+    
+    <WhatABook/>
+
+  </React.StrictMode>
 )
