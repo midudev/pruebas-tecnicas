7a00ea03ed93ab674e9683c43cae716f2a97c736
AntonioJesusRG <antoniogar129@gmail.com>
2023-07-17 19:42:12 +0200

    Implement load and remove actions within cstm hook

--- pruebas/01-reading-list/AntonioJRuizG/src/components/hooks/useBooks.tsx
+++
@@ -1,7 +0,0 @@
-import { BookProps } from '../model/book';
-import { BooksRepo } from '../services/books.repo';
-
-export default function useBooks(repo: BooksRepo) {
-	const books: BookProps[] = repo.getBooks();
-	return { books };
-}
--- pruebas/01-reading-list/AntonioJRuizG/src/components/list/list.tsx
+++ pruebas/01-reading-list/AntonioJRuizG/src/components/list/list.tsx
@@ -2,10 +2,15 @@ import { useMemo } from 'react';
 import useBooks from '../hooks/useBooks';
 import { BooksRepo } from '../services/books.repo';
 import styles from './list.module.scss';
+import { BookId } from '../../store/books/slice';
 
 export default function List() {
 	const repo = useMemo(() => new BooksRepo(), []);
-	const { books } = useBooks(repo);
+	const { books, deleteBook } = useBooks(repo);
+
+	const handleRemoveBook = (id: BookId) => {
+		deleteBook(id);
+	};
 
 	return (
 		<>
@@ -13,6 +18,7 @@ export default function List() {
 			<ul className={styles.listContainer}>
 				{books.map((book) => (
 					<li key={book.isbn} className={styles.bookCover}>
+						<button onClick={() => handleRemoveBook(book.isbn)}>X</button>
 						<img
 							width={240}
 							height={380}
