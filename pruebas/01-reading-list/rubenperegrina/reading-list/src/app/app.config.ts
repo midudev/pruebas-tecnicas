fbbf88358e209cdb3d34f9f679dc7e4bb74a8fdc
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-07-17 12:15:24 +0200

    Add material

--- pruebas/01-reading-list/rubenperegrina/reading-list/src/app/app.config.ts
+++ pruebas/01-reading-list/rubenperegrina/reading-list/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [provideRouter(routes), provideAnimations()]
 };
