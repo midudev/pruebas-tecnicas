00ab63179ad315e7f224a644372587cdf566d8ff
Foleas <sebastianfilipetto@gmail.com>
2023-07-17 13:44:19 -0300

    Vitest install & config

--- pruebas/01-reading-list/foleas/vite.config.ts
+++ pruebas/01-reading-list/foleas/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+});
