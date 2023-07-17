73e962482b6145cc359d9e8f2f4003f9bdbe7440
TobysNap <naptobys17@gmail.com>
2023-07-17 19:25:22 -0300

    Book Service

--- pruebas/01-reading-list/TobysNap/src/app/shared/services/book.service.ts
+++ pruebas/01-reading-list/TobysNap/src/app/shared/services/book.service.ts
@@ -1,18 +1,49 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, signal, computed, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs/operators';
+
 import { Book } from '../interfaces/Book';
+import { Library } from '../interfaces/Library';
+
+export interface BooksState {
+  books: Book[];
+  loaded: boolean;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
-  booksUrl = '../../../../../books.json'; 
-  books: Book[] = [];
+  booksUrl = 'assets/books.json';
+  http = inject(HttpClient);
 
-  constructor(private http: HttpClient) { }
+  // Estado
+  private state = signal<BooksState>({
+    books: [],
+    loaded: false,
+  });
 
-  getBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>(this.booksUrl);
+  // Selectores
+  books = computed(() => this.state().books);
+  loaded = computed(() => this.state().loaded);
+
+  // Fuentes
+  private booksLoaded$ = this.loadBooks();
+
+  constructor() {
+    this.booksLoaded$.pipe(takeUntilDestroyed()).subscribe((books) => {
+      this.state.update((state) => ({
+        ...state,
+        books,
+        loaded: true,
+      }));
+    });
+  }
+
+  loadBooks() {
+    return this.http
+      .get<Library>(this.booksUrl)
+      .pipe(map((data) => data.library.map((library) => library.book as Book)));
   }
 }
