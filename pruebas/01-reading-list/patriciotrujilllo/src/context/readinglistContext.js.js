9007ce3fd06afb90a4587054ec329dde2c30974c
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-17 14:08:54 -0400

    Add synchronization by localStorage

--- pruebas/01-reading-list/patriciotrujilllo/src/context/readinglistContext.js.js
+++ pruebas/01-reading-list/patriciotrujilllo/src/context/readinglistContext.js.js
@@ -1,35 +1,49 @@
-import { createContext,useState } from "react"
+import { createContext, useState, useEffect } from "react"
 
 export const ReadingContext = createContext()
 
 
 const initialState = JSON.parse(window.localStorage.getItem('bookToRead')) || []
 
-const updateLocatStorage = newBooksToRead =>{
-    window.localStorage.setItem('bookToRead',JSON.stringify(newBooksToRead))
+const updateLocatStorage = newBooksToRead => {
+    window.localStorage.setItem('bookToRead', JSON.stringify(newBooksToRead))
 }
 
-export const ReadingProvider = ({children}) =>{
-    const [read,setRead] = useState(initialState)
+export const ReadingProvider = ({ children }) => {
+    const [read, setRead] = useState(initialState)
 
-    const addtoToListRead = book =>{
-        
-        const indexFind = read.findIndex(item=> item.ISBN===book.ISBN)
+    useEffect(() => {
+        const handleStorageChange = (event) => {
+            if (event.key === "bookToRead") {
+                setRead(JSON.parse(event.newValue));
+            }
+        };
 
-        if(indexFind===-1){
-            const newList = [...read,book]
+        window.addEventListener("storage", handleStorageChange);
+        //Se comenta por ahora, si da problemas de rendimineto o problema visual por referencia
+        // return () => {
+        //     window.removeEventListener("storage", handleStorageChange);
+        // }
+    }, []);
+
+    const addtoToListRead = book => {
+
+        const indexFind = read.findIndex(item => item.ISBN === book.ISBN)
+
+        if (indexFind === -1) {
+            const newList = [...read, book]
             updateLocatStorage(newList)
             setRead(newList)
-            
+
         }
-    } 
-    const removeToListRead = book =>{
-        const newList = read.filter(item=> item.ISBN!==book.ISBN)
+    }
+    const removeToListRead = book => {
+        const newList = read.filter(item => item.ISBN !== book.ISBN)
         updateLocatStorage(newList)
         setRead(newList)
-        
+
     }
-    return(
+    return (
         <ReadingContext.Provider value={{
             read,
             addtoToListRead,
