2b48f56f3e9a5941cbeefc1fd49c354d63a9593a
TobysNap <naptobys17@gmail.com>
2023-07-17 18:56:40 -0300

    Mostrar componentes en app-component

--- pruebas/01-reading-list/TobysNap/src/app/app.component.ts
+++ pruebas/01-reading-list/TobysNap/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <app-books></app-books>
+    <app-reading-list></app-reading-list>
   `,
   styles: [``]
 })
