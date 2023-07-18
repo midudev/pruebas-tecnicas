be925d9d28a9f6c60d675e4cdaaa35aef5d68304
damian <the.dmian@live.cl>
2023-07-18 15:29:42 -0400

    First commit install styled-components and linter

---
+++ pruebas/01-reading-list/dmiante/lista-libros/.eslintrc.cjs
@@ -0,0 +1,29 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended',
+    './node_modules/ts-standard/eslintrc.json'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+}
---
+++ pruebas/01-reading-list/dmiante/lista-libros/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <h1>lista libros</h1>
+    </>
+  )
+}
+
+export default App
