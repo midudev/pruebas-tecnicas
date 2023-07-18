a27cbc9031087d615613e6f1f1e0839019d4fe4d
John Serrano <andrey_201015@hotmail.com>
2023-07-18 16:09:03 -0500

    list books with title and cover in main page

--- pruebas/01-reading-list/johnsi15/src/App.jsx
+++ pruebas/01-reading-list/johnsi15/src/App.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import data from './services/books.json'
 
 function App () {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const { library } = data
+    console.log(library)
+    setBooks(library)
+  }, [])
   return (
     <>
       <h3>Lista de libros</h3>
+      {books.length === 0 && <p>No hay libros</p>}
+
+      {books.map(dataBook => {
+        const { title, ISBN: id, cover } = dataBook.book
+        console.log(title)
+        return (
+          <div className="book" key={id}>
+            <h2>{title}</h2>
+            <figure>
+              <img src={cover} alt={title} />
+            </figure>
+          </div>
+        )
+      })}
     </>
   )
 }
