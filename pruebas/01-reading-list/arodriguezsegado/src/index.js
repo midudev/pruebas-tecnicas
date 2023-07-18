bed3dd9664ed43eeb46609fd08cd11ae5708f09a
Alejandro Rodriguez <arodriguezsegado@gmail.com>
2023-07-18 20:51:58 +0200

    Hook para obtener los libros y estilos de las tarjetas de libros disponibles

--- pruebas/01-reading-list/arodriguezsegado/src/index.js
+++ pruebas/01-reading-list/arodriguezsegado/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Library } from './componentes/Library';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library />
   </React.StrictMode>
 );
 
