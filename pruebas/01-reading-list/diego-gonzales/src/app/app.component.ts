a029f1ece54d40a026ed8bf7b7a31228c194feeb
Diego Gonzales <diegoegj14@gmail.com>
2023-07-18 15:09:06 -0500

    improvements in responsive design

--- pruebas/01-reading-list/diego-gonzales/src/app/app.component.ts
+++ pruebas/01-reading-list/diego-gonzales/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
