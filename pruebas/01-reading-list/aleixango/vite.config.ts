e6a8a61c15a90e3c04aa3283ddb247f03641b2d5
Aleix Anguita Gomez <aleixango@gmail.com>
2023-07-18 23:52:55 +0200

    Declare alias for src folder

--- pruebas/01-reading-list/aleixango/vite.config.ts
+++ pruebas/01-reading-list/aleixango/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import {resolve } from "path"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@': resolve(__dirname, 'src')
-    }
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
   },
   plugins: [vue()],
 })
