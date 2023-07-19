cced0e7e6f27c0d4dcb5659a6bb00afdcb8e7dad
Daniel Bueno <danielfelipe96@hotmail.com>
2023-07-19 08:31:21 -0500

    FIX: Estilo en carga inicial

--- pruebas/01-reading-list/dbueno96/reading-list/app/page.tsx
+++ pruebas/01-reading-list/dbueno96/reading-list/app/page.tsx
@@ -4,8 +4,8 @@ import { bookSectionTilte, readingListSectioNTitle } from './constants.texts'
 
 export default function MainPage () {
   return (
-    <main className='bg-gray-800 text-white text-4xl h-screen flex justify-between gap-0'>
-      <BookSection title={bookSectionTilte} />
+    <main className='bg-gray-800 text-white text-4xl h-screen flex justify-between gap-0  '>
+      <BookSection className='flex-grow' title={bookSectionTilte} />
       <BookSection title={readingListSectioNTitle} />
     </main>
   )
