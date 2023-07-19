59f94fd934a5cdb6cb78942be68e63e661eac0e8
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-07-19 15:45:32 -0400

    Create side menu component for movile devices

---
+++ pruebas/01-reading-list/tonysantana1492/src/components/SideMenu.tsx
@@ -0,0 +1,15 @@
+import { useMenu } from '../store'
+import { Favorites } from './Favorites'
+
+export const SideMenu = () => {
+  const { onClose } = useMenu()
+
+  return (
+		<nav
+			onClick={onClose}
+			className="fixed top-0 right-0 backdrop-blur-sm bg-[#6565659e] w-full h-screen flex justify-end"
+		>
+			<Favorites />
+		</nav>
+  )
+}
