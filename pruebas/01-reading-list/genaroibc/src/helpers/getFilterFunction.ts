f8f3e3889460115ae03c04fa07fc7e09d575a9b9
genaroibc <bonavitaismael@gmail.com>
2023-07-19 12:00:47 -0300

    [DEV]: add filters helpers

---
+++ pruebas/01-reading-list/genaroibc/src/helpers/getFilterFunction.ts
@@ -0,0 +1,37 @@
+import type { Book, Filter } from "~/types"
+
+export function getFilterFunction(filter: Filter) {
+  const filterFunction = (a: Book, b: Book) => {
+    const valueA = a[filter]
+    const valueB = b[filter]
+
+    const valuesAreStrings =
+      typeof valueA === "string" && typeof valueB === "string"
+
+    if (valuesAreStrings) {
+      return stringsFilter(valueA, valueB)
+    }
+
+    const valuesAreNumbers =
+      typeof valueA === "number" && typeof valueB === "number"
+
+    if (valuesAreNumbers) {
+      return numbersFilter(valueA, valueB)
+    }
+
+    return numbersFilter(Number(valueA), Number(valueB)) // default filter
+  }
+
+  return filterFunction
+}
+
+const numbersFilter = (a: number, b: number) => {
+  if (a < b) return 1
+  if (a > b) return -1
+
+  return 0
+}
+
+const stringsFilter = (a: string, b: string) => {
+  return a.localeCompare(b)
+}
