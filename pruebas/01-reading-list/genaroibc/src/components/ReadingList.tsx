9f06a0efd9674436d980b832076ff016c753ac77
genaroibc <bonavitaismael@gmail.com>
2023-07-19 18:14:32 -0300

    [FEAT]: show message when reading list is empty

--- pruebas/01-reading-list/genaroibc/src/components/ReadingList.tsx
+++ pruebas/01-reading-list/genaroibc/src/components/ReadingList.tsx
@@ -8,28 +8,49 @@ type Props = {
 
 export const ReadingList = component$(({ books, onBookSelect }: Props) => {
   return (
-    <aside class="bg-slate-950 p-4">
-      <ul class="flex items-center gap-2 group/list w-fit">
-        {books.map((book, index) => (
-          <li
-            key={book.ISBN}
-            class="aspect-[1/1.6] group"
-            style={{
-              transform: `translateX(-${1.8 * index}rem)`
-            }}
-          >
-            <button onClick$={() => onBookSelect(book.ISBN)} class="w-28">
-              <img
-                src={book.cover}
-                alt={book.title}
-                width={300}
-                height={500}
-                class="h-full shadow-2xl group-hover/list:grayscale-[1] group-hover/list:brightness-75 hover:!grayscale-0 hover:!brightness-110 group-hover:-translate-x-5 transition-all duration-300"
-              />
-            </button>
-          </li>
-        ))}
+    <aside class="bg-blue-950 px-8 p-4 max-w-full overflow-auto">
+      <ul
+        style={books.length > 0 ? { width: 0 } : undefined}
+        class="flex items-center gap-2 group/list"
+      >
+        {books.length === 0 ? (
+          <NoBooksMessage />
+        ) : (
+          books.map((book, index) => (
+            <li
+              key={book.ISBN}
+              class="aspect-[1/1.6] group"
+              style={{
+                transform: `translateX(-${1.8 * index}rem)`
+              }}
+            >
+              <button onClick$={() => onBookSelect(book.ISBN)} class="w-28">
+                <img
+                  src={book.cover}
+                  alt={book.title}
+                  width={300}
+                  height={500}
+                  class="h-full shadow-2xl group-hover/list:grayscale-[1] group-hover/list:brightness-75 hover:!grayscale-0 hover:!brightness-110 group-hover:-translate-x-5 transition-all duration-300"
+                />
+              </button>
+            </li>
+          ))
+        )}
       </ul>
     </aside>
+  )
+})
+
+const NoBooksMessage = component$(() => {
+  return (
+    <div class="py-8">
+      <h2 class="text-2xl font-bold text-white mb-4">
+        No books in your reading list
+      </h2>
+
+      <p class="text-white">
+        Add books to your reading list by clicking or dragging and dropping them
+      </p>
+    </div>
   )
 })
