405c8705f8cee6f5f8c2c348e3139be72e46c01a
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-07-19 02:01:27 -0400

    fix: replace special characters

--- pruebas/01-reading-list/SergioRibera/src/utils.rs
+++ pruebas/01-reading-list/SergioRibera/src/utils.rs
@@ -1,4 +1,10 @@
-
 pub fn to_slug(s: String) -> String {
-    s.to_lowercase().replace(" ", "-")
+    s.to_lowercase()
+        .replace(" ", "-")
+        .replace("á", "a")
+        .replace("é", "e")
+        .replace("í", "i")
+        .replace("ó", "o")
+        .replace("ú", "u")
+        .replace("ñ", "n")
 }
