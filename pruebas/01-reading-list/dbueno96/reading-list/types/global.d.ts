afb2fa3402d9512ed38c833330196dd37965f511
Daniel Bueno <danielfelipe96@hotmail.com>
2023-07-19 10:16:22 -0500

    ADD: Book interface

--- pruebas/01-reading-list/dbueno96/reading-list/types/global.d.ts
+++ pruebas/01-reading-list/dbueno96/reading-list/types/global.d.ts
@@ -9,11 +9,27 @@ declare global {
     }
 
     interface IWithClassName {
-        className: string
+        className?: string
    }
 
    interface IWithChildren {
        children: React.ReactNode
     }
+
     interface IWithTiltleAndClassName extends IWithClassName, IWithTitle {}
+
+    interface IAuthor {
+        name: string,
+        otherBooks: string[]
+    }
+
+    interface IBook extends IWithTitle{
+        pages: number,
+        genre: string,
+        cover: string,
+        synopsis: string,
+        year: number,
+        ISBN: string,
+        author: IAuthor,
+    }
 }
