4f26df48cc3eaaa687848298c0d56ee715864128
LuisMFG <luismiguelfloresgr@gmail.com>
2023-07-19 22:35:31 +0200

    filter display styles fixed

--- pruebas/01-reading-list/LuisMFG1992/src/Components/DisplayFilters.jsx
+++ pruebas/01-reading-list/LuisMFG1992/src/Components/DisplayFilters.jsx
@@ -6,22 +6,20 @@ const DisplayFilters = ({ selectedFilters }) => {
   const dispatch = useDispatch()
   return (
     <div className="flex flex-col items-center gap-5 lg:flex-row">
-      {/* <div className="bg-gray-001 w-[200px] inline-flex gap-5 h-[4.5rem] items-center justify-center rounded-lg px-2 border border-purple-600">
-        <p>Paginas:</p>
-        <p className="bg-purple-600 rounded-lg px-2">100</p>
-      </div> */}
-
-      {/* <div className="flex flex-col gap-1 p-5 h-[4.5rem] min-w-[100px] bg-gray-001 items-center justify-center rounded-lg px-2 border border-purple-600">
-        <p>Paginas:</p>
-        <div className=" inline-flex gap-5  items-center justify-center ">
-          <p className="bg-purple-600 rounded-lg px-2">100</p>
+      <div className="flex flex-col gap-1 bg-gray-001 w-[100px] p-3 h-[4.9rem] justify-center rounded-lg px-2 border border-purple-600">
+        <p className="text-center">Paginas:</p>
+        <div className="w-full inline-flex items-center justify-center">
+          <button className="inline-flex items-center gap-1 justify-center bg-purple-600 rounded-lg px-2">
+            100
+            <AiFillCloseCircle />
+          </button>
         </div>
-      </div> */}
+      </div>
 
       {selectedFilters.length !== 0 ? (
-        <div className="flex flex-col gap-2 sm:gap-1 p-3 min-h-[4rem] bg-gray-001 items-center justify-center rounded-lg px-2 border border-purple-600">
+        <div className="flex flex-col gap-2 sm:gap-1 p-3 min-h-[4.5rem] bg-gray-001 items-center justify-center rounded-lg px-2 border border-purple-600">
           <p className="text-center">Generos seleccionados:</p>
-          <div className=" inline-flex gap-4 items-center justify-center flex-wrap">
+          <div className="inline-flex gap-4 items-center justify-center flex-wrap">
             {selectedFilters.map((filter) => (
               <button
                 className="inline-flex items-center gap-1 justify-center bg-purple-600 rounded-lg px-2"
