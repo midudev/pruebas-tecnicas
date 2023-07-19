b029ef8f8af8fbedf4f260ee287ab6c8cdacb4af
DavidDeadly <jdrueda513@gmail.com>
2023-07-18 21:32:46 -0500

    Refactor filters

--- pruebas/01-reading-list/DavidDeadly/DavBooks/src/components/Header.svelte
+++ pruebas/01-reading-list/DavidDeadly/DavBooks/src/components/Header.svelte
@@ -1,17 +1,10 @@
 <script lang="ts">
-  import { booksStore } from '$lib';
-  import { NO_GENRES_FILTER } from '$lib/constants';
-  import { useBookFilteredEvent } from '$lib/events/booksFiltered';
+  import Filters from './Filters.svelte';
   import DavBooksIcon from './icons/DavBooksIcon.svelte';
 
   export let numAvaileblesBooks: number = 0;
+
   const plural = new Intl.PluralRules('es');
-  const { sendBooksFiltered } = useBookFilteredEvent();
-
-  let maxPages: number = $booksStore.max;
-  let genre: string = NO_GENRES_FILTER;
-  $: sendBooksFiltered({ genre, maxPages });
-
   let isOne: boolean = false;
   $: isOne = plural.select(numAvaileblesBooks) === 'one';
 </script>
@@ -25,35 +18,7 @@
     {numAvaileblesBooks}
     {isOne ? 'Libro' : 'Libros'} Disponibles
   </h1>
-  <div style:opacity={$booksStore.books.length ? '1' : '0'} class="filters">
-    <label for="pages">
-      <span>Filtrar por páginas</span>
-      <input
-        id="pages"
-        type="range"
-        min={$booksStore.min}
-        max={$booksStore.max}
-        on:change|nonpassive={ev => console.log('changing', ev)}
-        bind:value={maxPages}
-      />
-      <div class="ranges">
-        <span>{$booksStore.min}</span>
-        <span>{maxPages}</span>
-      </div>
-    </label>
-    <label for="genre">
-      <span>Filtrar por géneros</span>
-
-      <select id="genre" bind:value={genre}>
-        <option value={NO_GENRES_FILTER}>{NO_GENRES_FILTER}</option>
-        {#each $booksStore.genres as genre}
-          <option value={genre}>
-            {genre}
-          </option>
-        {/each}
-      </select>
-    </label>
-  </div>
+  <Filters on:filteredBooks />
 </nav>
 
 <style>
@@ -79,34 +44,5 @@
     top: 50%;
     transform: translateY(-50%);
     left: 0;
-  }
-
-  .filters {
-    display: flex;
-    width: 60%;
-    justify-content: space-between;
-  }
-
-  .ranges {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  select {
-    padding: 10px;
-    border-radius: 15px;
-  }
-
-  label {
-    display: flex;
-    width: 40%;
-    flex-direction: column;
-    justify-content: space-evenly;
-    text-align: center;
-  }
-
-  span {
-    display: inline-block;
-    margin-bottom: 10px;
   }
 </style>
---
+++ pruebas/01-reading-list/DavidDeadly/DavBooks/src/components/RangeInput.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { booksStore } from '$lib';
+
+  export let value: number;
+  export let id: string;
+</script>
+
+<span>Filtrar por páginas</span>
+
+<input
+  {id}
+  type="range"
+  min={$booksStore.min}
+  max={$booksStore.max}
+  bind:value
+/>
+
+<div class="ranges">
+  <span>{$booksStore.min}</span>
+  <span>{value}</span>
+</div>
+
+<style>
+  .ranges {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  span {
+    display: inline-block;
+    margin-bottom: 10px;
+  }
+</style>
