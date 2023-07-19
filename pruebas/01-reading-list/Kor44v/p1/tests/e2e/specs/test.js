fe707a95395862670dead2888c547d9238733347
Marcelo Gutierrez <m.gutierrezzamorano1@gmail.com>
2023-07-18 22:08:09 -0400

    init

---
+++ pruebas/01-reading-list/Kor44v/p1/tests/e2e/specs/test.js
@@ -0,0 +1,8 @@
+// https://docs.cypress.io/api/table-of-contents
+
+describe('My First Test', () => {
+  it('Visits the app root url', () => {
+    cy.visit('/')
+    cy.contains('h1', 'Welcome to Your Vue.js App')
+  })
+})
