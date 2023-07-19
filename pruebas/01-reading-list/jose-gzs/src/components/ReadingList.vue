abe453e4fbbe34d2a446fe8c67456b22057581e2
jose-gzs <jgonzalezsachez88@gmail.com>
2023-07-19 21:25:35 +0200

    Page Filter and some styles

--- pruebas/01-reading-list/jose-gzs/src/components/ReadingList.vue
+++ pruebas/01-reading-list/jose-gzs/src/components/ReadingList.vue
@@ -4,16 +4,25 @@
     import ReadingListBookItem from './ReadingListBookItem.vue'
 
     const storage = window.localStorage
-    const savedReadingList = storage.getItem('readingList') || []
+    const savedReadingList = JSON.parse(storage.getItem('readingList')) || []
 
-    const readingList = ref([savedReadingList])
+    const readingList = ref(savedReadingList)
+    const pagesFilter = ref(0)
 
     const availableBooks = computed(() => {
-        return books.library.filter( el => !readingList.value.includes(el.book.ISBN))
+        
+        return filteredBooks.value?.filter( el => !readingList.value.includes(el.book.ISBN))
+        // return books.library.filter( el => !readingList.value.includes(el.book.ISBN))
     })
 
     const readingListBooks = computed(() => {
         return books.library.filter( el => readingList.value.includes(el.book.ISBN))
+    })
+
+    const filteredBooks = computed(() => {
+        return books.library.filter( el => {
+            return !pagesFilter.value || (pagesFilter.value > 0 && el.book.pages<=pagesFilter.value)
+        }) || []
     })
 
     function addBookToReadingList(id) {
@@ -27,12 +36,17 @@
         storage.setItem('readingList', JSON.stringify(readingList.value))
     }
 
-
+    const maxPages = computed(() => {
+        return Math.max(...books.library.map((el) => el.book.pages))
+    })
 </script>
 
 <template>
     <h2 class="text-4xl font-bold px-8">{{ availableBooks.length }} libros disponibles</h2>
     <h2 v-if="readingListBooks.length" class="text-3xl font-semibold px-8">{{ readingListBooks.length }} en la lista de lectura</h2>
+
+    <input type="range" name="" id="" min="1" :max="maxPages" v-model="pagesFilter">
+    {{ pagesFilter }}
     <div class="inline-flex items-center justify-center px-8 w-full mt-4">
         <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 mx-auto gap-4">
             <template v-for="book in availableBooks" :key="book.book.ISBN">
@@ -42,8 +56,8 @@
     </div>
 
     <hr class="my-8">
-    <div class="inline-flex items-center justify-center px-8 w-full mt-4">
-        <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 mx-auto gap-4">
+    <div v-if="readingListBooks.length" class="inline-flex items-center justify-center px-8 w-full mt-4">
+        <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 mx-auto gap-4 border border-slate-300 rounded-lg shadow-md p-4">
             <template v-for="(book,index) in readingListBooks" :key="book.book.ISBN">
                 <ReadingListBookItem :book="book.book" @click="deleteBookFromReadingList(index)"/>
             </template>
