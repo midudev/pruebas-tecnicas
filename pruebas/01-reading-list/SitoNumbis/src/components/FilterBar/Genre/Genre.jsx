ddf5c4389b303acc3422390ec08d4566dbc1c59d
Sito8943 <sito8943@gmail.com>
2023-07-19 13:35:27 -0700

    aria-labels for buttons

--- pruebas/01-reading-list/SitoNumbis/src/components/FilterBar/Genre/Genre.jsx
+++ pruebas/01-reading-list/SitoNumbis/src/components/FilterBar/Genre/Genre.jsx
@@ -1,9 +1,15 @@
 import { memo } from "react";
 import PropTypes from "prop-types";
 
+// contexts
+import { useLanguage } from "../../../contexts/LanguageProvider";
+
 function Genre({ genre, active, onClick }) {
+  const { languageState } = useLanguage();
+
   return (
     <button
+      aria-label={`${languageState.texts.ariaLabels.filterByGenre} ${genre}`}
       onClick={() => onClick(genre)}
       className={`px-5 py-3 transition min-w-[150px] cursor-default ${
         active ? "bg-primary" : "bg-dark-alt-bg hover:bg-primary-dark"
