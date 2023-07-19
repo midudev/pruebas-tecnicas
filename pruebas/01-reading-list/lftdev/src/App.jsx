8cea6528692c2fd62ed3e066fabcbd87a0224d33
lftdev <torres.lf21@gmail.com>
2023-07-19 13:36:37 -0300

    Apply basic style

--- pruebas/01-reading-list/lftdev/src/App.jsx
+++ pruebas/01-reading-list/lftdev/src/App.jsx
@@ -5,41 +5,52 @@ import BooksList from './components/BooksList'
 export default function App () {
   const [booksList, setBooksList] = useState([])
   const [readList, setReadList] = useState([])
-  // Load book library to state bookList.
-  useEffect(() => BooksJSON.library.forEach(bookObject => setBooksList(prevList => [...prevList, bookObject.book])), [])
+  const [readListAvailable, setReadListAvailable] = useState(false)
+  // Load JSON books library to state bookList.
+  useEffect(() => BooksJSON.library.forEach((bookObject) => setBooksList((prevList) => [...prevList, bookObject.book])), [])
 
   function addToReadList (book) {
-    setReadList(prevList => [...prevList, book])
+    setReadList((prevList) => [...prevList, book])
+    if (!readListAvailable) setReadListAvailable(true)
   }
-
   function removeFromReadList (index) {
-    setReadList(prevList => prevList.splice(index, 1))
+    setReadList((prevList) => {
+      const newList = prevList.toSpliced(index, 1)
+      if (newList.length === 0) setReadListAvailable(false)
+      return newList
+    })
   }
 
   return (
     <>
-      <h3>Sin libros en la lista de lectura</h3>
-      <main>
-        <h1>13 libros disponibles</h1>
-        <form role='search'>
-          <label htmlFor='pages-filter'>
-            <div>Filtrar por páginas</div>
-            <input id='pages-filter' type='range' />
-          </label>
-          <label htmlFor='genre-filter'>
-            <div>Filtrar por género</div>
-            <select id='genre-filter'>
-              <option value='all'>Todos los géneros</option>
-            </select>
-          </label>
-        </form>
-        <BooksList list={booksList} onItemClick={addToReadList} />
+      <h3 className='text-2xl text-blue-500'>{readListAvailable ? 'Con' : 'Sin'} libros en la lista de lectura</h3>
+      <main className='grid [grid-template-columns:2fr_1fr] border border-white rounded-md pt-10 px-12'>
+        <aside className='flex flex-col gap-5'>
+          <div className='flex flex-col gap-4'>
+            <h1 className='text-4xl'>{booksList.length} libros disponibles</h1>
+            {readListAvailable && <p className='text-lg'>{readList.length} en la lista de lectura</p>}
+            <form role='search' className='flex gap-32'>
+              <label className='text-lg' htmlFor='pages-filter'>
+                <div>Filtrar por páginas</div>
+                <input id='pages-filter' type='range' />
+              </label>
+              <label className='text-md' htmlFor='genre-filter'>
+                <div>Filtrar por género</div>
+                <select className='bg-black' id='genre-filter'>
+                  <option value='all'>Todos</option>
+                </select>
+              </label>
+            </form>
+          </div>
+          <BooksList className='grid grid-cols-4 place-items-start gap-4' list={booksList} onItemClick={addToReadList} />
+        </aside>
+        {readListAvailable && (
+          <aside className='sticky top-0 max-h-screen overflow-y-scroll bg-[#040412] rounded-lg p-8' role='region'>
+            <h2 className='text-3xl'>Lista de lectura</h2>
+            <BooksList className='grid grid-cols-2 place-items-start gap-4' list={readList} removableItems onRemoveRequest={removeFromReadList} />
+          </aside>
+        )}
       </main>
-      {readList.length > 0 &&
-        <aside role='region'>
-          <h2>Lista de lectura</h2>
-          <BooksList list={readList} removableItems onRemoveRequest={removeFromReadList} />
-        </aside>}
     </>
   )
 }
--- pruebas/01-reading-list/lftdev/src/components/BooksList.jsx
+++ pruebas/01-reading-list/lftdev/src/components/BooksList.jsx
@@ -1,14 +1,14 @@
 import BookItem from './BookItem'
 
 export default function BooksList (props) {
-  const { list, onItemClick, removableItems, onRemoveRequest } = props
+  const { list, onItemClick, removableItems, onRemoveRequest, className } = props
   function removeRequest (index) {
     onRemoveRequest(index)
   }
   return (
-    <ul>
+    <ul className={className}>
       {list.map((book, index) =>
-        <li key={index}>
+        <li key={index} className='w-28'>
           {removableItems && <button onClick={() => removeRequest(index)}>x</button>}
           <BookItem book={book} onClick={onItemClick} />
         </li>
