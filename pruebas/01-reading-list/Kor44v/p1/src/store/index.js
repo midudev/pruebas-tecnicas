4f123b2ac23eb7d88d01650e282bb041bc838a90
Marcelo Gutierrez <m.gutierrezzamorano1@gmail.com>
2023-07-19 00:55:43 -0400

    vuetify added

--- pruebas/01-reading-list/Kor44v/p1/src/store/index.js
+++ pruebas/01-reading-list/Kor44v/p1/src/store/index.js
@@ -5,12 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    library:[]
   },
   getters: {
   },
   mutations: {
   },
   actions: {
+    getLibrary(){
+      fetch('/books.json')
+      .then(resp=>resp.json())
+      .then(data=>{
+        console.log(data)
+         //hacer la mutacion para agregar la data al store y hacer la llamada aqui en la action
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+    }
   },
   modules: {
   }
--- pruebas/01-reading-list/Kor44v/p1/src/views/HomeView.vue
+++ pruebas/01-reading-list/Kor44v/p1/src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <hello-world />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import HelloWorld from '../components/HelloWorld'
+import { mapActions } from 'vuex'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home-view',
+    created() {
+      this.getLibrary()
+    },
+    methods: {
+      ...mapActions(['getLibrary'])
+    },
+
+    components: {
+      HelloWorld,
+    },
   }
-}
 </script>
