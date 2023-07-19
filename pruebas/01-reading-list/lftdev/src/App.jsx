c3abf5a987de815c44ffe4b3198c85cd8aef3459
lftdev <torres.lf21@gmail.com>
2023-07-19 19:40:49 -0300

    Refactor creating useBooksList hook

--- pruebas/01-reading-list/lftdev/src/App.jsx
+++ pruebas/01-reading-list/lftdev/src/App.jsx
@@ -1,36 +1,29 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import BooksJSON from './database/books.json'
 import './style.css'
 import BooksList from './components/BooksList'
-export default function App () {
-  const [booksList, setBooksList] = useState([])
-  const [readList, setReadList] = useState([])
-  const [readListAvailable, setReadListAvailable] = useState(false)
-  // Load JSON books library to state bookList.
-  useEffect(() => BooksJSON.library.forEach((bookObject) => setBooksList((prevList) => [...prevList, bookObject.book])), [])
+import useBooksList from './hooks/useBooksList'
 
-  function addToReadList (book) {
-    setBooksList(prevList => prevList.filter(item => item !== book))
-    setReadList((prevList) => [...prevList, book])
-    if (!readListAvailable) setReadListAvailable(true)
-  }
-  function removeFromReadList (index) {
-    setReadList((prevList) => {
-      setBooksList(prev => [...prev, prevList[index]])
-      const newList = prevList.toSpliced(index, 1)
-      if (newList.length === 0) setReadListAvailable(false)
-      return newList
-    })
-  }
+export default function App () {
+  const {
+    availableBooks,
+    addToAvaiables,
+    readList,
+    addToReadList,
+    removeFromReadList,
+    readListHasBooks
+  } = useBooksList([])
+  // Load JSON books library to state bookList.
+  useEffect(() => BooksJSON.library.forEach((bookObject) => addToAvaiables(bookObject.book)), [])
 
   return (
     <>
-      <h3 className='text-2xl text-blue-500'>{readListAvailable ? 'Con' : 'Sin'} libros en la lista de lectura</h3>
+      <h3 className='text-2xl text-blue-500'>{readListHasBooks ? 'Con' : 'Sin'} libros en la lista de lectura</h3>
       <main className='grid [grid-template-columns:2fr_1fr] border border-white rounded-md pt-10 px-12'>
         <aside className='flex flex-col gap-5' role='presentation'>
           <div className='flex flex-col gap-4'>
-            <h1 className='text-4xl'>{booksList.length} libros disponibles</h1>
-            {readListAvailable && <p className='text-lg'>{readList.length} en la lista de lectura</p>}
+            <h1 className='text-4xl'>{availableBooks.length} libros disponibles</h1>
+            {readListHasBooks && <p className='text-lg'>{readList.length} en la lista de lectura</p>}
             <form role='search' className='flex gap-32'>
               <label className='text-lg' htmlFor='pages-filter'>
                 <div>Filtrar por páginas</div>
@@ -44,9 +37,9 @@ export default function App () {
               </label>
             </form>
           </div>
-          <BooksList className='grid grid-cols-4 place-items-start gap-4' list={booksList} onItemClick={addToReadList} />
+          <BooksList className='grid grid-cols-4 place-items-start gap-4' list={availableBooks} onItemClick={addToReadList} />
         </aside>
-        {readListAvailable && (
+        {readListHasBooks && (
           <aside className='sticky top-0 max-h-screen overflow-y-scroll bg-[#040412] rounded-lg p-8' role='region'>
             <h2 className='text-3xl'>Lista de lectura</h2>
             <BooksList className='grid grid-cols-2 place-items-start gap-4' list={readList} removableItems onRemoveRequest={removeFromReadList} />
---
+++ pruebas/01-reading-list/lftdev/src/hooks/useBooksList.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+export default function useBooksList (initialList) {
+  const [availableBooks, setAvailableBooks] = useState(initialList)
+  const [readList, setReadList] = useState([])
+  const [readListHasBooks, setReadListHasBooks] = useState(false)
+
+  const addToAvaiables = (book) => setAvailableBooks(prevList => [...prevList, book])
+  const removeFromAvailables = (book) => setAvailableBooks(prevList => prevList.filter(item => item !== book))
+  function addToReadList (book) {
+    removeFromAvailables(book)
+    setReadList((prevList) => [...prevList, book])
+    if (!readListHasBooks) setReadListHasBooks(true)
+  }
+  function removeFromReadList (index) {
+    setReadList((prevList) => {
+      addToAvaiables(prevList[index])
+      const newList = prevList.toSpliced(index, 1)
+      if (newList.length === 0) setReadListHasBooks(false)
+      return newList
+    })
+  }
+  return {
+    availableBooks,
+    addToAvaiables,
+    removeFromAvailables,
+    readList,
+    addToReadList,
+    removeFromReadList,
+    readListHasBooks
+  }
+}
