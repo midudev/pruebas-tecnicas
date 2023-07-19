5c7889c52cf7d1930ce5b34c14f1932fff9f6e53
Daniel Bueno <danielfelipe96@hotmail.com>
2023-07-19 14:30:07 -0500

    REFACTOR: Section width and animation duration

--- pruebas/01-reading-list/dbueno96/reading-list/app/components/BookSection/index.tsx
+++ pruebas/01-reading-list/dbueno96/reading-list/app/components/BookSection/index.tsx
@@ -8,7 +8,7 @@ export default function BookSection (props: IBookSection) {
   const { title, books, className = '' } = props
 
   return (
-    <Layout title={title} className={`transition-[width] duration-150 ease-out rounded-md border-solid border-2 border-gray-50 hover:w-11/12 ${className}`}>
+    <Layout title={title} className={`transition-[width] duration-500 ease-out rounded-md border-solid border-2 border-gray-50 w-1/12 hover:w-11/12 ${className}`}>
       <BookList books={books} />
     </Layout>
   )
