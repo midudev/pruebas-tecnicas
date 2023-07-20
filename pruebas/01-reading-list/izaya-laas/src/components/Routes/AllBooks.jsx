2b9891a161c5287995e19493ebf64f130d56d561
Lautaro Asat <lautaronorielasat@gmail.com>
2023-07-19 22:38:40 -0300

    Add filters on AllBooks

--- pruebas/01-reading-list/izaya-laas/src/components/Routes/AllBooks.jsx
+++ pruebas/01-reading-list/izaya-laas/src/components/Routes/AllBooks.jsx
@@ -1,34 +1,40 @@
-import { library as books } from '../../database/books.json';
-import { myReadingListISBN, myReadingListLength } from '../../states/store';
+// import { library as books } from '../../database/books.json';
+import { filterBooks } from '../../filters/forBook';
+import { allBooks, currentPath, myReadingListISBN } from '../../signals/store';
 
 const AllBooks = () => {
-	const addBookToReadingList = (isbn) => {
-		const setISBN = new Set(myReadingListISBN.value);
+	currentPath.value = '/';
 
-		if (setISBN.size <= myReadingListLength && setISBN.size !== 0) return '';
-
-		myReadingListISBN.value = [...myReadingListISBN.value, isbn];
+	const addBookToReadingList = (ISBN) => {
+		if (myReadingListISBN.value.includes(ISBN)) return;
+		myReadingListISBN.value = [...myReadingListISBN.value, ISBN];
 	};
 	return (
 		<>
-			{books.map(({ book }) => {
+			{filterBooks(allBooks.value).map((book) => {
 				const { title, cover, author, ISBN } = book;
 				const { name } = author;
 
 				return (
 					<figure
-						className="w-40 lg:w-48 border border-[#41403E] p-4 rounded-tl-[255px_15px] rounded-tr-[15px_255px] rounded-br-[255px_15px] rounded-bl-[15px_255px]"
+						className={`w-40 lg:w-48 border border-[#41403E] p-4 rounded-tl-[255px_15px] rounded-tr-[15px_255px] rounded-br-[255px_15px] rounded-bl-[15px_255px] ${
+							myReadingListISBN.value.includes(ISBN)
+								? 'bg-grated-pattern'
+								: 'bg-white'
+						}`}
 						id={ISBN}
 						onClick={() => {
 							addBookToReadingList(ISBN);
 						}}
 					>
 						<img
-							className="w-full object-cover aspect-[305/400] rounded-t-md grayscale-[60%]"
+							className="w-full object-cover aspect-[305/400] rounded-tl-[255px_15px] rounded-tr-[15px_255px] rounded-br-[255px_15px] rounded-bl-[15px_255px] grayscale-[60%]"
 							src={cover}
 							alt={`cover of ${title}`}
 						/>
-						<p className="mt-2 text-sm text-center text-gray-800">{name}</p>
+						<p className="bg-white mt-2 text-sm text-center text-gray-800">
+							{name}
+						</p>
 					</figure>
 				);
 			})}
--- pruebas/01-reading-list/izaya-laas/src/filters/forBook.js
+++ pruebas/01-reading-list/izaya-laas/src/filters/forBook.js
@@ -1,3 +1,44 @@
-const booksPerf = books.map(({ book: bookData }) => bookData);
-booksPerf.filter((book) => book.title.toLowerCase().includes('lo'));
-booksPerf.filter((book) => book.genre.toLowerCase() === 'terror');
+import { filterOptions } from '../signals/inputs.signals';
+import { myReadingListISBN } from '../signals/store';
+
+export const filterBooks = (books) => {
+	const { excludeBooks, genre, pages, specificBook } = filterOptions.value;
+	const [min, max] = pages;
+	let booksFiltered = books;
+
+	if (excludeBooks && myReadingListISBN.value.length > 0) {
+		booksFiltered = books.filter((book) => {
+			return !myReadingListISBN.value.includes(book.ISBN);
+		});
+	}
+
+	if (specificBook && booksFiltered) {
+		booksFiltered = booksFiltered.filter((book) =>
+			book.title.toLowerCase().includes(specificBook.toLocaleLowerCase())
+		);
+	}
+
+	if (genre !== 'any') {
+		booksFiltered = booksFiltered.filter((book) => book.genre === genre);
+	}
+
+	if (min || max) {
+		if (min && !max) {
+			booksFiltered = booksFiltered.filter((book) => {
+				return Number(book.pages) >= Number(min);
+			});
+		} else if (!min && max) {
+			booksFiltered = booksFiltered.filter((book) => {
+				return Number(book.pages) <= Number(max);
+			});
+		} else if (min && max) {
+			booksFiltered = booksFiltered.filter((book) => {
+				return (
+					Number(book.pages) >= Number(min) && Number(book.pages) <= Number(max)
+				);
+			});
+		}
+	}
+
+	return booksFiltered;
+};
