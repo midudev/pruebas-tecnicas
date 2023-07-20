2da16696a05803692b11de1be688113923d6195d
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-07-19 22:23:46 -0400

    Add style to input type range

--- pruebas/01-reading-list/tonysantana1492/src/components/PageFilter.tsx
+++ pruebas/01-reading-list/tonysantana1492/src/components/PageFilter.tsx
@@ -15,11 +15,11 @@ export const PageFilter: React.FC<Props> = ({ pageFilter, maxPages, updatePageFi
 
   return (
 		<div className="flex flex-col justify-center items-center w-2/4">
-			<div className='flex justify-between w-full items-center'>
+			<div className='flex justify-between w-full items-center mb-3'>
 				<label className='text-xs font-bold text-gray-500' htmlFor={idRange}>Pages:</label>
 				<p className='text-xs font-bold text-white bg-slate-900 px-2 rounded-full'>{pageFilter}</p>
 			</div>
-			<input className='w-full' id={idRange} type="range" onChange={handleChange} value={pageFilter} min={0} max={maxPages} />
+			<input className='w-full h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer accent-gray-800' id={idRange} type="range" onChange={handleChange} value={pageFilter} min={0} max={maxPages} />
 		</div>
   )
 }
