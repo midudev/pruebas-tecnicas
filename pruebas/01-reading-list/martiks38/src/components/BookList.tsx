325c7a7141bf5f4fb2880aa8bb82e41d15f83f8b
Martiks38 <cappa.giuliano@gmail.com>
2023-07-20 16:15:19 -0300

    feat: add functionality add/remove from list

--- pruebas/01-reading-list/martiks38/src/components/BookList.tsx
+++ pruebas/01-reading-list/martiks38/src/components/BookList.tsx
@@ -1,13 +1,21 @@
 import Image from 'next/image'
 
-import { BookDataList } from '@/assets/values'
+import { useBookList } from '@/hooks/useBookList'
 
+import { BookDataList } from '@/assets/values'
 import bookListStyles from '@/assets/styles/Book/BookList.module.css'
 import bookButton from '@/assets/styles/Book/Button.module.css'
 
 type BookListProps = { books: BookDataList; listType: 'available' | 'reading' }
 
 export function BookList({ books, listType }: BookListProps) {
+  const { addToReadingList, removeFromReadingList } = useBookList()
+
+  const typeIsAvailable = listType === 'available'
+
+  const lineStyle = typeIsAvailable ? bookButton.addButton__line : bookButton.removeButton__line
+  const buttonAction = typeIsAvailable ? addToReadingList : removeFromReadingList
+
   return (
     <ul className={bookListStyles.bookList}>
       {books.map(({ cover, ISBN, title }) => {
@@ -19,14 +27,8 @@ export function BookList({ books, listType }: BookListProps) {
               </div>
               <figcaption className={bookListStyles.bookList__item__description}>
                 <h3 className={bookListStyles.bookList__item__descriptionTitle}>{title}</h3>
-                <button className={bookButton.actionButton}>
-                  <span
-                    className={
-                      listType === 'available'
-                        ? bookButton.addButton__line
-                        : bookButton.removeButton__line
-                    }
-                  ></span>
+                <button className={bookButton.actionButton} onClick={() => buttonAction({ ISBN })}>
+                  <span className={lineStyle}></span>
                 </button>
               </figcaption>
             </figure>
