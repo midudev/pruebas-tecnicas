9929b5bc21ac09bc62fccdd33ff245d7c615eaf5
damian <the.dmian@live.cl>
2023-07-20 01:30:01 -0400

    Change linter ts

--- pruebas/01-reading-list/dmiante/lista-libros/.eslintrc.cjs
+++ pruebas/01-reading-list/dmiante/lista-libros/.eslintrc.cjs
@@ -1,30 +1,25 @@
-/* eslint-env node */
-
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react-hooks/recommended',
-    './node_modules/ts-standard/eslintrc.json'
+    'plugin:react/recommended',
+    'standard-with-typescript'
   ],
-  parser: '@typescript-eslint/parser',
+  overrides: [
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: true,
-    tsconfigRootDir: __dirname,
+    project: './tsconfig.json'
   },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react'
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'react/react-in-jsx-scope': 'off'
-  },
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off'
+  }
 }
