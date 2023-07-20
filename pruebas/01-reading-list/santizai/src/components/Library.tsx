06585bd6f589d1c49f2e013ee2ea2b0dc98bca0b
SantiZai <santyagozaidan@gmail.com>
2023-07-20 14:04:41 -0300

    LocalStorage for save readList

--- pruebas/01-reading-list/santizai/src/components/Library.tsx
+++ pruebas/01-reading-list/santizai/src/components/Library.tsx
@@ -1,35 +1,49 @@
 import { useEffect, useState } from "react";
-import type { Book, Books } from "../types/interfaces";
+import type { Library, Books } from "../types/interfaces";
 import { filterForId } from "../services/filter";
 
 interface Props {
-    books: Books;
+    library: Books;
+    readList: string | null;
 }
 
 const Library = (props: Props) => {
-    const [library, setLibrary] = useState<Book[]>([]);
+    const [library, setLibrary] = useState<Library[]>([]);
     const [readlist, setReadlist] = useState<string | null>(
         localStorage.getItem("books")
     );
 
-    // hacer que ande este useeffect
-    // poder tomar si hay en localStorage libros para setear readList y posterior setear la libreria con los que no estan guardados en la lista
-
     useEffect(() => {
-        console.log(
-            filterForId(props.books.library[0].book.ISBN, props.books.library)
-        );
-        /* if (readlist !== null) {
-            
-        } */
-    }, []);
+        if (props.library && props.library.library.length > 0) {
+            const booksLocalStorage: string[] = (readlist || "").split(",");
+            const filteredLibrary = filterForId(
+                booksLocalStorage,
+                props.library.library
+            );
+            setLibrary(filteredLibrary);
+        }
+    }, [localStorage, readlist]);
 
-    console.log(props.books.library[0].book.ISBN);
+    const moveToReadList = (id: string) => {
+        let storage: string | null = localStorage.getItem("books");
+        // Previene que aparezca la palabra null al inicio de la cadena
+        if (storage === null) storage = "";
+        // Verifica si ya existe la id en la lista guardada, si no existe la agrega, ademas se fija si no hay ninguna id para no agregar una coma al principio de más
+        if (!storage.split(",").includes(id)) {
+            if (storage !== "") storage += `,${id}`;
+            else storage += id;
+            localStorage.setItem("books", String(storage));
+        }
+    };
 
     return (
         <div>
             <div>
-                <h2>Hay {props.books.library.length} libros</h2>
+                <h2>Hay {props.library.library.length} libros</h2>
+                <button onClick={() => console.log(readlist)}>aaaa</button>
+                <button onClick={() => moveToReadList("978-0553103540")}>
+                    move
+                </button>
             </div>
         </div>
     );
--- pruebas/01-reading-list/santizai/src/services/filter.ts
+++ pruebas/01-reading-list/santizai/src/services/filter.ts
@@ -1,5 +1,10 @@
 import type { Library } from "../types/interfaces";
 
-export const filterForId = (id: string, books: Library[]) => {
-    return books.find(book => book.book.ISBN === id)
-}
+// Toma un array de ids para filtrar los libros que no esten en la lista de lectura pasada, si no están los devuelve para crear la lista de libros disponibles
+export const filterForId = (ids: string[], books: Library[]) => {
+    let result: Library[] = [];
+    books.map((book: Library) => {
+        if (!ids.includes(book.book.ISBN)) result.push(book);
+    });
+    return result;
+};
