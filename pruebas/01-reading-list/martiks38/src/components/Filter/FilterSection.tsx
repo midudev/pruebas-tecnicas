2d2c82633c59148bee150aa960e9fd4e5063a2e8
Martiks38 <cappa.giuliano@gmail.com>
2023-07-20 14:56:02 -0300

    refactor: apply Filter context changes

--- pruebas/01-reading-list/martiks38/src/components/Filter/FilterSection.tsx
+++ pruebas/01-reading-list/martiks38/src/components/Filter/FilterSection.tsx
@@ -1,29 +1,31 @@
 import { useId } from 'react'
 import { useFilters } from '@/hooks/useFilters'
 
-import { books, genres } from '@/assets/values'
+import { genres } from '@/assets/values'
 import { allGenre } from '@/assets/constants'
 
 import homeStyles from '@/assets/styles/Home.module.css'
 
 export function FilterSection() {
   const numberPagesID = useId()
-  const { currentGenre, numberPages, changeCurrentGenre, changeNumberPages } = useFilters()
+  const { currentGenre, range, changeCurrentGenre, changeNumberPages } = useFilters()
 
   return (
     <section className={homeStyles.homeMain__bookGenresSection}>
-      <h3>Cantidad de páginas</h3>
+      <h3 className={homeStyles.booksGenresSection__title}>Número de páginas</h3>
       <label htmlFor={numberPagesID}></label>
       <input
         type='range'
         name={numberPagesID}
         id={numberPagesID}
         min={0}
-        step={50}
+        max={range.maxNumberPages}
+        value={range.currentNumberPages}
+        step={range.step}
         onChange={changeNumberPages}
         aria-describedby='numberPage'
       />
-      <span aria-describedby='numberPage'>{numberPages}</span>
+      <span aria-describedby='numberPage'>{range.currentNumberPages}</span>
       <h3 className={homeStyles.booksGenresSection__title}>Género</h3>
       <ul className={homeStyles.booksGenresSection__list}>
         {genres.map((genre) => (
