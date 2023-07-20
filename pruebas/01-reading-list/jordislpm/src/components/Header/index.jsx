a509574fe7bb14e0c5fd5ab6acc4523990247334
jordislpm <jordislpm@gmail.com>
2023-07-20 16:53:40 -0400

    feat: add icon in header

--- pruebas/01-reading-list/jordislpm/src/components/Header/index.jsx
+++ pruebas/01-reading-list/jordislpm/src/components/Header/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import styles from "./header.module.css"
-import { useContext, useState } from 'react'
-import { BooksAvailable } from '../../context/contextBooks'
 import Form from '../Form'
-import useFilter from '../../Hooks/useFilter'
+import useFilter from '../../Hooks/useFilter';
+import icon from "../../../public/book.svg";
 
 
 const Header = () => {
@@ -14,7 +13,11 @@ const {books, read}=useFilter()
 
   return (
     <div className={styles.header}>
-           <h1>Book Lover</h1>
+            <div className={styles.boxTitle}>
+              <img className={styles.icon} src={icon}></img>
+            <h1>Book Lover</h1>
+            </div>
+           
         <p className={styles.availables}>{`Tienes ${books.length} libro${books.length != 1 ? "s" : ""} disponible${books.length != 1 ? "s" : ""}`}</p>
         {read.length == 0
         ? 
