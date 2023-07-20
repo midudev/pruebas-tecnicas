e280d45283d788308f61c23b3ce45907630a8f34
Martiks38 <cappa.giuliano@gmail.com>
2023-07-20 14:57:41 -0300

    refactor: apply FilterSection and BookList

--- pruebas/01-reading-list/martiks38/src/components/BookDashboard.tsx
+++ pruebas/01-reading-list/martiks38/src/components/BookDashboard.tsx
@@ -1,30 +1,56 @@
 'use client'
 
-// import { useId, useRef, useState } from 'react'
+import { useBookList } from '@/hooks/useBookList'
+import { useFilters } from '@/hooks/useFilters'
 
-import homeStyles from '@/assets/styles/Home.module.css'
-import { books, genres } from '@/assets/values'
+import { BookList } from './BookList'
 import { FilterSection } from './Filter/FilterSection'
 
+import homeStyles from '@/assets/styles/Home.module.css'
+import { allGenre } from '@/assets/constants'
+
+function filterBook({
+  genre,
+  maxNumberPages,
+  currentGenre,
+  pages
+}: {
+  currentGenre: string
+  genre: string
+  maxNumberPages: number
+  pages: number
+}) {
+  const inRange = pages <= maxNumberPages
+  const equalGenre = currentGenre === allGenre ? true : currentGenre === genre
+
+  return inRange && equalGenre
+}
+
 export function BookDashboard() {
+  const { range, currentGenre } = useFilters()
+  const { listBooksAvailable, readingList } = useBookList()
+
+  const currentListBooksAvailable = listBooksAvailable.filter(({ genre, pages }) =>
+    filterBook({ currentGenre, genre, maxNumberPages: range.currentNumberPages, pages })
+  )
+
+  const currentReadingList = readingList.filter(({ genre, pages }) =>
+    filterBook({ currentGenre, genre, maxNumberPages: range.maxNumberPages, pages })
+  )
+
   return (
     <>
       <FilterSection />
       <article className={homeStyles.homeMain__bookDashboard}>
         <section className={homeStyles.homeMain__bookListSection}>
           <h2 className={homeStyles.homeMain__bookListSection__title}>Libros disponibles</h2>
-          <ul>
-            {/* {books.map((book) => {
-              return (
-                <li>
-                  <img src={book.cover} alt='' width='120' height='180' />
-                </li>
-              )
-            })} */}
-          </ul>
+          <BookList books={currentListBooksAvailable} listType='available' />
         </section>
         <section className={homeStyles.homeMain__bookListSection}>
           <h2 className={homeStyles.homeMain__bookListSection__title}>Lista de lectura</h2>
+          <ul>
+            <BookList books={currentReadingList} listType='reading' />
+          </ul>
         </section>
       </article>
     </>
