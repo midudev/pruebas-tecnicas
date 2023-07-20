92e721fff898cd30d70d7d7e95c5fff4e895276a
Yeray Alonso <yeray@steema.com>
2023-07-20 10:27:13 +0200

    Pass IsSelected to fix icon in selected list

--- pruebas/01-reading-list/YerayAlonso/reading-list/src/lib/components/BooksList.svelte
+++ pruebas/01-reading-list/YerayAlonso/reading-list/src/lib/components/BooksList.svelte
@@ -3,13 +3,14 @@
 
   export let books
   export let action
+  export let isSelected
 </script>
 
 <ul class="grid grid-cols-2 md:grid-cols-4 gap-3">
   {#each books as { book }}
     <li class="hover:scale-105 inline-block transition-all hover:contrast-125 hover:shadow-2xl">
       <button class="group relative" on:click={() => action(book)}>
-        <Book {book} isSelected={false} />
+        <Book {book} {isSelected} />
       </button>
     </li>
   {/each}
--- pruebas/01-reading-list/YerayAlonso/reading-list/src/routes/+page.svelte
+++ pruebas/01-reading-list/YerayAlonso/reading-list/src/routes/+page.svelte
@@ -19,11 +19,11 @@
 <div class="grid grid-cols-3 gap-10">
   <section class="col-span-2">
     <h2 class="text-2xl pb-4">Libros disponibles</h2>
-    <BooksList books={$library.filter((b) => !$selectedBooks.some((sb) => sb.book === b.book))} action={addBook} />
+    <BooksList books={$library.filter((b) => !$selectedBooks.some((sb) => sb.book === b.book))} action={addBook} isSelected={false} />
   </section>
 
   <section>
     <h2 class="text-2xl pb-4">Libros seleccionados</h2>
-    <BooksList books={$selectedBooks} action={removeBook} />
+    <BooksList books={$selectedBooks} action={removeBook} isSelected={true} />
   </section>
 </div>
