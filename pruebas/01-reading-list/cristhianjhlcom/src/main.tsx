b9ffd2e73d88daf356aee6d19f6aa8e712e85c8b
cristhianjhlcom <cristhianjhl.web@gmail.com>
2023-07-19 21:30:44 -0500

    add import to reset css file

--- pruebas/01-reading-list/cristhianjhlcom/src/main.tsx
+++ pruebas/01-reading-list/cristhianjhlcom/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/styles/index.css";
+import "./assets/styles/reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
