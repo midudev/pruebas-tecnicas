23452028a4cfc55bb9320482bb72b3809d826a50
jordislpm <jordislpm@gmail.com>
2023-07-19 20:24:48 -0400

    feat: validate number of books for plural text

--- pruebas/01-reading-list/jordislpm/src/components/Header/index.jsx
+++ pruebas/01-reading-list/jordislpm/src/components/Header/index.jsx
@@ -15,12 +15,12 @@ const [books, read, setFilters]=useFilter()
   return (
     <div className={styles.header}>
            <h1>Book Lover</h1>
-        <p className={styles.availables}>{`Tienes ${books.length} libros disponibles`}</p>
+        <p className={styles.availables}>{`Tienes ${books.length} libro${books.length != 1 ? "s" : ""} disponible${books.length != 1 ? "s" : ""}`}</p>
         {read.length == 0
         ? 
         <p className={styles.read}>{`Agrega algunos libros en tu lista de lectura`}</p>
       :
-      <p className={styles.read}>{`Tienes ${read.length} libros en tu lista de lectura`}</p>
+      <p className={styles.read}>{`Tienes ${read.length} libro${read.length != 1 ? "s" : ""} en tu lista de lectura`}</p>
       }
         <Form/> 
     </div>
