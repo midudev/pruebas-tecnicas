60adfebbfb3f9c759dd116f6af9de52d449831af
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-07-19 20:58:29 -0400

    Create dashboard

--- pruebas/01-reading-list/tonysantana1492/src/App.tsx
+++ pruebas/01-reading-list/tonysantana1492/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+import { Books, Favorites, Loading, SideMenu } from './components'
+import { useBook, useLocalStore } from './hooks'
+import { useMenu } from './store'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const { favorites, getBooks, updateFavorites: setFavoritesFromLocalStore, loading, setLoading } = useBook()
+  const { getFromLocalStorage } = useLocalStore()
+  const { isOpen } = useMenu()
+
+  useEffect(() => {
+    setLoading(true)
+    getBooks()
+      .then(() => {
+        const favorites = getFromLocalStorage()
+        setFavoritesFromLocalStore({ updatedFavorites: favorites })
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [getBooks, getFromLocalStorage, setFavoritesFromLocalStore, setLoading])
+
+  if (loading) {
+    return <Loading />
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+		<main className="flex flex-row pt-4 h-screen">
+			<Books />
+			<section className="hidden lg:flex">{favorites.length > 0 && <Favorites />}</section>
+			{isOpen && <SideMenu />}
+		</main>
   )
 }
-
-export default App
