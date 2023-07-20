f9f80262349c37f91924b1a0ab35cf5fde10e570
Daniel Bueno <danielfelipe96@hotmail.com>
2023-07-20 02:26:13 -0500

    REFACTOR: Move books between lists

--- pruebas/01-reading-list/dbueno96/reading-list/app/page.tsx
+++ pruebas/01-reading-list/dbueno96/reading-list/app/page.tsx
@@ -5,14 +5,17 @@ import { BOOK_SECTION_TITLE, READING_LIST_SECTION_TITLE } from './constants.text
 import React from 'react'
 import { useInitializeBookList } from './hooks/useInitializeBookList'
 import { useBookStore } from './store/bookStore'
+import { useMappedBooks } from './hooks/useMappedBooks'
 
 export default function MainPage () {
   useInitializeBookList()
-  const { bookList: books } = useBookStore()
+  const { bookList: books, moveBookToReadingList, removeBookFromReadingList } = useBookStore()
+  const readingListBooks = useMappedBooks(books)
+
   return (
     <main className='bg-gray-800 text-white text-4xl h-screen flex justify-between gap-0 overflow-hidden'>
-      <BookSection className='flex-grow' title={BOOK_SECTION_TITLE} books={books} />
-      <BookSection title={READING_LIST_SECTION_TITLE} books={[]} />
+      <BookSection className='flex-grow' title={BOOK_SECTION_TITLE} books={books} onClick={moveBookToReadingList} />
+      <BookSection title={READING_LIST_SECTION_TITLE} books={readingListBooks} onClick={removeBookFromReadingList} />
     </main>
   )
 }
