896d9a930332fc4c326f04f6aace5d9e14dd4c66
jose-gzs <44565257+jose-gzs@users.noreply.github.com>
2023-07-20 18:09:41 +0200

    mobile fix

--- pruebas/01-reading-list/jose-gzs/src/components/ReadingListBookItem.vue
+++ pruebas/01-reading-list/jose-gzs/src/components/ReadingListBookItem.vue
@@ -17,7 +17,7 @@ onMounted(() => {
 
 <template>
   <article class="relative rounded-md overflow-hidden transition cursor-pointer hover:-translate-y-1 hover:shadow-lg">
-    <img :src="book.cover" class="h-full"/>
+    <img :src="book.cover" class="w-full h-full"/>
     <div class="absolute flex justify-center p-4 items-end top-0 left-0 h-full transition opacity-0 hover:opacity-100 w-full bg-gradient-to-b from-transparent to-black/90">
       <h4 class="font-semibold text-white text-center text-lg">{{ book.title }}</h4>
     </div>
