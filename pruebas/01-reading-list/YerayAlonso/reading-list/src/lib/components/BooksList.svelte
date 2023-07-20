0dc2882fa142d1a65283d214d1b8cf0d98c89da5
Yeray Alonso <yeray@steema.com>
2023-07-20 09:18:45 +0200

    Refactor to components and add stores

---
+++ pruebas/01-reading-list/YerayAlonso/reading-list/src/lib/components/BooksList.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Book from '$lib/components/Book.svelte'
+
+  export let books
+  export let action
+</script>
+
+<ul class="grid grid-cols-2 md:grid-cols-4 gap-3">
+  {#each books as { book }}
+    <li class="hover:scale-105 inline-block transition-all hover:contrast-125 hover:shadow-2xl">
+      <button class="group relative" on:click={() => action(book)}>
+        <Book {book} isSelected={false} />
+      </button>
+    </li>
+  {/each}
+</ul>
---
+++ pruebas/01-reading-list/YerayAlonso/reading-list/src/lib/utils/stores.js
@@ -0,0 +1,4 @@
+import { writable } from 'svelte/store'
+
+export const library = writable([])
+export const selectedBooks = writable([])
