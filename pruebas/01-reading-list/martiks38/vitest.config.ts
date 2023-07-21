15faafd74f69e8bfde428fdf3436a5bb09a7a56b
Martiks38 <cappa.giuliano@gmail.com>
2023-07-21 18:04:22 -0300

    test: add dependencies for tests

---
+++ pruebas/01-reading-list/martiks38/vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html']
+    }
+  }
+})
