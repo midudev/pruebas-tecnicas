f565d685cba611d2b19a8bde53b5c344f6a7dde9
LuisMFG <luismiguelfloresgr@gmail.com>
2023-07-21 20:17:31 +0200

    icon readingList

--- pruebas/01-reading-list/LuisMFG1992/src/App.jsx
+++ pruebas/01-reading-list/LuisMFG1992/src/App.jsx
@@ -25,8 +25,7 @@ function App() {
   return (
     <div className="w-full  bg-[#202124] flex-col justify-center items-center min-h-screen">
       <Navbar />
-      <div className="w-full px-5 pt-10 sm:px-0 sm:w-[90%] max-w-[2000px] min-h-screen flex items-center flex-col">
-        <h1 className="text-3xl text-center">Book Tracker</h1>
+      <div className="w-full px-5 pt-10 sm:px-0 max-w-[2000px] min-h-screen flex items-center flex-col">
         <p>Libros disponibles: 10</p>
         <p>Lista de lectura: 3</p>
         <div className="w-full flex gap-8 p-4 items-center justify-evenly flex-wrap sm:flex-row sm:justify-evenly">
--- pruebas/01-reading-list/LuisMFG1992/src/Components/Navbar.jsx
+++ pruebas/01-reading-list/LuisMFG1992/src/Components/Navbar.jsx
@@ -1,13 +1,23 @@
 import logo from '../assets/logo.png'
+import FavList from '../assets/FavList.png'
+import SearchBar from './SearchBar'
+
+import { IoIosSearch } from 'react-icons/io'
 
 const Navbar = () => {
   return (
-    <div className="inline-flex justify-between w-full p-5">
-      <img src={logo} className="w-[150px]" />
+    <div className="inline-flex justify-between items-center w-full p-5">
+      <img src={logo} className="w-[130px] h-[40px]" />
 
-      <div className="inline-flex justify-between gap-10">
-        <div className="">Buscar</div>
-        <div className="">Icono</div>
+      <div className="inline-flex justify-between items-center gap-4">
+        <SearchBar />
+        <div>
+          <IoIosSearch size={'2.5rem'} className="sm:hidden relative" />
+          <div className="absolute right-2 top-12 bg-white border-2 border-purple-900 text-purple-900 w-[28px] font-bold rounded-full flex justify-center">
+            1
+          </div>
+        </div>
+        <img src={FavList} className="w-[35px] p-1" />
       </div>
     </div>
   )
