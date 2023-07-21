4db97bc8dcfb85fe4f6c1f715f5211929330cbda
davide.g <davide.garrido@fastcomcorp.cl>
2023-07-21 14:05:14 -0400

    Create BookList component for rendering books

--- pruebas/01-reading-list/rozzu1/src/pages/Books.jsx
+++ pruebas/01-reading-list/rozzu1/src/pages/Books.jsx
@@ -1,8 +1,8 @@
-import { useEffect } from "react";
-import { Card } from "../components/Card";
+import { useEffect, useMemo } from "react";
 import { getBooksByFilter } from "../helpers/getBooksByFilter";
 import { bookStore } from "../state/bookStore";
 import { Filters } from "../components/Filters";
+import { BookList } from "../components/BookList";
 
 export const Books = () => {
   const { books, fetchBooks, readingList, selectedGenre, searchBook } =
@@ -12,30 +12,15 @@ export const Books = () => {
     fetchBooks();
   }, [fetchBooks]);
 
-  const isBookInReadingList = (ISBN) => {
-    return readingList.some((b) => b.book.ISBN === ISBN);
-  };
-
-  const filteredBooks = getBooksByFilter(books, selectedGenre, searchBook);
+  const filteredBooks = useMemo(
+    () => getBooksByFilter(books, selectedGenre, searchBook),
+    [books, selectedGenre, searchBook]
+  );
 
   return (
     <div className="flex flex-col m-10 gap gap-5">
       <Filters />
-
-      <div className="mb-10 grid gap-5 grid-cols-1 xl:grid-cols-4">
-        {filteredBooks.map(({ book }) => (
-          <Card
-            key={book.ISBN}
-            inReadingList={isBookInReadingList(book.ISBN)}
-            title={book.title}
-            pages={book.pages}
-            genre={book.genre}
-            synopsis={book.synopsis}
-            cover={book.cover}
-            ISBN={book.ISBN}
-          />
-        ))}
-      </div>
+      <BookList books={filteredBooks} readingList={readingList} />
     </div>
   );
 };
