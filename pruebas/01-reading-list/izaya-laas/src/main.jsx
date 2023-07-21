eb402468a6eea6a5aa0c3b437539611b7ee09e83
Lautaro Asat <lautaronorielasat@gmail.com>
2023-07-21 10:15:04 -0300

    Add sincronize data on diferents pages

--- pruebas/01-reading-list/izaya-laas/src/main.jsx
+++ pruebas/01-reading-list/izaya-laas/src/main.jsx
@@ -1,5 +1,14 @@
 import { render } from 'preact';
 import { App } from './app.jsx';
 import './index.css';
+import { myReadingListISBN } from './signals/store.js';
 
 render(<App />, document.getElementById('app'));
+
+//Mantenemos sincronización de datos entre diferentes pestañas
+window.addEventListener('storage', (e) => {
+	if (e.key === 'readingList') {
+		const newValue = e.newValue;
+		myReadingListISBN.value = JSON.parse(newValue);
+	}
+});
