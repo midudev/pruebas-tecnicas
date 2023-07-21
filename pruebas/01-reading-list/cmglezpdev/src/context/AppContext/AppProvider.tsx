27f2877c9b870e52889c93b8a1bd5a3da5cfd9c7
Carlos Manuel González <cmglezpdev@gmail.com>
2023-07-21 01:41:27 -0400

    feat: add syncronize between tabs

--- pruebas/01-reading-list/cmglezpdev/src/context/AppContext/AppProvider.tsx
+++ pruebas/01-reading-list/cmglezpdev/src/context/AppContext/AppProvider.tsx
@@ -22,12 +22,26 @@ export const AppProvider:FC<IAppProvider> = ({ children }) => {
     const [appState, dispatch] = useReducer(appReducer, INITIAL_STATE);
     const firstRef = useRef(true);
 
-    useEffect(() => {
+    const setData = () => {
         const allBooks: Book[] = BOOKS_JSON.library.map(library => library.book);
         const reading: Book[] = JSON.parse(localStorage.getItem("reading-books") || "[]");
         const available: Book[] = allBooks.filter(book => !reading.some(rd => rd.ISBN === book.ISBN));
-
+        
         dispatch({ type: "set available/reading books", payload: { available, reading } });
+    }
+
+    document.onvisibilitychange = () => {
+        console.log("✅ Visibility Change");
+        setData();
+    }
+    
+    window.onfocus = () => {
+        console.log("✅ Focused");
+        setData();
+    }
+
+    useEffect(() => {
+        setData();
     }, []);
 
     useEffect(() => {
