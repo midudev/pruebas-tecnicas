4f87388daf49d50451a71650e2875d5f042a2652
garu2 <elgafo.nakama@gmail.com>
2023-07-21 16:19:51 -0400

    ADD: firs commit boilerplate

---
+++ pruebas/01-reading-list/garu2/lista-lectura/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <h1 className='text-4xl'>test project</h1>
+    </main>
+  )
+}
+
+export default App
