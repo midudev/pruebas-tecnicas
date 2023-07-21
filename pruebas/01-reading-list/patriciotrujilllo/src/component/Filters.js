563033683bf0c758aa0c986212e313e9b2f02239
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-21 19:19:59 -0400

    Create Component filters

---
+++ pruebas/01-reading-list/patriciotrujilllo/src/component/Filters.js
@@ -0,0 +1,67 @@
+import { useId,} from 'react'
+import { useFilters } from '../hook/useFilters'
+import { useListBook } from '../hook/useListBook'
+
+export const Filters = ({bookNoFiltered}) =>{
+    const idGenero = useId()
+    const idRange = useId()
+    const idSeacher = useId()
+    const {filters,setFilters} = useFilters()
+    const { read } = useListBook()
+
+    const notFiltered = bookNoFiltered.filter(item=> !read.find(book => book.ISBN === item.book.ISBN))
+
+    const amountGeneralList = (notFiltered.filter(item=> filters.genre=== 'all' || item.book.genre===filters.genre)).length
+    const AmountInListToRead = (read.filter(item=> filters.genre=== 'all' || item.genre===filters.genre)).length
+
+    const handleChange = (e) =>{
+
+        setFilters(prevState=>({
+            ...prevState,
+            genre: e.target.value
+        }))
+    }
+    const handleChangePages = (e) =>{
+        setFilters(prevState=>({
+            ...prevState,
+            pages: e.target.value
+        }))
+    }
+    const handleChangeSeach = (e) =>{
+        setFilters(prevState=>({
+            ...prevState,
+            title: e.target.value
+        }))
+    }
+
+    return (
+        <header className='filters'>
+            
+            <div>
+                <label htmlFor={idSeacher}></label>
+                <input id={idSeacher} type="text" onChange={handleChangeSeach}/>
+            </div>
+
+            <div>
+                <label htmlFor={idRange}>Paginado: {filters.pages}</label>
+                <input id={idRange} type="range" min='0' max='2000' onChange={handleChangePages} value={filters.pages}/>
+            </div>
+            
+            <div>
+                <label htmlFor={idGenero}>Genero</label>
+                <select id={idGenero} onChange={handleChange}>
+                <option value='all'>Todas</option>
+                <option value='Fantasía'>Fantasía</option>
+                <option value='Ciencia ficción'>Ciencia ficción</option>
+                <option value='Zombies'>Zombis</option>
+                <option value='Terror'>Terror</option>
+                </select>
+            </div>
+            
+            <div data-testid='class-container-h4'>
+                <h4>{amountGeneralList} libros disponibles</h4>
+                <h4>{AmountInListToRead} libros en lista lectura: </h4>
+            </div>
+        </header>
+    )
+}
