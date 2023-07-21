c2a69e3ef5cf7672367fd6ea7fc6da03a3870483
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-21 19:19:32 -0400

    Refactor filters

--- pruebas/01-reading-list/patriciotrujilllo/src/component/Header.js
+++ pruebas/01-reading-list/patriciotrujilllo/src/component/Header.js
@@ -1,68 +1,8 @@
-import { useId,} from 'react'
-import { useFilters } from '../hook/useFilters'
-import { useListBook } from '../hook/useListBook'
-
-export const Header = ({bookNoFiltered}) =>{
-    const idGenero = useId()
-    const idRange = useId()
-    const idSeacher = useId()
-    const {filters,setFilters} = useFilters()
-    const { read } = useListBook()
-
-    const notFiltered = bookNoFiltered.filter(item=> !read.find(book => book.ISBN === item.book.ISBN))
-
-    const amountGeneralList = (notFiltered.filter(item=> filters.genre=== 'all' || item.book.genre===filters.genre)).length
-    const AmountInListToRead = (read.filter(item=> filters.genre=== 'all' || item.genre===filters.genre)).length
-
-    const handleChange = (e) =>{
-
-        setFilters(prevState=>({
-            ...prevState,
-            genre: e.target.value
-        }))
-    }
-    const handleChangePages = (e) =>{
-        setFilters(prevState=>({
-            ...prevState,
-            pages: e.target.value
-        }))
-    }
-    const handleChangeSeach = (e) =>{
-        setFilters(prevState=>({
-            ...prevState,
-            title: e.target.value
-        }))
-    }
-
-    return (
-        <header>
-            <h1>Recomendación de libros</h1>
-            <fieldset className='header'>
-            <div>
-                <label htmlFor={idSeacher}></label>
-                <input id={idSeacher} type="text" onChange={handleChangeSeach}/>
-            </div>
-
-            <div>
-                <label htmlFor={idRange}>Paginado: {filters.pages}</label>
-                <input id={idRange} type="range" min='0' max='2000' onChange={handleChangePages} value={filters.pages}/>
-            </div>
-            
-            <div>
-                <label htmlFor={idGenero}>Genero</label>
-                <select id={idGenero} onChange={handleChange}>
-                <option value='all'>Todas</option>
-                <option value='Fantasía'>Fantasía</option>
-                <option value='Ciencia ficción'>Ciencia ficción</option>
-                <option value='Zombies'>Zombis</option>
-                <option value='Terror'>Terror</option>
-                </select>
-            </div>
-            </fieldset>
-            <div data-testid='class-container-h4'>
-                <h4>{amountGeneralList} libros disponibles</h4>
-                <h4>{AmountInListToRead} libros en lista lectura: </h4>
-            </div>
+export const Header = () =>{
+    return(
+        <header className="header">
+            <h1>Recomedaciones de libros</h1>
         </header>
+        
     )
 }
