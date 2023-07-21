99d76971ba72a0140e203b84c0d898326865e489
lftdev <torres.lf21@gmail.com>
2023-07-21 20:28:11 -0300

    Reform BookItem.jsx

--- pruebas/01-reading-list/lftdev/src/components/BookItem.jsx
+++ pruebas/01-reading-list/lftdev/src/components/BookItem.jsx
@@ -1,7 +1,4 @@
 export default function BookItem (props) {
   const { book, onClick } = props
-  function handleClick () {
-    onClick(book)
-  }
-  return <img className='hover:cursor-pointer' src={book.cover} alt={`${book.title} book cover`} onClick={handleClick} />
+  return <img className='hover:cursor-pointer' src={book.cover} alt={`${book.title} book cover`} onClick={() => onClick(book)} />
 }
