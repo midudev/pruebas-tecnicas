e3d1cb1a93af8a6ad17965f7f0db85701d7252a0
MIGUEL TORRES ALAMO <mta162@inlumine.ual.es>
2023-07-21 23:57:08 +0200

    feat: Book component

---
+++ pruebas/01-reading-list/mitoral/src/components/Book.jsx
@@ -0,0 +1,7 @@
+export function Book () {
+  return (
+    <div>
+      <img className='rounded-lg object-cover aspect-[1/1.45]' src='https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1566425108i/33.jpg' alt='Portada' />
+    </div>
+  )
+}
