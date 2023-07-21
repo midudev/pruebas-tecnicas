44fc893b5aac8ffe0fc5e851ebc4885c007bfd3f
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-21 19:21:44 -0400

    Remove styles of header

--- pruebas/01-reading-list/patriciotrujilllo/src/App.js
+++ pruebas/01-reading-list/patriciotrujilllo/src/App.js
@@ -4,9 +4,9 @@ import { Books } from './component/Books'
 import { Header } from './component/Header'
 import { useFilters } from './hook/useFilters'
 import './styles.css'
-import './header.css'
 import { ReadingProvider } from './context/readinglistContext.js'
 import { BookToRead } from './component/BookToRead'
+import { Filters } from './component/Filters'
 
 function App() {
 
@@ -23,11 +23,13 @@ function App() {
   
   return (
     <ReadingProvider>
-      <Header bookNoFiltered={books}/>
-      <div>
+      <div className='container'>
+        <Header />
+        <Filters bookNoFiltered={books}/>
         <BookToRead />
         <Books books={filteredbooks}/>
       </div>
+      
     </ReadingProvider>
     
     
