35587259f053639132ccc0b97481710a0a1ad1b1
TobysNap <naptobys17@gmail.com>
2023-07-21 14:25:14 -0300

    Maquetado básico reading list

---
+++ pruebas/01-reading-list/TobysNap/src/app/components/reading-list/reading-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, computed, inject } from '@angular/core';
+import { Book } from 'src/app/shared/interfaces/Book';
+import { BookService } from 'src/app/shared/services/book.service';
+import { ListService } from 'src/app/shared/services/list.service';
+
+@Component({
+  selector: 'app-reading-list',
+  template: `
+    <h2>Mi lista de lectura</h2>
+    <div *ngFor="let book of list()">
+      <h1>{{ book.title }}</h1>
+      <button (click)="remove(book)">Quitar</button>
+    </div>
+  `,
+  styles: [
+    `
+      
+    `
+  ]
+})
+export class ReadingListComponent {
+  bookService = inject(BookService);
+  listService = inject(ListService);
+  list = computed(() => this.listService.list());
+
+  remove(item: Book) {
+    this.listService.remove$.next(item);
+    this.bookService.add$.next(item);
+  }
+
+}
