90e338a724859848d3ced2fc0aea4a3b53254425
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-07-21 01:49:42 -0400

    Add tests config

--- pruebas/01-reading-list/tonysantana1492/vite.config.ts
+++ pruebas/01-reading-list/tonysantana1492/vite.config.ts
@@ -1,7 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "happy-dom"
+  },
 })
