2962c7784b8444cded3add447c2577f7b081666b
cristhianjhlcom <cristhianjhl.web@gmail.com>
2023-07-20 21:53:39 -0500

    Feature add to favorite reducer case

--- pruebas/01-reading-list/cristhianjhlcom/src/utils/reducer/BookReducer.ts
+++ pruebas/01-reading-list/cristhianjhlcom/src/utils/reducer/BookReducer.ts
@@ -3,7 +3,7 @@ import { BookElement } from "../../interfaces/Book";
 import { type BookAction, type InitialBookState } from "../../types";
 
 export function getInitialBookState(
-	library: Array<BookElement>
+	library: Array<BookElement>,
 ): InitialBookState {
 	return {
 		books: library,
@@ -17,9 +17,8 @@ export function getInitialBookState(
 
 export function booksReducer(
 	state: InitialBookState,
-	action: BookAction
+	action: BookAction,
 ): InitialBookState {
-	console.log({ state, action });
 	switch (action.type) {
 		case BookTypes.LOAD_BOOKS:
 			return { ...state };
@@ -28,6 +27,25 @@ export function booksReducer(
 				...state,
 				bookPreview: action.payload,
 			};
+		case BookTypes.ADD_TO_FAVORITE:
+			return {
+				...state,
+				books: state.books.filter((book) => {
+					return book.book.ISBN !== action.payload.ISBN;
+				}),
+				favorites: state.favorites.concat(action.payload),
+			};
+		case BookTypes.REMOVE_FROM_FAVORITES: {
+			console.log("payload", action.payload);
+			console.log("state", state);
+			return {
+				...state,
+				books: state.books.concat(action.payload),
+				favorites: state.favorites.filter((book) => {
+					return book.ISBN !== action.payload.book.ISBN;
+				}),
+			};
+		}
 		default:
 			throw new Error("Error: Invalid type");
 	}
