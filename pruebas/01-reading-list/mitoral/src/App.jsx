6d796026cbace0d1ef7903683ec3740d89d2e060
MIGUEL TORRES ALAMO <mta162@inlumine.ual.es>
2023-07-21 23:58:48 +0200

    fix: App component and styles fixes

--- pruebas/01-reading-list/mitoral/src/App.jsx
+++ pruebas/01-reading-list/mitoral/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import { BookList } from './components/BookList'
 import { Header } from './components/Header'
 import { Title } from './components/Title'
+import { Footer } from './components/Footer'
 
 export default function App () {
   return (
     <>
       <Header />
       <Title />
+      <BookList />
+      <Footer />
     </>
   )
 }
--- pruebas/01-reading-list/mitoral/src/components/Header.jsx
+++ pruebas/01-reading-list/mitoral/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header () {
   return (
     <>
-      <header className='w-full'>
+      <header className='w-full mt-6'>
         <a href='#' className='flex flex-row items-center justify-center gap-2'>
           <img src='/icon.svg' alt='' />
           <h1 className='text-2xl font-bold'>Midureads</h1>
