65b144d3d7d2a00c7b1554c1d7479bab1ba70581
blasferna <blasferna@gmail.com>
2023-07-21 15:30:45 -0400

    Add genre counter

--- pruebas/01-reading-list/blasferna/src/components/book-list.jsx
+++ pruebas/01-reading-list/blasferna/src/components/book-list.jsx
@@ -1,10 +1,20 @@
 "use client";
 
-import { useAppContext } from "@/context/store";
-import { addToReadingList, getInReadingList, removeFromReadingList } from "@/lib/books";
+import { useAppContext, useFilterContext } from "@/context/store";
+import {
+  addToReadingList,
+  getInReadingList,
+  removeFromReadingList,
+} from "@/lib/books";
 import { PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
+
+const GenreCounter = ({ count }) => (
+  <span className="text-sky-400 ml-2 text-xs">
+    {count} Libro{count > 1 && "s"}
+  </span>
+);
 
 export function AvailableList() {
   const { availableList } = useAppContext();
@@ -17,7 +27,9 @@ export function ReadingList() {
 }
 
 export function BookList({ books, removable = false }) {
-  const { inReadingList, setInReadingList } = useAppContext();
+  const { setInReadingList } = useAppContext();
+  const { genreFilter } = useFilterContext();
+
   const onRemoveClickHander = (isbn) => {
     removeFromReadingList(isbn);
     setInReadingList(getInReadingList());
@@ -26,11 +38,17 @@ export function BookList({ books, removable = false }) {
     addToReadingList(isbn);
     setInReadingList(getInReadingList());
   };
+
   return (
     <div className="pb-5">
       {Object.keys(books).map((row) => (
         <div key={row}>
-          <div className="font-medium mt-5 mb-2 text-white"> {row} </div>
+          <div className="flex items-center font-medium mt-5 mb-2 text-white">
+            {row}
+            {genreFilter && (
+              <GenreCounter count={books[row].length}></GenreCounter>
+            )}
+          </div>
           <div className="flex gap-3 flex-wrap">
             {books[row].map((book) => (
               <div
@@ -46,7 +64,7 @@ export function BookList({ books, removable = false }) {
                   >
                     <TrashIcon className="w-4 h-4 text-white/75"></TrashIcon>
                   </button>
-                ): (
+                ) : (
                   <button
                     onClick={() => {
                       onAddClickHander(book.ISBN);
@@ -54,7 +72,7 @@ export function BookList({ books, removable = false }) {
                     className="absolute right-2 top-2 text-xs p-1 bg-blue-500/75 hover:bg-blue-600 rounded-full z-10 hover:scale-110 ease-in duration-75"
                   >
                     <PlusIcon className="w-4 h-4 text-white"></PlusIcon>
-                  </button>                  
+                  </button>
                 )}
                 <Link href={`/books/${book.ISBN}`}>
                   <Image
