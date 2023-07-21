b423b915fab0abff0d30daeeb4259220603bb60e
davide.g <davide.garrido@fastcomcorp.cl>
2023-07-21 13:59:14 -0400

    Refactor BookList component to optimize book checking

--- pruebas/01-reading-list/rozzu1/src/components/BookList.jsx
+++ pruebas/01-reading-list/rozzu1/src/components/BookList.jsx
@@ -1,15 +1,13 @@
 import { Card } from "./Card";
 export const BookList = ({ books, readingList }) => {
-  const isBookInReadingList = (ISBN) => {
-    return readingList.some((b) => b.book.ISBN === ISBN);
-  };
+  const readingListSet = new Set(readingList.map((b) => b.book.ISBN));
 
   return (
-    <section className="grid gap-5 grid-cols-1 xl:grid-cols-4">
+    <div className="mb-10 grid gap-5 grid-cols-1 xl:grid-cols-4">
       {books.map(({ book }) => (
         <Card
           key={book.ISBN}
-          inReadingList={isBookInReadingList(book.ISBN)}
+          inReadingList={readingListSet.has(book.ISBN)}
           title={book.title}
           pages={book.pages}
           genre={book.genre}
@@ -18,6 +16,6 @@ export const BookList = ({ books, readingList }) => {
           ISBN={book.ISBN}
         />
       ))}
-    </section>
+    </div>
   );
 };
