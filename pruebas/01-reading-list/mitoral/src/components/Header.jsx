c0f73eff8283ffbc4773e854842d5a0e8070d9b8
MIGUEL TORRES ALAMO <mta162@inlumine.ual.es>
2023-07-21 19:58:24 +0200

    feat: header component

---
+++ pruebas/01-reading-list/mitoral/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export function Header () {
+  return (
+    <>
+      <header className='w-full'>
+        <a href='#' className='flex flex-row items-center justify-center gap-2'>
+          <img src='/icon.svg' alt='' />
+          <h1 className='text-2xl font-bold'>Midureads</h1>
+        </a>
+      </header>
+    </>
+  )
+}
