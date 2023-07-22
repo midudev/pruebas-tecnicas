0de580cfad018d6cc8bb978af73501c3e3004f79
Jared Muñoz <amunozbaez669@gmail.com>
2023-07-22 17:56:23 -0600

    modified: imports update

--- pruebas/01-reading-list/Jared-MB/src/components/index.ts
+++ pruebas/01-reading-list/Jared-MB/src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as AnimationGroup } from './AnimationGroup';
-export { default as AvailableBooks } from './AvailableBooks';
+export * from './Books';
 export * from './Filters';
-export { default as Image } from './Image';
 export * from './Info';
-export { default as ReadingListBooks } from './ReadingListBooks';
+export * from './Lists';
 export * from './index';
