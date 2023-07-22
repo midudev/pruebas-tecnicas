21abccb1b89818a9f200c97646c390d587b44dc7
Jared Muñoz <amunozbaez669@gmail.com>
2023-07-22 17:43:52 -0600

    modified: main UI distribution change

--- pruebas/01-reading-list/Jared-MB/src/App.tsx
+++ pruebas/01-reading-list/Jared-MB/src/App.tsx
@@ -1,32 +1,30 @@
 import { useEffect } from 'react'
 
 import { mapBooks } from './utilities'
+import { useBooks } from './store'
 
-import { BookFilters, Info, ReadingListBooks } from './components'
+import { BookFilters, Info, ReadingListBooks, AvailableListBooks } from './components'
 
 import Books from './constants/books.json'
-import { useBooks } from './store'
-import AvailableBooks from './components/AvailableBooks'
 
 function App() {
 
-  const { setAvailableBooks, availableBooks } = useBooks()
+  const { setAvailableBooks, readingListBooks, availableBooks } = useBooks()
 
   useEffect(() => {
-    availableBooks.length === 0 && setAvailableBooks(mapBooks(Books))
+    availableBooks.length === 0 && readingListBooks.length === 0 && setAvailableBooks(mapBooks(Books))
   }, [])
 
   return (
-    <main className='w-screen h-screen bg-zinc-900 flex relative z-0'>
-      <section className='basis-4/5 h-full p-4 flex gap-4 flex-col'>
-        <header className='h-12 flex items-center justify-between'>
-          <h1 className='text-purple-700 text-4xl'>{availableBooks.length} libros disponibles</h1>
-          <BookFilters />
-        </header>
-        <hr className='border border-zinc-300' />
-        <AvailableBooks />
-      </section>
-      <ReadingListBooks />
+    <main className='w-screen h-screen bg-zinc-900 grid grid-rows-[15%_1fr]'>
+      <header className='flex flex-col justify-evenly px-4 border-b border-b-zinc-300'>
+        <h1 className='text-4xl text-purple-700'>Libros</h1>
+        <BookFilters />
+      </header>
+      <div className='overflow-hidden grid grid-cols-[1fr_25%] h-full'>
+        <AvailableListBooks />
+        <ReadingListBooks />
+      </div>
       <Info />
     </main>
   )
