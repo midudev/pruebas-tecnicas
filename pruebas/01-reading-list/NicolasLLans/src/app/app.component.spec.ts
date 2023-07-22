baa6212d26043e7fe26ebbdc4eb7146e4ee08c2b
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-07-21 22:40:13 -0300

    style(componente main.ts- componente milista): mejora en detalles

--- pruebas/01-reading-list/NicolasLLans/src/app/app.component.spec.ts
+++ pruebas/01-reading-list/NicolasLLans/src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent
