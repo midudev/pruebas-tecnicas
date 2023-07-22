990a9a48470f6caa394dfcfb278aced2895770c5
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2023-07-22 18:41:16 +0200

    01 - Reading List: Created base layout.

--- pruebas/01-reading-list/iivvaannxx/src/routes/+layout.svelte
+++ pruebas/01-reading-list/iivvaannxx/src/routes/+layout.svelte
@@ -2,4 +2,14 @@
   import "../app.css";
 </script>
 
-<slot />
+<header class='grid-in-header bg-blue-200'>
+  My Books
+</header>
+
+<main class="grid-in-main bg-green-200">
+  <slot />
+</main>
+
+<aside class='grid-in-aside bg-red-200'>
+  Menu
+</aside>
--- pruebas/01-reading-list/iivvaannxx/tailwind.config.cjs
+++ pruebas/01-reading-list/iivvaannxx/tailwind.config.cjs
@@ -1,8 +1,37 @@
 import defaultConfig from 'tailwindcss/defaultConfig'
 import xtendConfig from 'xtendui/tailwind.preset'
 
+import gridAreasPlugin from '@savvywombat/tailwindcss-grid-areas'
+
 module.exports = {
 
   presets: [defaultConfig, xtendConfig],
-  content: ['./node_modules/xtendui/src/*.mjs', './src/**/*.{svelte,html,js,ts}']
+  content: ['./node_modules/xtendui/src/*.mjs', './src/**/*.{svelte,html,js,ts}'],
+
+  theme: {
+
+    extend: {
+
+      gridTemplateAreas: {
+
+        'base': [
+
+          'header header',
+          'aside  main',
+        ]
+      },
+
+      gridTemplateColumns: {
+
+        'base': '0.5fr 1.5fr'
+      },
+
+      gridTemplateRows: {
+
+        'base': '0.15fr 1.85fr'
+      }
+    }
+  },
+
+  plugins: [gridAreasPlugin]
 }
