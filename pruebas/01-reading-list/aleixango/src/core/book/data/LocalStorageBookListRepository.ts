e0302bb2c1154fa18c61d6844350605c89e99736
Aleix Anguita Gomez <aanguita@agrienergia.com>
2023-07-22 20:26:20 +0200

    Refactor BookListRepository implementation

--- pruebas/01-reading-list/aleixango/src/core/book/data/LocalStorageBookListRepository.ts
+++ pruebas/01-reading-list/aleixango/src/core/book/data/LocalStorageBookListRepository.ts
@@ -2,35 +2,36 @@ import { Book } from "../domain/Book"
 import { BookListRepository } from "../domain/BookListRepository"
 
 export class LocalStorageBookListRepository implements BookListRepository {
-  private bookListKey: string = "bookList"
+  private bookList: Book[] = []
+  private readonly bookListKey: string = "bookList"
+
+  constructor() {
+    this.bookList = JSON.parse(localStorage.getItem(this.bookListKey) ?? "[]") as Book[]
+  }
 
   get(): Promise<Book[]> {
     return new Promise((resolve) => {
-      const bookList: Book[] = JSON.parse(localStorage.getItem(this.bookListKey) ?? "[]")
-      resolve(bookList)
+      resolve(this.bookList)
     })
   }
 
   add(book: Book): Promise<boolean> {
     return new Promise((resolve) => {
-      this.get().then((books: Book[]) => {
-        books.push(book)
-        localStorage.setItem(this.bookListKey, JSON.stringify(books))
-        resolve(true)
-      })
+      this.bookList.push(book)
+      localStorage.setItem(this.bookListKey, JSON.stringify(this.bookList))
+      resolve(true)
     })
   }
 
   remove(isbn: string): Promise<boolean> {
     return new Promise((resolve) => {
-      this.get().then((books: Book[]) => {
-        const bookIndex = books.findIndex((book: Book) => book.ISBN === isbn)
-        if (bookIndex !== -1) {
-          books.splice(bookIndex, 1)
-          localStorage.setItem(this.bookListKey, JSON.stringify(books))
-        }
+      const bookIndex = this.bookList.findIndex((book: Book) => book.ISBN === isbn)
+      if (bookIndex !== -1) {
+        this.bookList.splice(bookIndex, 1)
+        localStorage.setItem(this.bookListKey, JSON.stringify(this.bookList))
         resolve(true)
-      })
+      }
+      resolve(false)
     })
   }
 }
