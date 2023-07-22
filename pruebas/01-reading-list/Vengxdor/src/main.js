82b12e796ae4c6643a2c5a0b7f584e0542070082
steven <Fernandezsteven185@gmail.com>
2023-07-22 08:59:33 -0500

    Problem removing books from list

--- pruebas/01-reading-list/Vengxdor/src/main.js
+++ pruebas/01-reading-list/Vengxdor/src/main.js
@@ -6,6 +6,7 @@ async function getData() {
         const library = data.library
         const ul = document.getElementById('library')
         const avaiableBooks = document.getElementById('avaiableBooks')
+        const readList = document.getElementById('readList')
     
 
         let booktitle
@@ -23,8 +24,8 @@ async function getData() {
                 <div class="p-4 flex flex-col justify-between h-full w-full div">
 
                         <picture class="h-full w-full group relative">
-                        <img class="h-full w-full object-fill " src="${each.book.cover}">
-                        <div class="text-white bg-black bg-opacity-60  invisible 
+                        <img  class="h-full w-full object-fill" src="${each.book.cover}">
+                        <div class="synopsis text-white bg-black bg-opacity-60  invisible 
                             group-hover:visible flex justify-center items-center flex-col absolute w-full h-full z-10 top-0">
                             <h2 class="mb-2 text-3xl font-semibold">Synopsis</h2>
                             <p class="text-center w-4/5 text-zinc-200">${each.book.synopsis}</p>
@@ -75,55 +76,96 @@ async function getData() {
         })
 
         avaiableBooks.innerHTML = totalBooks.length
-
-        // notification
-        /* ================Problem ====================== */
-        const addedNotification = () => {
-            const notification = document.querySelector('.notification')
-            const div = document.createElement('div')
-
-            div.className =
-        'absolute -top-12 w-fit p-4 bg-[#333] text-white  flex items-center gap-3 rounded-xl'
-            div.innerHTML = `
-                        <i class="fa-solid fa-check p-1 rounded-full bg-emerald-400 "></i>
-                        <h2 class="max-w-xs whitespace-nowrap">${booktitle} added </h2>
-                    `
-
-            notification.append(div)
-
-            div.classList.replace('-top-12', '-top-0')
-            div.className += ' opacity-100 visible'
-
-            setTimeout(() => {
-                div.classList.replace('-top-0', '-top-12')
-                div.className += ' opacity-0 invisible'
-            }, 1000)
-        }
-
-        //add to read list
+        
+        //add the book to the read list
         const AddReadList = () => {
-            const readList = document.getElementById('readList')
-            const addBtn = document.querySelectorAll('.addBtn')
+            const addBtns = document.querySelectorAll('.addBtn')
             const booksInread = document.querySelector('#booksInread')
             const booksToRead = []
 
-            addBtn.forEach((btn) => {
-                btn.addEventListener('click', () => {
-                    btn.textContent = 'Added!'
-                    btn.disabled = true
-                    const parent = btn.parentNode.parentNode.parentNode
-                    booksInread.textContent = booksToRead.length + 1
-                    const clone = parent.cloneNode(true)
-                    clone.classList.add('clone')
+            
 
+            addBtns.forEach((btn) => {
+                //Create a clone and pushed to the reading list
+                btn.addEventListener('click', () => {
+                    const parent = btn.parentNode.parentNode.parentNode
+                    const clone = parent.cloneNode(true)
+                    const cloneTittle = clone.children[0].children[1].children[0]
+
+                    btn.disabled = true
+                    btn.textContent = 'Added!'
+                    booksInread.textContent = booksToRead.length + 1
+                    
+                    clone.classList.add('clone')
                     booksToRead.push(clone)
                     readList.append(clone)
 
+                    // Create a notification when a book is added to the read list
+                    const addedNotification = () => {
+                        const notification = document.querySelector('.notification')
+                        const div = document.createElement('div')
+            
+                        div.className ='absolute -top-12 w-fit p-4 bg-[#333] text-white  flex items-center gap-3 rounded-xl'
+                        div.innerHTML = `
+                                    <i class="fa-solid fa-check p-1 rounded-full bg-emerald-400 "></i>
+                                    <h2 class="max-w-xs whitespace-nowrap">${cloneTittle.textContent} added </h2>
+                                `
+            
+                        notification.append(div)
+            
+                        div.classList.replace('-top-12', '-top-0')
+                        div.className += ' opacity-100 visible'
+            
+                        setTimeout(() => {
+                            div.classList.replace('-top-0', '-top-12')
+                            div.className += ' opacity-0 invisible'
+                        }, 1000)
+                    }
+
+                    
+
+                    
                     addedNotification()
+
+                    const RemoveReadList = () => {
+                        const removeBtn = document.querySelectorAll('.clone .removeBtn')
+                        removeBtn.forEach((Remove) => {
+                            
+                            if(Remove.EliminateBtn){
+                                Remove.removeEventListener('click', Remove.EliminateBtn)
+                            }
+
+                            const clickEvent = () =>{
+                                const CloneParent = Remove.parentNode.parentNode.parentNode
+                                readList.removeChild(CloneParent)
+                                booksToRead.pop(CloneParent)
+
+                                btn.disabled = false
+                                btn.textContent = 'Read List'
+                                booksInread.textContent = booksToRead.length 
+                                console.log(btn)
+                                console.log(booksToRead)
+                            }
+
+                            Remove.addEventListener('click', clickEvent)
+
+                            Remove.EliminateBtn = clickEvent
+                        })
+                        
+                    }
+                    RemoveReadList()
+
                 })
             })
+
+           
+            
         }
         AddReadList()
+
+
+        //remove the book from the read list
+        
     } catch (error) {
         console.error(`valiste, ya tienes otro error🤦${error}`)
     }
