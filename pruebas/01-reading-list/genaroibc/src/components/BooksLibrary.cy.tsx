2ba914a4307645ddf0883554a9680456121f67e0
genaroibc <bonavitaismael@gmail.com>
2023-07-23 20:53:49 -0300

    [TEST]: add tests for reading list

--- pruebas/01-reading-list/genaroibc/src/components/BooksLibrary.cy.tsx
+++ pruebas/01-reading-list/genaroibc/src/components/BooksLibrary.cy.tsx
@@ -53,4 +53,67 @@ describe("BooksLibrary", () => {
 
     cy.get("article div img").should("have.attr", "src", mockedBook.cover)
   })
+
+  it("adds book to reading list on click", () => {
+    cy.mount(
+      <BooksLibrary
+        initialBooks={[mockedBook]}
+        initialFilter={"year"}
+        initialGenre={"all"}
+        initialReadingList={[]}
+      />
+    )
+
+    cy.get("section button").click()
+
+    cy.get("aside ul li div p").should(
+      "not.exist",
+      "No books in your reading list"
+    )
+
+    cy.get("aside ul li button img").should(
+      "have.attr",
+      "src",
+      mockedBook.cover
+    )
+  })
+
+  it("removes book from reading list on click", () => {
+    const mockedBook: Book = {
+      title: "Juego de Tronos",
+      pages: 694,
+      genre: "Fantasía",
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1273763400i/8189620.jpg",
+      synopsis:
+        "En un reino donde las estaciones duran años, una batalla épica por el trono se desarrolla.",
+      year: 1996,
+      ISBN: "978-0553103540",
+      author: {
+        name: "George R. R. Martin",
+        otherBooks: [
+          "Choque de Reyes",
+          "Tormenta de Espadas",
+          "Festín de Cuervos"
+        ]
+      },
+      isInReadingList: true
+    }
+
+    cy.mount(
+      <BooksLibrary
+        initialBooks={[mockedBook]}
+        initialFilter={"year"}
+        initialGenre={"all"}
+        initialReadingList={[mockedBook]}
+      />
+    )
+
+    cy.get("aside ul li button").click()
+
+    cy.get("aside ul li div p").should(
+      "contain.text",
+      "No books in your reading list"
+    )
+  })
 })
