815149d701c32543cbb203f872e6ab313a0eb1f6
Fede Krenn <fedekrenn@gmail.com>
2023-07-23 18:12:00 -0300

    Se renderizan los primeros libros

--- pruebas/01-reading-list/fedekrenn/src/App.jsx
+++ pruebas/01-reading-list/fedekrenn/src/App.jsx
@@ -1,11 +1,34 @@
+import { useState, useEffect } from 'react'
+import getBooks from './utils/getBooks'
+// CSS
 import './App.css'
 
 function App() {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    getBooks().then((books) => {
+      setBooks(books.library)
+    })
+  }, [])
+
+  console.log(books)
 
   return (
-    <>
-      <h1>Reading list</h1>
-    </>
+    <section className='books-container'>
+      <h1>Listado de libros</h1>
+      <h2>{books.length} libros disponibles</h2>
+      <ul>
+        {books.map((book) => {
+          const { ISBN, cover, title } = book.book
+          return (
+            <li key={ISBN}>
+              <img className='cover-img' src={cover} alt={title} />
+            </li>
+          )
+        })}
+      </ul>
+    </section>
   )
 }
 
