3b456978c7e83651a07d967e5f857d66d520c640
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-07-23 16:10:30 -0400

    Create unit test for Header component

--- pruebas/01-reading-list/tonysantana1492/src/components/__test__/Header.test.tsx
+++ pruebas/01-reading-list/tonysantana1492/src/components/__test__/Header.test.tsx
@@ -2,29 +2,6 @@ import { render, screen } from '@testing-library/react'
 import { vi, describe, it, expect } from 'vitest'
 
 import { Header } from '../Header'
-import { type Book } from '../../types'
-
-const book: Book[] = [{
-  title: 'El Señor de los Anillos',
-  pages: 1200,
-  genre: 'Fantasía',
-  cover:
-          'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1566425108i/33.jpg',
-  synopsis:
-          'Una aventura épica en un mundo de fantasía llamado la Tierra Media.',
-  year: 1954,
-  ISBN: '978-0618640157',
-  author: {
-    name: 'J.R.R. Tolkien',
-    otherBooks: ['El Hobbit', 'El Silmarillion']
-  }
-}]
-
-vi.mock('../hooks/useBook', () => ({
-  useBook: vi.fn().mockReturnValue({
-    favorites: book
-  })
-}))
 
 const mockOnOpen = vi.fn()
 
@@ -38,17 +15,19 @@ describe('Header Component Test', () => {
   it('should render the header with correct title and badge value', () => {
     render(<Header />)
 
-    const titleElement = screen.getByText('Available Books')
+    const titleElement = screen.getAllByText(/^Available Books/i)
     expect(titleElement).toBeTruthy()
 
     const badgeElement = screen.findByText('1')
     expect(badgeElement).toBeTruthy()
   })
 
-  it('should call the onOpen function when button is clicked', async () => {
+  it('should call the onOpen function when button is clicked', () => {
     render(<Header />)
-    const buttonElement = await screen.findByRole('open-button')
+
+    const buttonElement = screen.getByRole('open-button')
     buttonElement.click()
+
     expect(mockOnOpen).toHaveBeenCalledTimes(1)
   })
 })
