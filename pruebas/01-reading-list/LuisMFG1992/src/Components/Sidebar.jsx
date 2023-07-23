68fc3fd65ef182078bd43362eb70c0d0d4475476
LuisMFG <luismiguelfloresgr@gmail.com>
2023-07-23 21:16:53 +0200

    close sidebar onBlur with onClick

--- pruebas/01-reading-list/LuisMFG1992/src/Components/Sidebar.jsx
+++ pruebas/01-reading-list/LuisMFG1992/src/Components/Sidebar.jsx
@@ -11,7 +11,10 @@ const Sidebar = ({ handelSidabar, readingList }) => {
 
   return (
     <div className="inline-flex">
-      <div className="w-full min-h-[100vh] [z-index:100] backdrop-blur fixed top-0"></div>
+      <div
+        className="w-full min-h-[100vh] [z-index:100] backdrop-blur fixed top-0"
+        onClick={handelSidabar}
+      ></div>
       <div className="flex flex-col gap-4 w-full xs:w-[70%] [z-index:100] xs:min-w-[350px] sm:w-[50%] sm:min-w-[400px] lg:w-[50%] max-w-[600px] bg-gray-001 min-h-screen fixed top-0 right-0 p-4">
         <HiChevronDoubleRight size={'2rem'} onClick={handelSidabar} />
         <p className="text-center text-3xl font-semibold">Lista de lectura</p>
