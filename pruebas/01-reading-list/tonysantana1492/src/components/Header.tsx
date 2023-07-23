5ec6defbf175a3972641db5793b7ec82f023e336
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-07-23 16:07:15 -0400

    Add availablesBooks count to the header

--- pruebas/01-reading-list/tonysantana1492/src/components/Header.tsx
+++ pruebas/01-reading-list/tonysantana1492/src/components/Header.tsx
@@ -7,7 +7,7 @@ import { PageFilter } from './PageFilter'
 import { TitleFilter } from './TitleFilter'
 
 export const Header = () => {
-  const { maxPages, favorites } = useBook()
+  const { maxPages, favorites, availablesBooks } = useBook()
   const { pageFilter, updatePageFilter, updateTitleFilter } = useFilter()
   const { onOpen } = useMenu()
 
@@ -15,7 +15,7 @@ export const Header = () => {
 		<header className="flex flex-col gap-4 w-full">
 			<div className="flex flex-col lg:flex-row gap-4 justify-between items-start w-full">
 				<div className="flex justify-between items-center w-full">
-					<h2 className="font-bold text-2xl text-left">Available Books</h2>
+					<h2 className="font-bold text-2xl text-left">Available Books {availablesBooks.length}</h2>
 					<button role='open-button' className="relative flex lg:hidden" onClick={onOpen}>
 						<OpenBook />
 						<Badge className='top-[-10px] right-[-10px] bg-red-500 rounded-full text-white w-5 h-5' value={favorites.length} />
