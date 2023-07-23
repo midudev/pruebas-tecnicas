e9562be7cf5a9b359adf3a1848ee39f8e33713a3
MIGUEL TORRES ALAMO <mta162@inlumine.ual.es>
2023-07-23 23:38:43 +0200

    feat: Badge component

---
+++ pruebas/01-reading-list/mitoral/src/components/Badge.jsx
@@ -0,0 +1,7 @@
+export function Badge ({ children }) {
+  return (
+    <span className='px-3 pb-1 pt-0.5 rounded-full text-sm font-normal bg-[#0099FF] text-white'>
+      {children}
+    </span>
+  )
+}
