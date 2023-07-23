febcfa75be93c8fc7db51ac64904a5b3f905e512
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-07-23 16:17:48 -0300

    add test

--- pruebas/01-reading-list/EmiLopez23/src/App.test.js
+++ pruebas/01-reading-list/EmiLopez23/src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render,screen} from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom/extend-expect';
+import { AvailableBooksProvider } from './context/AvailableBooksContext';
+import { ReadingListProvider } from './context/ReadingListContext';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders BooksToRead when "Reading List" button is clicked', () => {
+  render( <AvailableBooksProvider>
+  <ReadingListProvider>
+    <App />
+  </ReadingListProvider>
+</AvailableBooksProvider>);
+  //Verifico que el unico elemento con el texto reading list es el boton
+  expect(screen.getAllByText('Reading List',{exact:true}).length).toBe(1);
+
+  fireEvent.click(screen.getByText('Reading List',{exact:true}));
+
+  // Verifico que ahora los elementos con el texto 'Reading List' son dos, el boton y el h1 del componente <BooksToRead>
+  expect(screen.getAllByText('Reading List',{exact:true}).length).toBe(2);
 });
+
