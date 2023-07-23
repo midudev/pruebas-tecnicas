1355a352572080cf5284a6431dcc6714f8287013
Ronaldo Jara <sergiojara0609@gmail.com>
2023-07-23 17:12:26 -0500

    feat: add chip component

---
+++ pruebas/01-reading-list/sergio-jc/src/components/chip.jsx
@@ -0,0 +1,7 @@
+export function Chip ({ value }) {
+  return (
+    <span className='bg-rose-300 px-2 min-w-min rounded-xl grid place-content-center text-white font-bold'>
+      {value}
+    </span>
+  )
+}
