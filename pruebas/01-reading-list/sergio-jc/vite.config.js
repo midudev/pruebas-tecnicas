ab72a19cd253abc46f3383eafacda5d96d519bb7
Ronaldo Jara <sergiojara0609@gmail.com>
2023-07-23 17:08:53 -0500

    build: add import alias

--- pruebas/01-reading-list/sergio-jc/vite.config.js
+++ pruebas/01-reading-list/sergio-jc/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // '@assets': '/src/assets',
+      // '@pages': '/src/pages',
+      '@hooks': '/src/hooks',
+      // '@services': '/src/services',
+      // '@routes': '/src/routes',
+      '@context': '/src/context',
+      // '@constants': '/src/constants',
+      '@components': '/src/components',
+      '@utils': '/src/utils',
+      '@': '/src'
+    }
+  }
 })
