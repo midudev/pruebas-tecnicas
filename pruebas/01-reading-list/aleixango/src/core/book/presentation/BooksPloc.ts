a90b5f3ddbc60159cf11efd4c764469086ed0af0
Aleix Anguita Gomez <aanguita@agrienergia.com>
2023-07-23 12:17:50 +0200

    Add method for removing book and minor refactor

--- pruebas/01-reading-list/aleixango/src/core/book/presentation/BooksPloc.ts
+++ pruebas/01-reading-list/aleixango/src/core/book/presentation/BooksPloc.ts
@@ -1,10 +1,14 @@
 import { Ploc } from "../../common/presentation/Ploc"
 import { Book } from "../domain/Book"
 import { GetBooks } from "../domain/use_cases/GetBooks"
+import { RemoveBook } from "../domain/use_cases/RemoveBook"
 import { BooksState, booksInitialState } from "./BooksState"
 
 export class BooksPloc extends Ploc<BooksState> {
-  constructor(private getBooks: GetBooks) {
+  constructor(
+    private readonly getBooks: GetBooks,
+    private readonly removeBook: RemoveBook,
+  ) {
     super(booksInitialState)
   }
 
@@ -18,7 +22,13 @@ export class BooksPloc extends Ploc<BooksState> {
 
   async search(filterGenre: string): Promise<void> {
     try {
+      this.changeState({
+        kind: "LoadingBooksState",
+        filterGenre,
+      })
+
       const books: Book[] = await this.getBooks.execute(filterGenre)
+
       this.changeState({
         kind: "LoadedBooksState",
         books,
@@ -29,6 +39,25 @@ export class BooksPloc extends Ploc<BooksState> {
         kind: "ErrorBooksState",
         error: "Ha ocurrido un error. Por favor, prueba más tarde",
         filterGenre,
+      })
+    }
+  }
+
+  async remove(isbn: string): Promise<void> {
+    try {
+      await this.removeBook.execute(isbn)
+      const books: Book[] = await this.getBooks.execute(this.state.filterGenre)
+
+      this.changeState({
+        kind: "LoadedBooksState",
+        books,
+        filterGenre: this.state.filterGenre,
+      })
+    } catch (error) {
+      this.changeState({
+        kind: "ErrorBooksState",
+        error: "Ha ocurrido un error. Por favor, prueba más tarde",
+        filterGenre: "",
       })
     }
   }
