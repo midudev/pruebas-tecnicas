1f2025cda540e7667210af0cf4f85e0690851809
Kallox <d34d.mc@gmail.com>
2023-07-23 20:25:19 -0300

    Book details

--- pruebas/01-reading-list/Kallox/src/components/Book.tsx
+++ pruebas/01-reading-list/Kallox/src/components/Book.tsx
@@ -19,9 +19,25 @@ interface BookProps{
 }
 
 const Book: React.FC<BookProps> = ({book}) => {
+    const onClick = () => {
+        console.log("Clickeado")
+    }
     return(
-        <div className='rounded overflow-hidden shadow border-2'>
-            <img className='w-64 h-96' src={book.cover} alt={book.title} />
+        <div className='rounded overflow-hidden shadow border-2 relative group'>
+            <img className='w-64 h-96 group-hover:opacity-20' src={book.cover} alt={book.title} />
+            <div className='opacity-0 group-hover:opacity-100 duration-700 absolute inset-x-0 top-0 h-full block'>
+                <div className='p-4 justify-center items-center text-x text-white font-semibold h-5/6'>
+                    <h1 className='uppercase text-xl mb-4 underline text-center'>{book.title}</h1>
+                    <p className='text-gray-100 mb-1'>{book.synopsis}</p>
+                    <p className='text-gray-100 mb-1'><span className='underline'>Páginas:</span> {book.pages}</p>
+                    <p className='text-gray-100 mb-1'><span className='underline'>Año:</span> {book.year}</p>
+                    <p className='text-gray-100 mb-1'><span className='underline'>Autor:</span> {book.author.name}</p>
+                    <p className='text-gray-100 mb-1'><span className='underline'>Género:</span> {book.genre}</p>
+                </div>
+                <div className='items-center flex justify-center bg-slate-600 rounded-full mx-2 border-2 border-slate-500'>
+                    <button onClick={onClick} className='text-white text-lg font-bold w-full h-full'>Lista de lectura</button>
+                </div>
+            </div>
         </div>
     )
 }
