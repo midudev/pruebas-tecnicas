40d675182c54d25124f972e6c65706c8777ecfdd
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-07-23 16:41:10 -0300

    add styles for responsive page

--- pruebas/01-reading-list/EmiLopez23/src/App.js
+++ pruebas/01-reading-list/EmiLopez23/src/App.js
@@ -17,7 +17,7 @@ function App() {
       <main className="body">
         <h1>Available books: {availableBooks.length}</h1>
         <h3>Books in reading list: {booksToRead.length} </h3>
-        <div style={{display:'flex',justifyContent:'space-between',alignItems:'center',width:'100%'}}>
+        <div style={{display:'flex',justifyContent:'space-between',alignItems:'center',width:'100%', flexWrap:'wrap-reverse',gap:'10px'}}>
         <Filters />
         <button className='reading-list-btn' onClick={()=>setShowReadingList(!showReadingList)}>Reading List</button>
         </div>
