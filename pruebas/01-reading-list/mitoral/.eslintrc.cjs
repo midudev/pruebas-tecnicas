2b9a8635813bbabc85548561173304580302bc3d
MIGUEL TORRES ALAMO <mta162@inlumine.ual.es>
2023-07-23 23:37:18 +0200

    fix: disable prop-types eslint rule

--- pruebas/01-reading-list/mitoral/.eslintrc.cjs
+++ pruebas/01-reading-list/mitoral/.eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
