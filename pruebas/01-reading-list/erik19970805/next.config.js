dbbfb2857f555edbefbb74a33151cd59c84f4961
erik <erik19970805@gmail.com>
2023-07-22 19:00:54 -0500

    feat: se añadio la configuracion de github accion para subir la app de nextjs

--- pruebas/01-reading-list/erik19970805/next.config.js
+++ pruebas/01-reading-list/erik19970805/next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        hostname: "images-na.ssl-images-amazon.com",
-      },
-    ],
-  },
-};
+const nextConfig = { output: 'export' };
 
 module.exports = nextConfig;
