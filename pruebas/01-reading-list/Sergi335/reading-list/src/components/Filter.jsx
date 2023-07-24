a9bce8eff31f3159d0e2667c4022bf1ea73f5858
Sergio <sergiadn335@gmail.com>
2023-07-24 19:47:43 +0200

    fixed

--- pruebas/01-reading-list/Sergi335/reading-list/src/components/Filter.jsx
+++ pruebas/01-reading-list/Sergi335/reading-list/src/components/Filter.jsx
@@ -1,25 +1,37 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 export default function Filter ({ changeFilters }) {
   const [minPages, setMinPages] = useState(0)
+  const [key, setKey] = useState(1)
+  const [key2, setKey2] = useState(1)
+
+  // Usar referencias para obtener los elementos correctos
+  const labelPaginasRef = useRef(null)
+  const labelGeneroRef = useRef(null)
+
   const handleChangeMinPages = (event) => {
     setMinPages(event.target.value)
-    changeFilters(prevState => ({
+    changeFilters((prevState) => ({
       ...prevState,
       pages: event.target.value
     }))
-    const label = document.querySelector('.labelPaginas')
-    event.target.value > 0 ? label.classList.remove('hidden') : label.classList.add('hidden')
+    const label = labelPaginasRef.current
+    event.target.value > 0
+      ? label.classList.remove('hidden')
+      : label.classList.add('hidden')
   }
+
   const handleChangeGenre = (event) => {
-    changeFilters(prevState => ({
+    changeFilters((prevState) => ({
       ...prevState,
       genre: event.target.value
     }))
-    const label = document.querySelector('.labelGenero')
-    event.target.value !== 'all' ? label.classList.remove('hidden') : label.classList.add('hidden')
+    const label = labelGeneroRef.current
+    event.target.value !== 'all'
+      ? label.classList.remove('hidden')
+      : label.classList.add('hidden')
   }
-  const [key, setKey] = useState(1)
+
   const handleResetPages = (event) => {
     changeFilters((prevState) => ({
       ...prevState,
@@ -27,21 +39,38 @@ export default function Filter ({ changeFilters }) {
     }))
     setMinPages(0)
     setKey((prevKey) => prevKey + 1)
-    event.target.parentNode.classList.add('hidden')
+
+    // Usar la referencia para obtener el elemento correcto
+    labelPaginasRef.current.classList.add('hidden')
   }
-  const [key2, setKey2] = useState(1)
+
   const handleResetGenre = () => {
     changeFilters((prevState) => ({
       ...prevState,
       genre: 'all'
     }))
     setKey2((prevKey) => prevKey + 1)
-    event.target.parentNode.classList.add('hidden')
+
+    // Usar la referencia para obtener el elemento correcto
+    labelGeneroRef.current.classList.add('hidden')
   }
+
   return (
-        <div className="flex 2xl:gap-10 gap-2 my-0 border-slate-100 dark:border-[darkslategray] 2xl:border dark:2xl:border-0 py-[5px] 2xl:shadow-sm rounded px-[30px] dark:bg-[#100c18]">
-          <div onClick={handleResetPages} className='labelPaginas dark:bg-[#100c18] w-max top-[109px] p-[15px] cursor-pointer hidden absolute 2xl:right-[30%] 2xl:top-[91px] bg-blue-500 2xl:py-[17px] 2xl:px-[35px] rounded-md text-white'><p>Número de páginas X</p></div>
-                  <div onClick={handleResetGenre} className='labelGenero cursor-pointer w-max right-[32px] dark:bg-[#100c18] top-[109px] p-[15px] hidden absolute 2xl:right-[23%] 2xl:top-[91px] bg-blue-500 2xl:py-[17px] 2xl:px-[35px] rounded-md text-white'><p>Género X</p></div>
+    <div className="flex 2xl:gap-10 gap-2 my-0 border-slate-100 dark:border-[darkslategray] 2xl:border dark:2xl:border-0 py-[5px] 2xl:shadow-sm rounded px-[30px] dark:bg-[#100c18]">
+      <div
+        onClick={handleResetPages}
+        ref={labelPaginasRef} // Agregar la referencia al elemento
+        className="labelPaginas dark:bg-[#100c18] w-max top-[109px] p-[15px] cursor-pointer hidden absolute 2xl:right-[30%] 2xl:top-[91px] bg-blue-500 2xl:py-[17px] 2xl:px-[35px] rounded-md text-white"
+      >
+        <p>Número de páginas X</p>
+      </div>
+      <div
+        onClick={handleResetGenre}
+        ref={labelGeneroRef} // Agregar la referencia al elemento
+        className="labelGenero cursor-pointer w-max right-[32px] dark:bg-[#100c18] top-[109px] p-[15px] hidden absolute 2xl:right-[23%] 2xl:top-[91px] bg-blue-500 2xl:py-[17px] 2xl:px-[35px] rounded-md text-white"
+      >
+        <p>Género X</p>
+      </div>
             <div className='flex justify-center items-center gap-2 2xl:gap-8'>
                 <label htmlFor="pages">Páginas</label>
                 <input className='h-2 bg-gray-200 rounded-lg dark:bg-gray-700 w-[100%] 2xl:w-auto'
--- pruebas/01-reading-list/Sergi335/reading-list/src/components/Search.jsx
+++ pruebas/01-reading-list/Sergi335/reading-list/src/components/Search.jsx
@@ -7,8 +7,6 @@ export default function Search ({ books }) {
   const [searchResults, setSearchResults] = useState([])
   const [visible, setVisible] = useState(false)
 
-  console.log(searchResults)
-
   // la diferencia de libros que hay en la lista con los totales
   const filteredBooks = books.filter((item) => {
     return !list.some((item2) => item2.book.ISBN === item.book.ISBN)
@@ -19,7 +17,6 @@ export default function Search ({ books }) {
     const query = event.target.value.trim().toLowerCase()
     setQuery(query)
   }
-  console.log(query)
 
   const searchBooks = (query, filteredBooks) => {
     if (query === '') {
@@ -37,21 +34,20 @@ export default function Search ({ books }) {
     setSearchResults(searchBooks(query, filteredBooks))
   }, [query])
 
-  console.log(searchResults.length)
   const [key, setKey] = useState(1)
-  const cleanSearch = () => {
-    if (visible) {
+  const cleanSearch = (event) => {
+    if (visible && event.target.nodeName !== 'BUTTON') {
+      console.log(event.target)
       setVisible(false)
       setKey((prevKey) => prevKey + 1)
     }
   }
-  console.log(visible)
   return (
       <div className="search w-[25%] hidden 2xl:block">
         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400 absolute top-8 ml-[12px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
-        <input key={key} className='block w-full p-[8px] pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+        <input key={key} className='block w-full p-[8px] pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-0 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-[#100c18]'
           onChange={handleSearch}
           type="text"
           placeholder="Busca en el catálogo"
@@ -61,6 +57,7 @@ export default function Search ({ books }) {
             ? 'searchResults fixed bg-white w-screen left-0 top-[151px] h-screen overflow-y-scroll flex dark:bg-[#3f2e3e] pt-[80px]'
             : 'hidden' }>
             <ul className='flex flex-wrap justify-center gap-36 p-0 min-h-[100vh] w-[100%] 2xl:pl-[184px] 2xl:justify-start'>
+          <p className='cursor-pointer fixed top-[172px] left-[47%]'>Cerrar ❌</p>
           {/* Mostrar los resultados de la búsqueda */}
           {searchResults.length > 0
             ? (
