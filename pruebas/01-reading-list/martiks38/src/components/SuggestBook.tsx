de89fc904a8235d6c34860df812274f4cd539624
Martiks38 <cappa.giuliano@gmail.com>
2023-07-24 16:09:50 -0300

    feat: add tooltip to SuggestBook add book button

--- pruebas/01-reading-list/martiks38/src/components/SuggestBook.tsx
+++ pruebas/01-reading-list/martiks38/src/components/SuggestBook.tsx
@@ -10,8 +10,18 @@ import suggestBookStyles from '@/assets/styles/Book/SuggestBook.module.css'
 
 import type { Book } from '@/typings/books'
 
+const restartAnimation = (ev: React.MouseEvent<HTMLButtonElement>) => {
+  const btn = ev.currentTarget
+
+  btn.style.animationName = ''
+  setTimeout(() => {
+    btn.style.animationName = 'moveAddButton'
+  })
+}
+
 export function SuggestBook() {
   const [suggestion, setSuggestion] = useState<Book | null>(null)
+  const [isAdd, setIsAdd] = useState(false)
   const { addToReadingList } = useBookList()
 
   useEffect(() => {
@@ -32,18 +42,33 @@ export function SuggestBook() {
     }
   }, [])
 
+  const addBook = (ISBN: string) => {
+    if (isAdd) return
+
+    setIsAdd(true)
+    addToReadingList({ ISBN })
+  }
+
   return suggestion ? (
     <>
       <div className={suggestBookStyles.suggestSection__info}>
         <h4 className={suggestBookStyles.suggestSection__title}>{suggestion.title}</h4>
         <h5 className={suggestBookStyles.suggestSection__subtitle}>{suggestion.author.name}</h5>
         <p className={suggestBookStyles.suggestSection__synopsis}>{suggestion.synopsis}</p>
-        <button
-          className={suggestBookStyles.suggestSection__info__button}
-          onClick={() => addToReadingList({ ISBN: suggestion.ISBN })}
-        >
-          Añadir a la lista
-        </button>
+        <div className={suggestBookStyles.addBookContainer}>
+          <button
+            className={suggestBookStyles.suggestSection__info__button}
+            onClick={() => addBook(suggestion.ISBN)}
+            onMouseLeave={restartAnimation}
+            aria-disabled={isAdd}
+            aria-describedby='disabledReasonSuggestBook'
+          >
+            <span>Añadir a la lista</span>
+          </button>
+          <p className={suggestBookStyles.tooltip} id='disabledReasonSuggestBook' role='tooltip'>
+            Ya se encuentra en la lista
+          </p>
+        </div>
       </div>
       <Image src={suggestion.cover} alt={suggestion.title} width={145} height={217} priority />
     </>
