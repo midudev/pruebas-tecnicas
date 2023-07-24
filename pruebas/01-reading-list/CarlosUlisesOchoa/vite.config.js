eab0dbd37224dc134260408429288c318e1f31ee
Carlos Ochoa <uligibson@hotmail.com>
2023-07-24 13:29:44 -0600

    vite config modified

--- pruebas/01-reading-list/CarlosUlisesOchoa/vite.config.js
+++ pruebas/01-reading-list/CarlosUlisesOchoa/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import eslint from 'vite-plugin-eslint'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
