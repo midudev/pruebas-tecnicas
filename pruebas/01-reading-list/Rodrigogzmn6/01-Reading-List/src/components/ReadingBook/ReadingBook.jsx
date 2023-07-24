051bebbf38b2497ef5c70b1804c38fc32857ef4a
Rodrigo Guzman <rodrigo.guzman@sovos.com>
2023-07-24 12:10:53 -0300

    Fix: README file updated

--- pruebas/01-reading-list/Rodrigogzmn6/01-Reading-List/src/components/ReadingBook/ReadingBook.jsx
+++ pruebas/01-reading-list/Rodrigogzmn6/01-Reading-List/src/components/ReadingBook/ReadingBook.jsx
@@ -2,8 +2,11 @@ import Cross from "../../assets/icon-cross.svg";
 import "./ReadingBook.css";
 export const ReadingBook = ({ book, index, onClick }) => {
   return (
-    <div className="reading-book relative inline-block sm:w-1/4 lg:w-max lg:flex lg:flex-col lg:items-center ">
-      <img className="reading-book-cover rounded-lg sm:h-64" src={book.cover} />
+    <div className="reading-book relative inline-block sm:w-1/4 lg:w-full lg:flex lg:flex-col lg:items-center ">
+      <img
+        className="reading-book-cover rounded-lg sm:h-64 lg:w-full lg:h-auto"
+        src={book.cover}
+      />
       <img
         className="reading-book-button cursor-pointer"
         src={Cross}
