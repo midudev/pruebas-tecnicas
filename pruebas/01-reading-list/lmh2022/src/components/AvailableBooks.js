05d51dccd2841591719794e3bffbcda952432d0c
leo123 <familiahirsch@live.com.ar>
2023-07-24 16:10:41 -0300

    Starting with pages filter

--- pruebas/01-reading-list/lmh2022/src/components/AvailableBooks.js
+++ pruebas/01-reading-list/lmh2022/src/components/AvailableBooks.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import data from '.././books.json';
 import Selector from './Selector';
+import {useState, useEffect} from 'react';
+
+
 
 function AvailableBooks() {
 
+  const [selected, setSelected] = useState("")
   let books= data.library
-  console.log(books)
-  let filteredBooks= books.filter(f=>f.book.genre==="Fantasía")
+  const [filteredBooks, setFilteredBooks] = useState(books)
+  const [pages, setPages] = useState(3000)
 
+
+useEffect(
+  ()=>{
+    setFilteredBooks(books.filter(f=>f.book.genre===(selected===""?f.book.genre:selected)&&f.book.pages<=pages))
+
+  },[selected, pages]
+)
+
+
+
+
+
+  const handleChange = (event) => {
+    setPages(event.target.value)
+
+    console.log(pages)
+  }
+
+ 
   return (
-    <>
-        <Selector field="genre" record="book" object={books}/>
-      {filteredBooks.map(e=><div key={e.book.ISBN}>{e.book.title}</div>)}
+    <>Hay {filteredBooks.length} libros Disponibles <br/>
+          <input type="range" id="page" name="page" min="0" max="2000" onChange={handleChange} value={pages}></input>
+
+        <Selector field="genre" record="book" object={books} setSelected={setSelected} selected={selected}/>
+      {filteredBooks.map(e=><div key={e.book.ISBN}>{e.book.title} - {e.book.pages}</div>)}
 
     </>
   );
--- pruebas/01-reading-list/lmh2022/src/components/Selector.js
+++ pruebas/01-reading-list/lmh2022/src/components/Selector.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-function Selector({field, record, object}) {
+function Selector({field, record, object, setSelected, selected}) {
 
-  let options = [...new Set(object.map(e=>e[record][field]).sort((a,b)=>a<b?-1:a>b?1:0))]
+  let options = ["", ...new Set(object.map(e=>e[record][field]).sort((a,b)=>a<b?-1:a>b?1:0))]
 
-  console.log(options)
+  //console.log(options)
+  const handleChange = (event) => {
+    setSelected(event.target.value)
 
+  }
 
 
 
   return (
     <>
-      <select name="cars" id="cars">
-            {options.map(o=><option value="{o}" key={Math.random()}>{o}</option>)}
+
+      <select name="selector" id="selector" onChange={handleChange} value={selected}>
+            {options.map(o=><option value={o} key={Math.random()}>{o}</option>)}
     </select>
 
     </>
