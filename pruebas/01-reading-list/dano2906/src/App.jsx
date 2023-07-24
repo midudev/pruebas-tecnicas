63287a3e9a79ef3671d841f06c6af2c9cce044a7
Dano <dlg9806@gmail.com>
2023-07-24 10:35:51 -0400

    feat: Add footer

--- pruebas/01-reading-list/dano2906/src/App.jsx
+++ pruebas/01-reading-list/dano2906/src/App.jsx
@@ -6,13 +6,14 @@ import { useQuantity } from './hooks/useQuantity'
 import OpenTag from './components/OpenTag'
 import ResetFilters from './components/ResetFilters'
 import CleanReadList from './components/CleanReadList'
+import Footer from './components/Footer'
 
 function App () {
   const { books, quantityByGenre, filterPages, searchBooks, setFilterGenre, setFilterPages, setSearchBooks } = useBooks()
   const { quantityBooks } = useQuantity()
 
   return (
-    <div className='w-screen min-h-screen bg-slate-900 py-24 sm:py-10 flex flex-col gap-y-6 relative'>
+    <div className='w-screen min-h-screen bg-slate-900 pt-24 pb-8 sm:pt-10 flex flex-col gap-y-6 relative'>
       <h2 className='text-3xl text-center font-bold text-blue-300 px-2'>Cantidad de libros en la líbreria: {quantityBooks}</h2>
       <h3 className='text-xl text-center font-bold text-blue-300 px-2'>Cantidad de libros del género seleccionado: {quantityByGenre}</h3>
       <FiltersForm setFilterGenre={setFilterGenre} setFilterPages={setFilterPages} pages={filterPages} search={searchBooks} setSearchBooks={setSearchBooks} />
@@ -21,6 +22,7 @@ function App () {
       <OpenTag />
       <CleanReadList />
       <ResetFilters setFilterGenre={setFilterGenre} setFilterPages={setFilterPages} setSearchBooks={setSearchBooks} />
+      <Footer />
     </div>
   )
 }
---
+++ pruebas/01-reading-list/dano2906/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer () {
+  return (
+    <footer className='w-full h-auto flex items-center justify-center'>
+      <p className='text-center text-base text-blue-500 font-semibold'>Con ❤️ y moviendo las manitas <span className='text-lg text-blue-400 font-bold'>DanoDev</span></p>
+    </footer>
+  )
+}
