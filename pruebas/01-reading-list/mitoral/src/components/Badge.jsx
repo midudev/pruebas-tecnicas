3d85965ef5e23b314019c26f49a0d95f10f7f6a9
MIGUEL TORRES ALAMO <mta162@inlumine.ual.es>
2023-07-25 00:00:37 +0200

    fix: className prop in Badge component

--- pruebas/01-reading-list/mitoral/src/components/Badge.jsx
+++ pruebas/01-reading-list/mitoral/src/components/Badge.jsx
@@ -1,6 +1,6 @@
-export function Badge ({ children }) {
+export function Badge ({ className = '', children }) {
   return (
-    <span className='px-3 pb-1 pt-0.5 rounded-full text-sm font-normal bg-[#0099FF] text-white'>
+    <span className={`px-3 pb-1 pt-0.5 rounded-full text-sm font-normal bg-[#0099FF] text-white line-clamp-1 ${className}`}>
       {children}
     </span>
   )
