9479b4a8f391ec0632571d37920086b0b5ed37ac
MIGUEL TORRES ALAMO <mta162@inlumine.ual.es>
2023-07-25 00:05:05 +0200

    fix: BookList wrapper with ReadingListProvider and FiltersProvider

--- pruebas/01-reading-list/mitoral/src/App.jsx
+++ pruebas/01-reading-list/mitoral/src/App.jsx
@@ -3,13 +3,19 @@ import { BookList } from './components/BookList'
 import { Header } from './components/Header'
 import { Title } from './components/Title'
 import { Footer } from './components/Footer'
+import { FiltersProvider } from './context/FiltersContext'
+import { ReadingListProvider } from './context/ReadingListContext'
 
 export default function App () {
   return (
     <>
       <Header />
       <Title />
-      <BookList />
+      <ReadingListProvider>
+        <FiltersProvider>
+          <BookList />
+        </FiltersProvider>
+      </ReadingListProvider>
       <Footer />
     </>
   )
