5185363f043d2f3c8e96e3823799898219572227
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-25 17:38:09 -0300

    Display books data

--- pruebas/01-reading-list/nico-bt/src/App.jsx
+++ pruebas/01-reading-list/nico-bt/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./App.css"
+import useBooksData from "./hooks/useBooksData"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { books, isLoading, error } = useBooksData()
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>Reading List</h1>
+
+      <section>
+        <ul className="booklist-container">
+          {books?.map((item) => (
+            <li key={item.ISBN}>
+              <img src={item.book.cover} alt={`${item.book.title} cover`} />
+              {/* <h2>{item.book.title}</h2> */}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </main>
   )
 }
 
---
+++ pruebas/01-reading-list/nico-bt/src/hooks/useBooksData.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react"
+import data from "../../../books.json"
+
+export default function useBooksData() {
+  const [books, setBooks] = useState([])
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setBooks(data.library)
+  }, [])
+
+  return { books, error, isLoading }
+}
