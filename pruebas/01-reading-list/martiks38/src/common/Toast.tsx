51989896d3a651256fcfd837cd938920c8a3bb26
Martiks38 <cappa.giuliano@gmail.com>
2023-07-25 09:28:28 -0300

    chore: add cross-env and react-toastify dependencies

--- pruebas/01-reading-list/martiks38/src/common/Toast.tsx
+++
@@ -1,9 +0,0 @@
-import { createPortal } from 'react-dom'
-
-type ToastProps = {
-  message: string
-}
-
-export function Toast({ message }: ToastProps) {
-  return createPortal(<p>{message}</p>, document.getElementById('toast') as HTMLDivElement)
-}
