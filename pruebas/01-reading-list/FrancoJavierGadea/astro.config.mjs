68d225e8bce73084bd2ae38065e73d4e5735557a
1812 <francogadea1812@gmail.com>
2023-07-25 05:45:41 -0300

    primer deploy

--- pruebas/01-reading-list/FrancoJavierGadea/astro.config.mjs
+++ pruebas/01-reading-list/FrancoJavierGadea/astro.config.mjs
@@ -2,15 +2,18 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 import node from '@astrojs/node';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
 
     output: 'server',
     
-    adapter: node({
-      mode: 'standalone',
-    }),
+    // adapter: node({
+    //   mode: 'standalone',
+    // }),
+
+    adapter: netlify(),
 
     integrations: [react()]
 });
