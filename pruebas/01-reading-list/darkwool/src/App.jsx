debc43c1f2341df4db504ac060d207f7be1ad7db
DarkWool <cooper27@live.com.mx>
2023-07-25 16:34:50 -0600

    Init project using Vite and React
    style: format files using Prettier
    
    build: add Tailwind
    
    style: format code

---
+++ pruebas/01-reading-list/darkwool/src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <section className="app_container">
+      <header>
+        <h1 className="header_title">Tu Libreria</h1>
+        <hr className="header_separator" />
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna sem,
+          aliquam vel erat et, eleifend dapibus sem. Aliquam vehicula lectus eu urna
+          mattis, vel imperdiet arcu dignissim. Cras a suscipit felis. Integer et
+          sollicitudin nunc. Curabitur imperdiet justo egestas tortor consequat, non
+          tristique nisl dignissim. Fusce laoreet rutrum tellus vehicula tristique.
+          Maecenas tempus pretium quam, ac tempus libero egestas et. Ut in pharetra eros.
+          Proin vestibulum semper hendrerit. Proin finibus diam ut molestie sollicitudin.
+          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+          turpis egestas. Maecenas ac nibh vel tortor molestie posuere. Nam sagittis
+          vestibulum libero, eu tincidunt nulla finibus vel. Nam nec gravida nisi.
+        </p>
+      </header>
+    </section>
+  );
+}
+
+export default App;
