00596d127e0a29346e28fa8c5b6668d2fabe3e99
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2023-07-25 13:43:59 +0200

    01 - Reading List: Modified base layout.

--- pruebas/01-reading-list/iivvaannxx/src/routes/+layout.svelte
+++ pruebas/01-reading-list/iivvaannxx/src/routes/+layout.svelte
@@ -3,21 +3,35 @@
   import "../app.css";
 
   import { Modal } from '$features/modal'
+  import { BookList } from "$features/booklist";
   import Sidebar from '$lib/components/sidebar.svelte'
+
+  import { overrideItemIdKeyNameBeforeInitialisingDndZones } from "svelte-dnd-action";
+
+  // We will only be dragging book objects, which 'ID' is their ISBN.
+  overrideItemIdKeyNameBeforeInitialisingDndZones('ISBN');
 
 </script>
 
-<Sidebar />
+<header class='fixed inset-0 bg-white z-10 border-b-2 h-28 w-full p-4'>
+  My Books
+</header>
 
-<main class="relative h-full w-full overflow-auto">
 
-  <header class='sticky inset-0 border-b-2 h-28 w-full p-4'>
-    My Books
-  </header>
+<main class="relative h-full w-full mt-28 flex">
 
-  <section class='p-6'>
+  <Sidebar />
+
+  <section class='p-6 bg-red flex-grow'>
     <slot />
   </section>
+
+  <aside class='h-full p-4 border-l-2 hidden lg:block lg:w-96'>
+
+    <BookList name='test' />
+
+  </aside>
+
 
 </main>
 
