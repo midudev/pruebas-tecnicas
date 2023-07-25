6dc7d702a231b5bd8d6ddcf45f1ca783af94ac38
Pablo Martínez González <95294905+PabloMartinez01@users.noreply.github.com>
2023-07-25 22:56:19 +0200

    update: filters.spec.ts

--- pruebas/01-reading-list/pablomartinez01/tests/filters.spec.ts
+++ pruebas/01-reading-list/pablomartinez01/tests/filters.spec.ts
@@ -1,15 +1,19 @@
 import { test, expect } from '@playwright/test';
 const URL = "http://localhost:5173"
 
-test('has title', async ({ page }) => {
-  const OPTION = "Fantasía"
-  await page.goto(URL);
-  await page.locator(".filter-select > select").selectOption(OPTION)
-  const categories = await page.locator(".category").allTextContents()
+const OPTIONS = ['Fantasía','Ciencia ficción', 'Zombies','Terror']
+
+for (const option of OPTIONS){
+  test(`filter by genre: ${option}`, async ({ page }) => {
+
+    await page.goto(URL);
+    await page.locator(".filter-select > select").selectOption(option)
+    const genres = await page.locator(".category").allTextContents()
+    
+    await expect(genres.every(item => item === option)).toBeTruthy()
+    
+  });
+}
 
 
-  
-  await expect(categories.every(item => item === OPTION)).toBeTruthy()
-  
-});
 
