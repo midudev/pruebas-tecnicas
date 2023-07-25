3c4075eafeec9070574065d9f12b0f2c4321c01b
Diego Gonzales <diegoegj14@gmail.com>
2023-07-25 18:37:39 -0500

    trying view transition api

--- pruebas/01-reading-list/diego-gonzales/src/app/components/book-item/book-item.component.ts
+++ pruebas/01-reading-list/diego-gonzales/src/app/components/book-item/book-item.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, NgZone, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import type { Book } from '~/interfaces/books.interface';
 import { BooksService } from '~/services/books.service';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-book-item',
@@ -14,9 +14,24 @@ import { RouterLink } from '@angular/router';
 export class BookItemComponent {
   @Input({ required: true }) book!: Book;
   private _booksService = inject(BooksService);
+  private _router = inject(Router);
+  private _ngZone = inject(NgZone);
 
   updateReadingList(event: Event) {
     event.stopPropagation();
     this._booksService.updateReadingList(this.book);
+  }
+
+  goToBookDetail() {
+    if (!(document as any).startViewTransition) {
+      this._router.navigateByUrl(`/detail/${this.book.ISBN}`);
+      return;
+    }
+
+    (document as any).startViewTransition(() => {
+      this._ngZone.run(() =>
+        this._router.navigate(['/detail', this.book.ISBN])
+      );
+    });
   }
 }
