cc2440bda8dd769963dab54ec08ec95c68671fac
Lautaro Asat <lautaronorielasat@gmail.com>
2023-07-25 08:34:03 -0300

    Add animations heightdown & up in tailwind

--- pruebas/01-reading-list/izaya-laas/tailwind.config.js
+++ pruebas/01-reading-list/izaya-laas/tailwind.config.js
@@ -1,28 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				mystery: ['Mystery Quest', 'cursive'],
-				tilt: ['Tilt Neon', 'cursive'],
-				handlee: ['Handlee', 'cursive'],
-			},
-			backgroundImage: {
-				'grated-pattern': "url('public/grated-pattern.gif')",
-			},
-			keyframes: {
-				heightdown: {
-					'0%': { height: '100%' },
-					'33%': { height: '70%' },
-					'66%': { height: '33%' },
-					'100%': { height: '0' },
-				},
-			},
-			animation: {
-				heightdown: 'heightdown 1s ease-in-out',
-			},
-		},
-	},
-	plugins: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        mystery: ['Mystery Quest', 'cursive'],
+        tilt: ['Tilt Neon', 'cursive'],
+        handlee: ['Handlee', 'cursive'],
+      },
+      backgroundImage: {
+        'grated-pattern': "url('public/grated-pattern.gif')",
+      },
+      keyframes: {
+        heightdown: {
+          '0%': { height: 'auto' },
+          '100%': { height: '0%' },
+        },
+        heightup: {
+          '0%': { height: '0%' },
+          '100%': { height: 'auto' },
+        },
+      },
+      animation: {
+        heightdown: 'heightdown 1s ease-in-out',
+        heightup: 'heightup 1s ease-in-out',
+      },
+    },
+  },
+  plugins: [],
 };
