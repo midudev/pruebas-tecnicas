a1fc4f03923d8e016b9ccba102791a5d7f9330a2
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-07-26 11:26:46 +0200

    vamos a maquetar fuentes y filtros

--- pruebas/01-reading-list/app/src/app/page.tsx
+++ pruebas/01-reading-list/app/src/app/page.tsx
@@ -4,13 +4,22 @@ import libraryHook from "./hooks/library.hook"
 import ForReading from "./pages/forReading.pages"
 import Library from "./pages/library.pages"
 
+import credits from './styles/credits.module.css'
+
+const Credits = () => 
+    <div className={credits.credits}>
+        <p> 01 - Lista de lecturas pendientes <span className={credits.divisor}>|</span> Carlos Manuel Fraile Gómez </p>
+    </div>
+
 const Main = () => {
 
     return(
         <div className="container my-5"><div className="row">
             <div className="col-9"><Library/></div>
             <div className="col-3"><ForReading/></div>
-        </div></div>
+        </div>
+        <Credits/>
+        </div>
     )
 
 }
