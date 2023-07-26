eef38b9f7595a1bd6cfdb61e367fa3b9ea827231
Foleas <sebastianfilipetto@gmail.com>
2023-07-26 15:24:59 -0300

    Test: Book Fixture add

---
+++ pruebas/01-reading-list/foleas/src/fixture/booksFixture.ts
@@ -0,0 +1,22 @@
+// FIXTURE
+export const getBooksResponseFixture = {
+  library: [
+    {
+      book: {
+        title: "El Señor de los Anillos",
+        pages: 1200,
+        genre: "Fantasía",
+        cover:
+          "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1566425108i/33.jpg",
+        synopsis:
+          "Una aventura épica en un mundo de fantasía llamado la Tierra Media.",
+        year: 1954,
+        ISBN: "978-0618640157",
+        author: {
+          name: "J.R.R. Tolkien",
+          otherBooks: ["El Hobbit", "El Silmarillion"],
+        },
+      },
+    },
+  ],
+};
