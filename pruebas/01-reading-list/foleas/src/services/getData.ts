0081d961055b5715be8607da5be4fcf8d3d594ef
Foleas <sebastianfilipetto@gmail.com>
2023-07-26 15:25:20 -0300

    Test: Fetch Data

--- pruebas/01-reading-list/foleas/src/services/getData.ts
+++ pruebas/01-reading-list/foleas/src/services/getData.ts
@@ -2,6 +2,6 @@ import { Book, Library } from "../types";
 
 export const getData = async (endpoint: string): Promise<Array<Book>> => {
   const response = await fetch(endpoint);
-  const data: Library = (await response.json()) as Library;
+  const data = (await response.json()) as Library;
   return data.library;
 };
