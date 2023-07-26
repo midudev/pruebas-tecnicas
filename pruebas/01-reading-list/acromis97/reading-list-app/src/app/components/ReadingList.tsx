c3a57214df1ead1fd5a2cd0dc1fe96ba75fbed63
Fabricio <fepafumi97@gmail.com>
2023-07-25 23:00:19 -0300

    fix: :alembic: Cambiando renderizacion de lista de lectura

--- pruebas/01-reading-list/acromis97/reading-list-app/src/app/components/ReadingList.tsx
+++ pruebas/01-reading-list/acromis97/reading-list-app/src/app/components/ReadingList.tsx
@@ -6,17 +6,17 @@ import { Book } from "../types/Book";
 import { BookCard } from "./BookCard";
 
 export function ReadingList() {
-    const {readingList } = useReadingList()
-	// const { storeList } = useStoreList()
+    // const {readingList } = useReadingList()
+	const { storeList } = useStoreList()
 
 	return (
 		<div className="grid lg:gap-16 md:gap-6 sm:gap-3 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 grid-rows-3 lg:p-5">
-			{readingList.map((book : Book) => {
+{/* 			{readingList.map((book : Book) => {
 				return <BookCard key={book.ISBN} book={book} />;
-			})}
-{/* 			{storeList.map((book : Book) => {
-				if(book.inReadingList) return <BookCard key={book.ISBN} book={book} />;
 			})} */}
+			{storeList.map((book : Book) => {
+				if(book.inReadingList) return <BookCard key={book.ISBN} book={book} />;
+			})}
 		</div>
 	);
 }
