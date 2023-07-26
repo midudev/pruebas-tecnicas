ce2975fd5f80a1ba2f890415f8a9b4bf0c71ab1d
Jhon Hernandez <jhobahego16@hotmail.com>
2023-07-25 20:57:59 -0500

    Feat: pinia instalado

--- pruebas/01-reading-list/jhobahego/reading-list/src/main.ts
+++ pruebas/01-reading-list/jhobahego/reading-list/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia()).mount('#app')
