899575b487e753d3c95e3b15208b1f6ba9f5ee9e
Davidvn95 <davidvergaraok@gmail.com>
2023-07-26 16:40:20 -0500

    corrigiendo errores y eliminando tests

--- pruebas/01-reading-list/Davidvn95/reading-lest-davidvn95/vite.config.js
+++ pruebas/01-reading-list/Davidvn95/reading-lest-davidvn95/vite.config.js
@@ -1,17 +1,8 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import swcOptions from "./.swcrc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    test: {
-        globals: true,
-        environment: "jsdom",
-        // transform: { "^.+.jsx?$": "swc" },
-        // swcOptions,
-    },
 });
