47c34c47ef4f3286aa5d5074c658dc68ae8b3f22
Ivan Torres <40922354+torresgol10@users.noreply.github.com>
2023-07-26 14:09:47 +0200

    Modificaciones visuales en los componentes

--- pruebas/01-reading-list/torresgol10/src/components/Aside.tsx
+++ pruebas/01-reading-list/torresgol10/src/components/Aside.tsx
@@ -2,13 +2,16 @@ import useBooks from "../hooks/useBooks"
 
 export default function Aside() {
     const { booksRead, removeRead } = useBooks()
-    
+
     return (
-        <aside className='max-w-sm h-max min-h-screen px-4 pb-4 bg-slate-300'>
-            <h2 className='text-lg font-bold text-center lg:mb-8'>{booksRead.length} Libros leidos</h2>
+        <aside className='max-w-sm h-max min-h-screen px-4 pb-4 bg-black'>
+            <h2 className='text-xl text-white font-bold my-4'>{booksRead.length} Libros leidos</h2>
             <section className='grid gap-2' style={{ gridTemplateColumns: "repeat(2, minmax(150px,1fr))" }}>
                 {booksRead.map(({ book }) => (
-                    <img key={book.ISBN} src={book.cover} className='h-full object-cover hover:cursor-pointer' onClick={() => removeRead(book.ISBN)} />
+                    <article key={book.ISBN} className='group relative overflow-hidden rounded'>
+                        <img  src={book.cover} className='h-full object-cover'/>
+                        <span className="absolute top-4 right-4 flex lg:hidden lg:group-hover:flex justify-center align-center rounded-full text-2xl w-9 h-9 text-black hover:cursor-pointer" style={{ background: "#ffd100" }}  onClick={() => removeRead(book.ISBN)} >-</span>
+                    </article>
                 ))}
             </section>
 
--- pruebas/01-reading-list/torresgol10/src/components/Form.tsx
+++ pruebas/01-reading-list/torresgol10/src/components/Form.tsx
@@ -22,22 +22,28 @@ export default function Form({ filterByGenre, filterByText, filterByPages }: Pro
         <form>
             <fieldset className='flex justify-start gap-10'>
                 <legend className='text-lg font-bold'>Filtrar por:</legend>
-                <div className='flex flex-col gap-2'>
+                <div className="flex flex-col gap-2">
                     <label htmlFor='genre'>Genero</label>
-                    <select name='genre' id='genre' onChange={(e) => filterByGenre(e.target.value)}>
+                    <div className="inline-block relative w-64 h-full">
+                        <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" name='genre' id='genre' onChange={(e) => filterByGenre(e.target.value)}>
                         <option value=''>Todos</option>
-                        {genres.map((genre: string, index: number) => (
-                            <option key={index} value={genre}>{genre}</option>
-                        ))}
-                    </select>
+                            {genres.map((genre: string, index: number) => (
+                                <option key={index} value={genre}>{genre}</option>
+                            ))}
+                        </select>
+                        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                        </div>
+                    </div>
                 </div>
                 <div className='flex flex-col gap-2'>
                     <label htmlFor='genre'>Numero de pagina maximo: {pages ?? ''}</label>
-                    <input type="range" min="100" max="1200" step="100" onChange={handlerChangePages} />
+                    <input className="h-full accent-black" type="range" min="100" max="1200" step="100" onChange={handlerChangePages} />
                 </div>
+
                 <div className='flex flex-col gap-2'>
                     <label htmlFor='genre'>Texto libre</label>
-                    <input type='text' className='border-4' placeholder='Buscar' onChange={(e) => filterByText(e.target.value)} />
+                    <input type='text' className='h-full shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Buscar' onChange={(e) => filterByText(e.target.value)} />
                 </div>
             </fieldset>
         </form>
