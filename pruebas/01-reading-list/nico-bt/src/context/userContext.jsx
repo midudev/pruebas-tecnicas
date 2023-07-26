8fbd86747d11606447fd1790a5a5668f441af266
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-26 19:25:36 -0300

    Add state for number of available books and change name of reading list state to userList

--- pruebas/01-reading-list/nico-bt/src/context/userContext.jsx
+++ pruebas/01-reading-list/nico-bt/src/context/userContext.jsx
@@ -1,10 +1,12 @@
 import { createContext, useContext, useEffect, useState } from "react"
+import useBooksData from "../hooks/useBooksData"
 
 export const UserContext = createContext()
 
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState()
   const [userList, setUserList] = useState([])
+  const [availableBooks, setAvailableBooks] = useState()
 
   // Tracking data in local storage
   //-----------------------------------------------------------------
@@ -36,8 +38,14 @@ export const UserContextProvider = ({ children }) => {
     setUserList((prevState) => prevState.filter((book) => book.ISBN !== item.ISBN))
   }
 
+  const setNumberOfAvailableBooks = (bookListLength) => {
+    setAvailableBooks(bookListLength - userList.length)
+  }
+
   return (
-    <UserContext.Provider value={{ userList, addBook, removeBook }}>
+    <UserContext.Provider
+      value={{ userList, addBook, removeBook, setNumberOfAvailableBooks, availableBooks }}
+    >
       {children}
     </UserContext.Provider>
   )
