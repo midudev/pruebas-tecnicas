5abe0e3fa6d8e32b11e9bd936cbf9afe581cbe2c
Carlos Manuel González <cmglezpdev@gmail.com>
2023-07-26 15:34:20 -0400

    chore: remove React Strict Mode

--- pruebas/01-reading-list/cmglezpdev/src/main.tsx
+++ pruebas/01-reading-list/cmglezpdev/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
@@ -19,9 +18,7 @@ const router = createBrowserRouter([
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <AppProvider>
       <RouterProvider router={router} />
     </AppProvider>
-  </React.StrictMode>,
 )
