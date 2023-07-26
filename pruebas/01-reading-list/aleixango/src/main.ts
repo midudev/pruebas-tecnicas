0f06f83861bad7b0f98289b6e2e3af84883a6028
Aleix Anguita Gomez <aanguita@agrienergia.com>
2023-07-26 16:21:16 +0200

    Inject dependencies

--- pruebas/01-reading-list/aleixango/src/main.ts
+++ pruebas/01-reading-list/aleixango/src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from "vue"
 import "@/ui/style.css"
 import App from "@/ui/App.vue"
+import { BooksPloc } from "./core/book/presentation/BooksPloc"
+import { dependenciesLocator } from "@/core/common/dependencies/DependenciesLocator"
+import { BookListPloc } from "./core/book/presentation/BookListPloc"
 
-createApp(App).mount("#app")
+const app = createApp(App)
+app.provide<BooksPloc>("booksPloc", dependenciesLocator.provideBooksPloc())
+app.provide<BookListPloc>("bookListPloc", dependenciesLocator.provideBookListPloc())
+
+app.mount("#app")
