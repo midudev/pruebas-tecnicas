c908d8095eaad333c51dae9845d92fad0fed8214
David Ponce Vargas <buap.david@gmail.com>
2023-07-26 08:44:34 -0600

    style: add z-index

--- pruebas/01-reading-list/david-ponc/src/lib/components/drawer/drawer.svelte
+++ pruebas/01-reading-list/david-ponc/src/lib/components/drawer/drawer.svelte
@@ -24,7 +24,11 @@
 
 <svelte:window on:keydown={handleKeydown} />
 
-<dialog class="relative h-screen overflow-y-hidden" {open} data-state={open ? 'open' : 'closed'}>
+<dialog
+	class="relative z-10 h-screen overflow-y-hidden"
+	{open}
+	data-state={open ? 'open' : 'closed'}
+>
 	<div class="fixed inset-0 bg-sky-950/10 backdrop-blur" on:click={close} role="presentation" />
 	<section
 		class="fixed right-0 top-0 grid h-full w-screen max-w-lg grid-rows-[auto_auto_1fr] border-l border-l-slate-900 bg-slate-950 text-slate-50"
