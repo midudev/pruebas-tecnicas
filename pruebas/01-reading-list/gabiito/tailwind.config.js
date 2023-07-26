7e764d385a7a685471bb2b8bd633dc265cdf8fd4
Gabriel Zerbino <gabozerbino@gmail.com>
2023-07-26 16:42:07 -0300

    add: project configs & init from vite

---
+++ pruebas/01-reading-list/gabiito/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  corePlugins: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
