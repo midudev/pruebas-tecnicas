4a85ac8106370b2652ff8e110da4dae7f0f058b6
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2023-07-26 15:45:33 +0200

    01 - Reading List: App global refactoring.

--- pruebas/01-reading-list/iivvaannxx/src/assets/icons/index.ts
+++ pruebas/01-reading-list/iivvaannxx/src/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as InfoIcon } from '~icons/fa-solid/info'
 export { default as CloseIcon } from '~icons/fa-solid/times'
 export { default as BookmarkIcon } from '~icons/fa-solid/bookmark'
+export { default as SearchIcon } from '~icons/fa-solid/search'
