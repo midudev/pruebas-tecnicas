d3714f5c370df39fae6091691976a5f0b01ddd43
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-26 14:19:43 -0300

    Add context provider

--- pruebas/01-reading-list/nico-bt/src/main.jsx
+++ pruebas/01-reading-list/nico-bt/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+import { UserContextProvider } from "./context/userContext.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <UserContextProvider>
     <App />
-  </React.StrictMode>,
+  </UserContextProvider>
 )
