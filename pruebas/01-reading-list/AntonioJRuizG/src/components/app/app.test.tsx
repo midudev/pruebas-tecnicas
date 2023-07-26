5ec29289067056f1b955f5df7d519dca37efb6fb
AntonioJesusRG <antoniogar129@gmail.com>
2023-07-27 00:33:43 +0200

    Fix build command

--- pruebas/01-reading-list/AntonioJRuizG/src/components/app/app.test.tsx
+++ pruebas/01-reading-list/AntonioJRuizG/src/components/app/app.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { beforeEach, describe, expect, test, vi } from 'vitest';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
