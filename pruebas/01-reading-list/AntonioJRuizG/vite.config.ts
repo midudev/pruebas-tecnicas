b596b92fc567010786bf41d2a97e9e5d8bf026d2
AntonioJesusRG <antoniogar129@gmail.com>
2023-07-27 00:07:04 +0200

    Config test tools

--- pruebas/01-reading-list/AntonioJRuizG/vite.config.ts
+++ pruebas/01-reading-list/AntonioJRuizG/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './utils/tests/setup.js',
+	},
+});
