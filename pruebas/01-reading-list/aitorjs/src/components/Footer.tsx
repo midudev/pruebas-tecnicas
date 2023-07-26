90b94348b85a7cbfc0d3b292abec4011dc17035d
aitorjs <aitiba@gmail.com>
2023-07-26 16:26:45 +0200

    add: favicons

--- pruebas/01-reading-list/aitorjs/src/components/Footer.tsx
+++ pruebas/01-reading-list/aitorjs/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = (): ReactElement => {
   return (
     <section className='footer-container bg-purple-800 text-white text-center py-4'>
       <p>
-        aitorjs | Prueba técnica: <a href='https://github.com/midudev/pruebas-tecnicas/tree/main/pruebas/01-reading-list' target='_blank' title='Acceso a la prueba técnica' className='underline' rel='noreferrer'>Reading List</a> (frontend)
+        aitorjs | Prueba técnica: <a href='https://github.com/midudev/pruebas-tecnicas/blob/main/pruebas/01-reading-list/README.md' className='underline' rel='noreferrer'>Reading List</a> (frontend)
         | <a href='https://github.com/aitorjs/pruebas-tecnicas/tree/main/pruebas/01-reading-list/aitorjs' target='_blank' className='underline' title='Mi solución a la prueba técnica' rel='noreferrer'>Código</a>
       </p>
     </section>
