0a8000611152ebcede9bd749e2d34ad412faaf73
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2023-07-26 15:41:42 +0200

    01 - Reading List: Refactored global types into app types.d.ts.

--- pruebas/01-reading-list/iivvaannxx/src/app.d.ts
+++ pruebas/01-reading-list/iivvaannxx/src/app.d.ts
@@ -15,10 +15,40 @@ declare global {
 
     interface HTMLAttributes<T> {
 
+      // Custom events for the svelte-dnd-action
       'on:consider'?: (event: CustomEvent<DndEvent<Item>> & { target: EventTarget & T }) => void
       'on:finalize'?: (event: CustomEvent<DndEvent<Item>> & { target: EventTarget & T }) => void
     }
   }
+}
+
+// Types that are widely used in the entire app.
+declare global {
+
+  /** @brief Defines all the fields of the data contained in the 'books.json'. */
+  interface Book {
+
+    title: string
+    pages: number
+    genre: string
+
+    cover: string
+    synopsis: string
+
+    year: number
+    ISBN: string
+
+    author: Author
+  }
+
+  interface Author {
+
+    name: string
+    otherBooks: string[]
+  }
+
+  // We declare a book array type to easily use it in the entire app.
+  interface BookArray extends Array<Book> { }
 }
 
 export {}
--- pruebas/01-reading-list/iivvaannxx/src/types/index.ts
+++
@@ -1,1 +0,0 @@
-export type { Author, Book, Library, BooksData } from './book'
