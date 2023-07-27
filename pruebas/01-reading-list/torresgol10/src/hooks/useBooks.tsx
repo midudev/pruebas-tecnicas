7c19b2c6a04ed31e31a3dc70d502fde2f03aaaad
torresgol10 <torresgol10.it@gmail.com>
2023-07-27 21:05:33 +0200

    Añadimos la posibilidad los libros disponibles.

--- pruebas/01-reading-list/torresgol10/src/hooks/useBooks.tsx
+++ pruebas/01-reading-list/torresgol10/src/hooks/useBooks.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useRef, useState } from "react"
 import { useStore } from "../store/useStore"
+import { Library } from "../types"
 
+/* Funcion para ordenar los libros una vez filtros la key siempre sera un string*/
+const sortBooks: { [key: string]: (books: Library) => Library } = {
+    title: (books) => [...books.sort((a, b) => a.book.title.localeCompare(b.book.title))],
+    author: (books) => [...books.sort((a, b) => a.book.author.name.localeCompare(b.book.author.name))],
+    pages: (books) => [...books.sort((a, b) => b.book.pages - a.book.pages)],
+    year: (books) => [...books.sort((a, b) => b.book.year - a.book.year)],
+}
 
 export default function useBooks() {
     const { store, storeRead, addRead, removeRead } = useStore()
@@ -12,6 +20,9 @@ export default function useBooks() {
     const [filterGenre, setFilterGenre] = useState<string>("")
     const [filterText, setFilterText] = useState<string>("")
     const [filterPages, setFilterPages] = useState<number>(1200)
+
+    const [sortType, setSortType] = useState<string>("")
+
 
     /* Cada vez que cambie uno de los 3 posibles filtros tenemos que llamar a su funcion correspondiente, 
     teniendo en cuenta que si hay mas de un filtro activo tenemos que aplicar los todos los filtros y actualizar los books */
@@ -29,13 +40,17 @@ export default function useBooks() {
             newStore = [...newStore.filter(({ book }) => book.pages <= filterPages)]
         }
 
+        if(sortType !== ""){
+            newStore = sortBooks[sortType](newStore)
+        }
+
         setBooks(newStore)
-    }, [filterGenre, filterText, filterPages, store])
+    }, [filterGenre, filterText, filterPages, sortType, store])
 
     useEffect(() => {
         genres.current = [...new Set(store.map(({ book }) => book.genre))]
     }, [store])
 
 
-    return { books, booksRead: storeRead, genres: genres.current, nPages: filterPages, addRead, removeRead, filterByGenre: setFilterGenre, filterByText: setFilterText, filterByPages: setFilterPages }
+    return { books, booksRead: storeRead, genres: genres.current, nPages: filterPages, addRead, removeRead, filterGenre, filterByGenre: setFilterGenre, filterByText: setFilterText, filterByPages: setFilterPages, setSortType }
 }
--- pruebas/01-reading-list/torresgol10/src/types.d.ts
+++ pruebas/01-reading-list/torresgol10/src/types.d.ts
@@ -19,3 +19,4 @@ export interface Author {
     name: string
     otherBooks: string[]
 }
+
