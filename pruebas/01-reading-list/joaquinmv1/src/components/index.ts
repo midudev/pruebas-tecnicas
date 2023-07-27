31224b6a2747c964e01e67afe8e701dfdbc438c1
Joaquinmv1 <villarrealjoaquin09@gmail.com>
2023-07-26 22:36:49 -0300

    se soluciono error en la lista de lectura

--- pruebas/01-reading-list/joaquinmv1/src/components/index.ts
+++ pruebas/01-reading-list/joaquinmv1/src/components/index.ts
@@ -2,6 +2,5 @@ export * from './BookItem/BookItem';
 export * from './BooksList';
 export * from './BooksPages';
 export * from './MainButton';
-export * from './ReadingItem/ReadingItem';
 export * from './SearchInput/SearchInput';
 export * from './SelectCategory';
