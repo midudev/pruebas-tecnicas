572acf8575719a8e2b39b39ba3ed6c4569b6ecba
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-26 22:02:44 -0400

    Add filter: se añade otro filtro de paginas, ahora tiene un rango de min y max

--- pruebas/01-reading-list/patriciotrujilllo/src/hook/useFilters.js
+++ pruebas/01-reading-list/patriciotrujilllo/src/hook/useFilters.js
@@ -6,7 +6,7 @@ export const useFilters = () =>{
 
     const filterBooks = (books) =>{
         return(
-            books.filter(book => filters.genre=== 'all' || book.book.genre===filters.genre).filter(book => filters.pages>=book.book.pages)
+            books.filter(book => filters.genre=== 'all' || book.book.genre===filters.genre).filter(book => filters.pages[0]<=book.book.pages & filters.pages[1]>=book.book.pages)
                 .filter(book => filters.title==='' || book.book.title.toLowerCase().includes(filters.title.toLowerCase()))
         )
         //bookNoFiltered.filter(item=> read.includes(book => book.title === item.book.title))
