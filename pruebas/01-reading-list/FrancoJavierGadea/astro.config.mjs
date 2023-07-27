b743b1cd49ea4a5ed188f65261b5083423cce121
1812 <francogadea1812@gmail.com>
2023-07-27 06:20:46 -0300

    Probando las view transition con edge functions de netlify

--- pruebas/01-reading-list/FrancoJavierGadea/astro.config.mjs
+++ pruebas/01-reading-list/FrancoJavierGadea/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 import node from '@astrojs/node';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 export default defineConfig({
