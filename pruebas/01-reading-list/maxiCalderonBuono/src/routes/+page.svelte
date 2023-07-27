b2c34a391967452fd5da6e9df64dc6c243f73793
Maximiliano Emanuel Calderón Buono <77062697+maxiCalderonBuono@users.noreply.github.com>
2023-07-27 09:06:53 +0200

    fix: modify grid

--- pruebas/01-reading-list/maxiCalderonBuono/src/routes/+page.svelte
+++ pruebas/01-reading-list/maxiCalderonBuono/src/routes/+page.svelte
@@ -175,7 +175,7 @@
 		{/if}
 		{#if show === 'library'}
 			<div
-				class="grid items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10"
+				class="grid items-center grid-cols-[auto-fit, minmax(250px, 1fr)] gap-x-4 gap-y-8"
 				in:fly={{ y: 200, duration: 500 }}
 			>
 				{#each $initialDataStore.renderlist as { book }}
