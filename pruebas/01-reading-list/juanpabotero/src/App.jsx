40ec1d3ec296e9eea2c9a63bee5467dd3dbed287
juanpabotero <juanpabotero1@gmail.com>
2023-07-27 18:40:48 -0500

    feat: add background colors

--- pruebas/01-reading-list/juanpabotero/src/App.jsx
+++ pruebas/01-reading-list/juanpabotero/src/App.jsx
@@ -28,6 +28,14 @@ function App() {
           <Books books={filteredBooks} />
         </main>
       </div>
+      <div
+        className="fixed -z-10 h-[134px] w-[134px] lg:w-[300px] lg:h-[300px] rounded-full 
+        bg-red-600 blur-[150px] lg:blur-[350px] opacity-50 left-0 top-0"
+      ></div>
+      <div
+        className="fixed -z-10 h-[134px] w-[134px] lg:w-[300px] lg:h-[300px] rounded-full 
+        bg-amber-600 blur-[150px] lg:blur-[350px] opacity-50 right-0 bottom-0"
+      ></div>
     </ReadingListProvider>
   );
 }
