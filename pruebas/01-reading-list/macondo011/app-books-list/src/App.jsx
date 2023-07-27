803f6ac8ffa3a2b29198a2232cb73763339e1b25
Marcos Bautista P <macondo777@hotmail.com>
2023-07-27 13:53:54 -0500

    cambios de ultimo momento

--- pruebas/01-reading-list/macondo011/app-books-list/src/App.jsx
+++ pruebas/01-reading-list/macondo011/app-books-list/src/App.jsx
@@ -6,22 +6,20 @@ import {library as initialBooks} from '../../../books.json'
 import { ReadingList } from './components/ReadingList';
 import { ReadingListProvider } from './components/context/ReadingListContext';
 
-
 function App() {
   const { filterBooks } = useLibraryFilters()
   const filteredBooks = filterBooks(initialBooks)
   return (
     <ReadingListProvider>
        <Header/>
-
-        <div className='Body'>
-          <section className='principal' >
-            <Books books={filteredBooks}/>
-          </section>
-          <article className='article'>
-            <ReadingList/>
-          </article>
-        </div>
+          <div className='Body'>
+            <section className='principal' >
+              <Books books={filteredBooks}/>
+            </section>
+            <article className='article'>
+              <ReadingList/>
+            </article>
+          </div>
     </ReadingListProvider> 
   )
 }
