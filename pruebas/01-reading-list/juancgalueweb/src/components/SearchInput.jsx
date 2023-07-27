b7d21331dd28ac120a80238294e33d8b2c9201c4
Juan Carlos Galue <juancgalue@icloud.com>
2023-07-27 02:38:14 -0400

    fix: :bug: Corrigiendo otro bug

--- pruebas/01-reading-list/juancgalueweb/src/components/SearchInput.jsx
+++ pruebas/01-reading-list/juancgalueweb/src/components/SearchInput.jsx
@@ -1,15 +1,22 @@
 import { Input } from 'antd'
+import { useEffect } from 'react'
+import { useBooksStore } from '../stores/books'
 import { useSearchBooks } from '../stores/searchBooks'
 
 export const SearchInput = () => {
   const search = useSearchBooks((state) => state.search)
   const setSearch = useSearchBooks((state) => state.setSearch)
   const searchBooks = useSearchBooks((state) => state.searchBooks)
+  const readingList = useBooksStore((state) => state.readingList)
 
   const handleChange = (event) => {
     setSearch(event.target.value)
     searchBooks()
   }
+
+  useEffect(() => {
+    searchBooks()
+  }, [readingList])
 
   return (
     <div>
