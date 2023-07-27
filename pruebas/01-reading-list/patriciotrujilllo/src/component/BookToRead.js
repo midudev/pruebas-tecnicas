2d30937f23a9c6761f3816d321942ffdd0eac3c9
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-26 22:12:25 -0400

    Add close button

--- pruebas/01-reading-list/patriciotrujilllo/src/component/BookToRead.js
+++ pruebas/01-reading-list/patriciotrujilllo/src/component/BookToRead.js
@@ -1,15 +1,19 @@
 import { useListBook } from "../hook/useListBook"
+import CloseButton from 'react-bootstrap/CloseButton'
 import './bootoread.css'
 
 export const BookToRead = () =>{
 
-    const {read} = useListBook()
+    const {read,removeToListRead} = useListBook()
     return (
-            <div data-test-id='id-container-list-read' className={`to-read ${read.length ? '' : 'hidden'}`}>
+            <div data-test-id='id-container-list-read' className='to-read'>
                 <div className="container-list">
                     {
                     read.map((book=>(
                         <figure key={book.ISBN} className="figure">
+                            <div className="close-button" data-bs-theme="dark">
+                            <CloseButton onClick={()=>removeToListRead(book)}/>
+                            </div>
                             <img src={book.cover} alt={book.title} />
                         </figure>
                     )))
