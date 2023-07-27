bbf37044f652be5bf8bcd5bf503af39ad395d197
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-26 22:12:57 -0400

    Add styles of react bootstrap

--- pruebas/01-reading-list/patriciotrujilllo/src/index.js
+++ pruebas/01-reading-list/patriciotrujilllo/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { FilterProvider } from './context/filtersContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
