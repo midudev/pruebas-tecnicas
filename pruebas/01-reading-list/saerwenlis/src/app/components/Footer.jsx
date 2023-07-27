069cf2d518ee7ae1917e9769d1e2f9c53d23c46d
Maria Soledad Lanfranconi <pennymsl@gmail.com>
2023-07-27 10:41:30 -0300

    fix: background color

--- pruebas/01-reading-list/saerwenlis/src/app/components/Footer.jsx
+++ pruebas/01-reading-list/saerwenlis/src/app/components/Footer.jsx
@@ -20,14 +20,14 @@ export default function Dashboard() {
 
     return (
         <footer className="fixed bottom-0 h-20 w-full pl-24 flex items-center">
-            <section className="w-8/12 pl-12 flex justify-between items-center">
-                <h3 className="text-sm font-light">Available books <span className="rounded-full bg-primary px-2 py-1 text-bgwarn text-xs font-medium">{availableBooks}</span></h3>
-                <section className="mr-24">
+            <section className="w-[65.65%] h-full pl-12 flex justify-between items-center bg-white">
+                <h3 className="text-sm font-light">Available books <span className="rounded-full bg-primary px-2 py-1 ml-1 text-bgwarn text-xs font-medium">{availableBooks}</span></h3>
+                <section className="pr-[70px]">
                     <GenreButton /> 
                 </section>
             </section>
-            <section className="relative right-0 w-4/12 pl-4 flex items-center">
-                <h3 className="text-sm font-light">My books <span className="rounded-full bg-primary px-2 py-1 text-bgwarn text-xs font-medium">{myBooks}</span></h3>          
+            <section className="relative right-0 w-4/12 h-full pl-2 flex items-center 2xl:ml-10 bg-bgcold">
+                <h3 className="text-sm font-light">My books <span className="rounded-full bg-primary px-2 py-1 ml-1 text-bgwarn text-xs font-medium">{myBooks}</span></h3>          
             </section>
         </footer>
     )
