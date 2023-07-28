62672ae7823a3b1b347db69bcf1f1ecaf09b05c4
Foleas <sebastianfilipetto@gmail.com>
2023-07-28 17:13:50 -0300

    SVG plugin config

--- pruebas/01-reading-list/foleas/vite.config.ts
+++ pruebas/01-reading-list/foleas/vite.config.ts
@@ -3,10 +3,11 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: "jsdom",
