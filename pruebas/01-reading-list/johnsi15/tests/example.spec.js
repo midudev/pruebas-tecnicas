13900b124c27d87e8d029853130e9f512b5d2f1a
John Serrano <andrey_201015@hotmail.com>
2023-07-28 18:05:17 -0500

    Add more test, has book details and add book to reading list

--- pruebas/01-reading-list/johnsi15/tests/example.spec.js
+++ pruebas/01-reading-list/johnsi15/tests/example.spec.js
@@ -22,27 +22,41 @@ test('has book list', async ({ page }) => {
   await expect(books).toHaveCount(13)
 })
 
-test.only('has book details', async ({ page }) => {
+test('has book details', async ({ page }) => {
   await page.goto(TEST_URL)
   const bookTitle = 'El Señor de los Anillos'
 
-  const firstBook = page.locator('section > li')
-  console.log({ firstBook })
-  // Realizar el hover sobre el primer elemento de la lista de libros
-  // await firstBook.first().hover()
+  const firstBook = page.locator('section ul > li').first()
+  await firstBook.hover()
 
-  // await page.getByRole('link').hover();
-  await page.locator('section > li').hover()
+  // Localizar el elemento que contiene los detalles del libro después del hover
+  const bookDetailsElement = page.locator('article > h2')
 
-  // // Asegúrate de que haya suficiente tiempo para que se muestren los detalles del libro
-  // await page.waitForTimeout(1000)
+  // Obtener el texto de los detalles del libro
+  const bookDetails = await bookDetailsElement.textContent()
 
-  // // Localizar el elemento que contiene los detalles del libro después del hover
-  // const bookDetailsElement = await page.locator('article > h2')
+  // Verificar que el elemento esté visible y que el texto sea el esperado
+  await expect(bookDetailsElement).toBeVisible()
+  expect(bookDetails).toEqual(bookTitle)
+})
 
-  // // Obtener el texto de los detalles del libro
-  // const bookDetails = await bookDetailsElement.textContent()
-  // console.log('Información del libro:', bookDetails)
+test.only('add book to reading list', async ({ page }) => {
+  await page.goto(TEST_URL)
+  const bookTitle = 'El Señor de los Anillos'
 
-  // Aquí puedes realizar las expectativas o aserciones sobre los detalles del libro si es necesario.
+  const firstBook = page.locator('section ul > li').first()
+  await firstBook.hover()
+
+  const bookDetailsElement = page.locator('article > h2')
+  const bookDetails = await bookDetailsElement.textContent()
+
+  await expect(bookDetailsElement).toHaveText(bookTitle)
+
+  const bookButton = page.getByRole('button', { name: 'Agregar a la lista' })
+  await bookButton.click()
+
+  const readingListImg = page.locator('div > ul li figure > img')
+  const altText = await readingListImg.getAttribute('alt')
+  await expect(readingListImg).toBeVisible()
+  expect(altText).toEqual(bookDetails)
 })
