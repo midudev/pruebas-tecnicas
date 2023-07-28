8618d1ce1a81e468382efd01e1935b77c10bf2ac
John Serrano <andrey_201015@hotmail.com>
2023-07-28 16:40:40 -0500

    Add footer and styles

--- pruebas/01-reading-list/johnsi15/src/components/Footer/index.jsx
+++ pruebas/01-reading-list/johnsi15/src/components/Footer/index.jsx
@@ -1,8 +1,15 @@
+import IconLinkedin from '../IconLinkedin'
+
 import styles from './Footer.module.css'
 export default function Footer () {
   return (
     <footer className={styles.footer}>
-      <div>Hecho con 💖 por <a href='https://johnserrano.co/' target='_blank' rel="noreferrer">John Serrano</a></div>
+      <div>
+        Hecho con 💖 por <a href='https://johnserrano.co/' target='_blank' rel="noreferrer">John Serrano</a>
+        <a href="https://www.linkedin.com/in/jandreys15/" target='_blank' rel="noreferrer">
+          <IconLinkedin />
+        </a>
+      </div>
     </footer>
   )
 }
