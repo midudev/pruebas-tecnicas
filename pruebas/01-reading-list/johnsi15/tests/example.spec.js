243f2589c096ce789c9a27eed35dac4e4d90d81d
John Serrano <andrey_201015@hotmail.com>
2023-07-28 14:25:24 -0500

    Changes readme and styles font, add more test

--- pruebas/01-reading-list/johnsi15/tests/example.spec.js
+++ pruebas/01-reading-list/johnsi15/tests/example.spec.js
@@ -21,3 +21,28 @@ test('has book list', async ({ page }) => {
   await expect(books.first()).toBeVisible()
   await expect(books).toHaveCount(13)
 })
+
+test.only('has book details', async ({ page }) => {
+  await page.goto(TEST_URL)
+  const bookTitle = 'El Señor de los Anillos'
+
+  const firstBook = page.locator('section > li')
+  console.log({ firstBook })
+  // Realizar el hover sobre el primer elemento de la lista de libros
+  // await firstBook.first().hover()
+
+  // await page.getByRole('link').hover();
+  await page.locator('section > li').hover()
+
+  // // Asegúrate de que haya suficiente tiempo para que se muestren los detalles del libro
+  // await page.waitForTimeout(1000)
+
+  // // Localizar el elemento que contiene los detalles del libro después del hover
+  // const bookDetailsElement = await page.locator('article > h2')
+
+  // // Obtener el texto de los detalles del libro
+  // const bookDetails = await bookDetailsElement.textContent()
+  // console.log('Información del libro:', bookDetails)
+
+  // Aquí puedes realizar las expectativas o aserciones sobre los detalles del libro si es necesario.
+})
