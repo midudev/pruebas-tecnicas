53be8013c25e46a8096c99f5b06e53b4920e2f9a
offneti <aniolneti@gmail.com>
2023-07-28 14:49:48 +0200

    Making it responsive and clean

--- pruebas/01-reading-list/offneti/src/App.vue
+++ pruebas/01-reading-list/offneti/src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import booksData from '../books.json';
-import { ref } from 'vue';
+import booksData from "../books.json";
+import { ref } from "vue";
 
-const selectedType = ref('all');
-const books = ref(booksData['library']);
+const selectedType = ref("all");
+const books = ref(booksData["library"]);
 const readingList = ref<Book[]>([]);
 const selectedBook = ref<Book | null>(null);
 const alreadyInReadingList = ref(false);
@@ -19,16 +19,18 @@ interface Book {
 }
 
 function getFilteredBooks() {
-  if (selectedType.value === 'all') {
-    books.value = booksData['library'];
-    return
+  if (selectedType.value === "all") {
+    books.value = booksData["library"];
+    return;
   }
-  books.value = booksData['library'].filter(book => book.book.genre === selectedType.value);
+  books.value = booksData["library"].filter(
+    (book) => book.book.genre === selectedType.value
+  );
 }
 
 function getGenres() {
   const genres = new Set();
-  booksData['library'].forEach(book => genres.add(book.book.genre));
+  booksData["library"].forEach((book) => genres.add(book.book.genre));
   return Array.from(genres);
 }
 
@@ -46,15 +48,15 @@ function addToReadingList(cover: string, title: string) {
   for (let i = 0; i < readingList.value.length; i++) {
     if (readingList.value[i].title === title) {
       alreadyInReadingList.value = true;
-      return
+      return;
     }
   }
   const newBook: Book = {
     cover,
     title,
-    author: { name: '' },
-    genre: '',
-    year: 0
+    author: { name: "" },
+    genre: "",
+    year: 0,
   };
   // Add the new book to the reading list
   readingList.value.push(newBook);
@@ -72,105 +74,140 @@ function deleteFromReadingList(title: string) {
     books_avaliables.value = false;
   }
 }
-
 </script>
 
 <template>
-  <body>
-    <div class="container">
-      <header>
-        <h4 id="navbar_title">Book repository</h4>
-        <img id="logo"
-          src="https://static.vecteezy.com/system/resources/previews/009/902/691/original/stack-of-colored-books-3d-render-png.png"
-          alt="Book" style="width: 70px; height: 70px; margin-left:5px; margin-top:10px;" />
-      </header>
-      <main>
-        <section>
-          <div class="filters_container">
-            <div id="info">
-              <h2 v-show="countBooksAvailable() !== 0">Books availables: {{ countBooksAvailable() }}</h2>
-              <h2 v-show="countBooksAvailable() === 0">No books availables</h2>
-              <h4>
-                Here you can find all the books avaliables in our repository.
-                <br> You can add new books to your reading list and delete them.
-              </h4>
-            </div>
-            <div id="filters">
-              <h4 id="selectedType" for="filtrar_genero">Search by genre</h4>
-              <select id="select_gender" v-model="selectedType" @change="getFilteredBooks">
-                <option value="all">All</option>
-                <option v-for="genre in getGenres()" :value="genre">{{ genre }}</option>
-              </select>
-            </div>
-          </div>
-        </section>
-        <article>
-          <div id="main_grid">
-            <div v-for="book in books" :key="book.book.title" class="book_item">
-              <img class="have_hover" :src="book.book.cover" alt="Book" style="width: 180px; height: 280px;"
-                @click="showBookPopup(book.book)" />
-            </div>
-          </div>
-        </article>
-      </main>
-      <aside>
-        <h2>Reading list</h2>
-        <div id="aside_grid">
-          <div v-show="readingList.length" v-for="book in readingList" :key="book.title" class="book_item">
-            <img @click="deleteFromReadingList(book.title)" class="have_hover" :src="book.cover" alt="Book"
-              style="width: 100px; height: 140px;" />
-          </div>
+  <header>
+    <h4 id="navbar_title">Book repository</h4>
+    <img
+      id="logo"
+      src="https://static.vecteezy.com/system/resources/previews/009/902/691/original/stack-of-colored-books-3d-render-png.png"
+      alt="Book"
+      style="width: 70px; height: 70px; margin-left: 5px; margin-top: 10px"
+    />
+  </header>
+  <div class="container">
+    <main>
+      <div class="filters_container">
+        <div id="info">
+          <h2 v-show="countBooksAvailable() !== 0">
+            Books availables: {{ countBooksAvailable() }}
+          </h2>
+          <h2 v-show="countBooksAvailable() === 0">No books availables</h2>
+          <h4>
+            Here you can find all the books avaliables in our repository.
+            <br />
+            You can add new books to your reading list and delete them.
+          </h4>
         </div>
-        <div v-show="!readingList.length">
-          <h4>You are not reading any book :(</h4>
-        </div>
-      </aside>
-    </div>
-    <div v-if="selectedBook" class="modal">
-      <div id="modal-backdrop"></div>
-      <div class="modal-content">
-        <div>
-          <span class="close" @click="selectedBook = null">&times;</span>
-          <img :src="selectedBook.cover" alt="Book" style="width: 200px; height: 280px;" />
-        </div>
-        <div id="book_info">
-          <h2 style="margin:20px;">{{ selectedBook.title }}</h2>
-          <h4 style="margin:20px;">Author: {{ selectedBook.author.name }}</h4>
-          <h4 style="margin:20px;">Genre: {{ selectedBook.genre }}</h4>
-          <h4 style="margin:20px;">Year: {{ selectedBook.year }}</h4>
-          <button @click="addToReadingList(selectedBook.cover, selectedBook.title), selectedBook = null" id="add_book"
-            style="margin:20px;">Add
-            to reading
-            list</button>
+        <div id="filters">
+          <h4 id="selectedType" for="filtrar_genero">Search by genre</h4>
+          <select
+            id="select_gender"
+            v-model="selectedType"
+            @change="getFilteredBooks"
+          >
+            <option value="all">All</option>
+            <option v-for="genre in getGenres()" :value="genre">
+              {{ genre }}
+            </option>
+          </select>
         </div>
       </div>
-    </div>
-    <div v-show="alreadyInReadingList" class="modal_already_on_list">
-      <div id="modal-backdrop"></div>
-      <div class="modal-content-error">
-        <div id="error">
-          <span class="close_error" @click="alreadyInReadingList = false">&times;</span>
-          <h4 style="margin:20px;">Already on list</h4>
+      <div id="main_grid">
+        <div v-for="book in books" :key="book.book.title" class="book_item">
+          <img
+            class="have_hover"
+            :src="book.book.cover"
+            alt="Book"
+            style="width: 180px; height: 280px"
+            @click="showBookPopup(book.book)"
+          />
         </div>
       </div>
+    </main>
+    <aside hidden>
+      <h2>Reading list</h2>
+      <div id="aside_grid">
+        <div
+          v-show="readingList.length"
+          v-for="book in readingList"
+          :key="book.title"
+          class="book_item"
+        >
+          <img
+            @click="deleteFromReadingList(book.title)"
+            class="have_hover"
+            :src="book.cover"
+            alt="Book"
+            style="width: 100px; height: 140px"
+          />
+        </div>
+      </div>
+      <div v-show="!readingList.length">
+        <h4>You are not reading any book :(</h4>
+      </div>
+    </aside>
+  </div>
+  <div v-if="selectedBook" class="modal">
+    <div id="modal-backdrop"></div>
+    <div class="modal-content">
+      <div>
+        <span class="close" @click="selectedBook = null">&times;</span>
+        <img
+          :src="selectedBook.cover"
+          alt="Book"
+          style="width: 200px; height: 280px"
+        />
+      </div>
+      <div id="book_info">
+        <h2 style="margin: 20px">{{ selectedBook.title }}</h2>
+        <h4 style="margin: 20px">Author: {{ selectedBook.author.name }}</h4>
+        <h4 style="margin: 20px">Genre: {{ selectedBook.genre }}</h4>
+        <h4 style="margin: 20px">Year: {{ selectedBook.year }}</h4>
+        <button
+          @click="
+            addToReadingList(selectedBook.cover, selectedBook.title),
+              (selectedBook = null)
+          "
+          id="add_book"
+          style="margin: 20px"
+        >
+          Add to reading list
+        </button>
+      </div>
     </div>
-    <footer>
-      <h6 style="color: rgb(68, 67, 67);">This is a technical test by midudev (Junior Level) | Developed
-        by
-        Aniol Nevado
-        | Github
-        user: offneti</h6>
-    </footer>
-  </body>
+  </div>
+  <div v-show="alreadyInReadingList" class="modal_already_on_list">
+    <div id="modal-backdrop"></div>
+    <div class="modal-content-error">
+      <div id="error">
+        <span class="close_error" @click="alreadyInReadingList = false"
+          >&times;</span
+        >
+        <h4 style="margin: 20px">Already on list</h4>
+      </div>
+    </div>
+  </div>
+  <footer>
+    <h6 style="color: rgb(68, 67, 67)">
+      This is a technical test by midudev (Junior Level) | Developed by Aniol
+      Nevado | Github user: offneti
+    </h6>
+  </footer>
 </template>
 <style scoped>
+body {
+  height: 100vh;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 footer {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
   width: 100%;
-  height: 40px;
+  height: 2.5rem;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -195,12 +232,7 @@ footer {
 
 #select_gender {
   border-radius: 10px;
-  scale: 1.2;
-}
-
-#filters {
-  display: inline-block;
-  margin-right: 20px;
+  scale: 1;
 }
 
 .modal {
@@ -236,12 +268,12 @@ footer {
 }
 
 .have_hover {
-  transition: transform .2s ease-in-out;
+  transition: transform 0.2s ease-in-out;
 }
 
 #logo:hover {
   transform: scale(1.3);
-  transition: transform .4s ease-in-out;
+  transition: transform 0.4s ease-in-out;
 }
 
 .modal-content {
@@ -278,13 +310,6 @@ footer {
   cursor: pointer;
 }
 
-section {
-  height: 120px;
-  text-align: left;
-  margin-left: 15px;
-  margin-bottom: 30px;
-}
-
 .filters_container {
   display: flex;
   flex-direction: row;
@@ -293,10 +318,21 @@ section {
 }
 
 #main_grid {
+  /* display: flex;
+  flex-flow: row wrap;
+  gap: 10px;
+  align-items: center; */
   display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  grid-gap: 10px;
+  grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
+  place-items: center center;
+  gap: 10px;
 }
+
+/* @media screen and (max-width: 940px) {
+  #main_grid {
+    justify-content: center;
+  }
+} */
 
 #aside_grid {
   display: grid;
@@ -305,7 +341,6 @@ section {
 }
 
 .book_item {
-  padding: 3px;
 }
 
 h1,
@@ -331,33 +366,33 @@ aside {
   scale: 1;
 }
 
-
 main {
-  position: fixed;
-  margin-left: 80px;
-  top: 114px;
   background-image: linear-gradient(to right, #2c463a, #42b883, #445e48);
+  margin-top: 2.5rem;
   border-radius: 40px;
   padding: 1%;
-  scale: 1;
+  height: 80vh;
+  width: 60vw;
+  overflow: auto;
 }
-
-
+@media screen and (max-width: 1000px) {
+  main {
+    width: 90vw;
+  }
+}
+@media screen and (max-width: 550px) {
+}
 
 header {
   display: flex;
-  top: 0;
-  left: 0;
-  right: 0;
-  position: fixed;
   font-size: calc(10px + 1vmin);
   background-image: linear-gradient(to right, #2c463a, #42b883, #445e48);
 }
 
 article {
-  height: 35vw;
   overflow-y: auto;
-  position: sticky;
+  display: grid;
+  place-content: center;
 }
 
 #navbar_title {
