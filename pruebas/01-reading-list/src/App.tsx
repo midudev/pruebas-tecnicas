91a01e806404ad4a0606e7326329d5caf4a1a3ab
Damien Monasterios <monasterio13septiembre@gmail.com>
2023-07-30 12:33:48 -0500

    feature/ filtro por paginas

--- pruebas/01-reading-list/src/App.tsx
+++ pruebas/01-reading-list/src/App.tsx
@@ -11,7 +11,9 @@ function App() {
   const [books, setBooks] = useState<BookWithReadList[]>([]);
   const [filteredBooks, setFilteredBooks] = useState<Book[]>([]);
   const [selectedGenre, setSelectedGenre] = useState<string>("");
+  const [sliderValue, setSliderValue] = useState<string>();
   const bookData = filteredBooks.length > 0 ? filteredBooks : books;
+
   const [readList, setReadList] = useState<BookWithReadList[]>(() => {
     return parseLocalStorageData<BookWithReadList[]>("readList", []);
   });
@@ -58,9 +60,23 @@ function App() {
     );
   };
 
-  // Function to update readList state
   const updateReadListState = (newReadList: BookWithReadList[]) => {
     setReadList(newReadList);
+  };
+
+  const MaxPagesBook = books.reduce(
+    (acc, current) => {
+      return current.pages > acc.pages ? current : acc;
+    },
+    { pages: -Infinity }
+  );
+
+  const changeSlider = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    const result = books.filter((x) => x.pages <= parseInt(value));
+
+    setFilteredBooks(result);
+    setSliderValue(value);
   };
 
   useEffect(() => {
@@ -93,21 +109,44 @@ function App() {
     <div className="App">
       <header>
         <h1>Book Catalog App</h1>
+
+        <div style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
+          <div>
+            <label htmlFor="genreSelect">Filtra por género:</label>
+            <select
+              id="genreSelect"
+              value={selectedGenre}
+              onChange={(e) => setSelectedGenre(e.target.value)}
+            >
+              <option value="">Seleccionar</option>
+              {listGenres.map((genre, index) => (
+                <option key={index} value={genre}>
+                  {genre}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="myRange">Filtra por páginas: {sliderValue}</label>
+            <input
+              type="range"
+              min="0"
+              max={MaxPagesBook?.pages}
+              value={sliderValue}
+              onChange={changeSlider}
+              id="myRange"
+            />
+          </div>
+        </div>
+
+        <div style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
+          <p>{books.length - readList.length} Libros disponibles</p>
+          <p>{readList.length} en la lista de lectura</p>
+        </div>
       </header>
+
       <main>
-        <label htmlFor="genreSelect">Seleccionar género:</label>
-        <select
-          id="genreSelect"
-          value={selectedGenre}
-          onChange={(e) => setSelectedGenre(e.target.value)}
-        >
-          <option value="">Seleccionar</option>
-          {listGenres.map((genre, index) => (
-            <option key={index} value={genre}>
-              {genre}
-            </option>
-          ))}
-        </select>
         <BookList
           books={bookData}
           readList={readList}
