9470dd8ed1c6ee68dedbc0c0560aaf4c1471cba3
Dagadev <gacub20ru@gmail.com>
2023-07-30 23:43:42 -0600

    update read & update styles

--- pruebas/01-reading-list/dagamdev/src/components/read/ReadList.tsx
+++ pruebas/01-reading-list/dagamdev/src/components/read/ReadList.tsx
@@ -2,21 +2,23 @@
 
 import styles from './read.module.scss'
 import { useReadingBooks } from '@/hooks/useReadingBooks'
-import Book from '../book/BookCard'
 import BooksCounter from '../book/BooksCounter'
+import ReadCard from './ReadCard'
 
 export default function ReadList(){
   const { openList, readingBooks } = useReadingBooks()
 
   return (
     <section className={`${styles.read} ${openList ? styles.open : ''}`}>
-      {readingBooks.length ? 
-        <BooksCounter text={`Lista de lectura con {} libros`} /> :
-        <strong>Lista de lectura bacia</strong>
-      }
+      <div className={styles['read-title']}>
+        {readingBooks.length ? 
+          <BooksCounter text={`Lista de lectura con {} libros`} /> :
+          <strong>Lista de lectura bacia</strong>
+        }
+      </div>
 
       <ul className={styles['read-list']}>
-        {readingBooks.map(b=> <Book key={b.ISBN} book={b} />)}
+        {readingBooks.map(b=> <ReadCard key={b.ISBN} book={b} />)}
       </ul>
     </section>
   )
