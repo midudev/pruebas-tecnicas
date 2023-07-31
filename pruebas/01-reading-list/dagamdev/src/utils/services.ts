9660264083425d8474d4b73afec6de1f80d280de
Dagadev <gacub20ru@gmail.com>
2023-07-30 23:19:54 -0600

    update get books and get books ISBNs

--- pruebas/01-reading-list/dagamdev/src/utils/services.ts
+++ pruebas/01-reading-list/dagamdev/src/utils/services.ts
@@ -1,6 +1,6 @@
 import books from './books.json'
 import { LOCAL_KEYS } from './config'
-import { Book } from './types'
+import { Book, LocalKeys } from './types'
 
 export function getBooks() {
   return books.library.map(({book})=> book)
@@ -12,17 +12,33 @@ export function getBook(bookISBN: string) {
 
 export const windowExist = typeof window != 'undefined'
 
+export function getBooksISBNs(localKey: LocalKeys) {
+  if(typeof localStorage != 'undefined'){
+    const localISBNsData = localStorage.getItem(localKey)
+    return localISBNsData ? JSON.parse(localISBNsData) as string[] : []
+  }
+
+  return []
+}
+
 export function getBooksByISBNs(ISBNs: string[]) {
   return getBooks().filter(f=> ISBNs.some((s: string)=> s == f.ISBN))
 }
 
-export function getReadingBooks() {
-  const localBooksData = localStorage.getItem(LOCAL_KEYS.READING_BOOKS)
-  const readingBooksISBNs: string[] = localBooksData ? JSON.parse(localBooksData) : undefined
+function getBooksFromLocalStorage(localKey: LocalKeys) {
+  const readingBooksISBNs = getBooksISBNs(localKey)
   
   if(Array.isArray(readingBooksISBNs)) return getBooksByISBNs(readingBooksISBNs)
   
   return []
+}
+
+export function getReadingBooks() {
+  return getBooksFromLocalStorage(LOCAL_KEYS.READING_BOOKS)
+}
+
+export function getPriorityBooks() {
+  return getBooksFromLocalStorage(LOCAL_KEYS.PRIORITY_BOOKS)
 }
 
 export function updateReadingBooks(booksISBNs: Book[]) {
