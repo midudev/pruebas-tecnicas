0f41ea5a2098f38c59fdf9da1470108f2eda998d
TobysNap <naptobys17@gmail.com>
2023-07-30 23:57:30 -0300

    Guardar lista + n° libros en lista

--- pruebas/01-reading-list/TobysNap/src/app/shared/services/list.service.ts
+++ pruebas/01-reading-list/TobysNap/src/app/shared/services/list.service.ts
@@ -1,11 +1,12 @@
-import { Injectable, computed, signal } from '@angular/core';
+import { Injectable, computed, signal, effect } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Book } from '../interfaces/Book';
-import { Subject, of } from 'rxjs';
+import { Subject, from, of } from 'rxjs';
 
 export interface ListState {
   list: Book[];
   loaded: boolean;
+  quantity: number;
 }
 
 @Injectable({
@@ -15,10 +16,13 @@ export class ListService {
   private state = signal<ListState>({
     list: [],
     loaded: false,
+    quantity: 0,
   });
 
   list = computed(() => this.state().list);
   loaded = computed(() => this.state().loaded);
+  quantity = computed(() => this.state().quantity);
+
 
   private listLoaded$ = this.loadList();
   add$ = new Subject<Book>();
@@ -30,6 +34,7 @@ export class ListService {
         ...state,
         list,
         loaded: true,
+        quantity: list.length
       }));
     });
 
@@ -37,6 +42,7 @@ export class ListService {
       this.state.update((state) => ({
         ...state,
         list: [...state.list, listItem],
+        quantity: state.quantity + 1,
       }));
     });
 
@@ -44,12 +50,24 @@ export class ListService {
       this.state.update((state) => ({
         ...state,
         list: state.list.filter((listItem) => listItem.title !== item.title),
+        quantity: state.quantity - 1,
       }));
     });
+
+    effect(() => {
+      if (this.loaded()) {
+        this.saveList(this.list());
+      }
+    })
   }
 
   loadList() {
     const list = window.localStorage.getItem('readingList');
     return of(list ? (JSON.parse(list) as Book[]) : []);
   }
+
+  saveList(list: Book[]) {
+    localStorage.setItem('readingList', JSON.stringify(list));
+  }
+
 }
