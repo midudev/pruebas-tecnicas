9e4c25bbf8a36eaf8d8540153167384053d9cad9
danmondra <danimonta15@gmail.com>
2023-08-03 21:58:22 -0600

    Refactor books origin adding a asynchronous origin

--- pruebas/01-reading-list/danmondra/src/context/books.jsx
+++ pruebas/01-reading-list/danmondra/src/context/books.jsx
@@ -1,28 +1,53 @@
-import { createContext } from 'react'
-import books from '../mocks/newBooks.json'
+import { createContext, useEffect, useState } from 'react'
+import mockedBooks from '../mocks/newBooks.json'
 
 export const BooksContext = createContext()
 
-export const BooksProvider = ({ children }) => {
-  const mappedBooks = books.library.map(({ book }) => {
-    return {
-      title: book.title,
-      pages: book.pages,
-      genre: book.genre,
-      cover: book.cover,
-      synopsis: book.synopsis,
-      year: book.year,
-      ISBN: book.ISBN,
-      author: {
-        name: book.author.name,
-        otherBooks: book.author.otherBooks
-      }
-    }
+async function getBooks() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(mockedBooks)
+    }, 900)
   })
+}
+
+export const BooksProvider = ({ children }) => {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    // TODO --- Try with IIFE
+    const initBooks = async () => {
+      const prueba = await getBooks()
+
+      const mappedBooks = prueba?.library?.map(({ book }) => {
+        return {
+          title: book.title,
+          pages: book.pages,
+          genre: book.genre,
+          cover: book.cover,
+          synopsis: book.synopsis,
+          year: book.year,
+          ISBN: book.ISBN,
+          author: {
+            name: book.author.name,
+            otherBooks: book.author.otherBooks
+          }
+        }
+      })
+
+      setBooks(mappedBooks)
+    }
+
+    initBooks()
+  }, [])
+
+  // get all genres here
+  //
+  // get book with more pages here
 
   return (
     <BooksContext.Provider
-      value={{ books: mappedBooks }}
+      value={{ books }}
     >
       {children}
     </BooksContext.Provider>
