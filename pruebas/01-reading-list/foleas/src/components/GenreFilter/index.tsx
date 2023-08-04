485501f077ca9c0f5712daeb2c30630ccbd9c6dc
Foleas <sebastianfilipetto@gmail.com>
2023-08-04 14:17:24 -0300

    Style: select styles class

--- pruebas/01-reading-list/foleas/src/components/GenreFilter/index.tsx
+++ pruebas/01-reading-list/foleas/src/components/GenreFilter/index.tsx
@@ -1,5 +1,5 @@
 import { useStore } from "../../store/store";
-import { textColorAnimationClass } from "../../utils/tailwind";
+import { selectStyle, textColorAnimationClass } from "../../utils/tailwind";
 
 const GenderFilter = () => {
   const { genres, currentGenre, changeCurrentGenre } = useStore();
@@ -19,7 +19,7 @@ const GenderFilter = () => {
       <select
         id="genre"
         value={currentGenre}
-        className="transition duration-300 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className={selectStyle}
         onChange={({ target }) => selectChangeHandler(target.value)}
       >
         <option value="">Todos</option>
