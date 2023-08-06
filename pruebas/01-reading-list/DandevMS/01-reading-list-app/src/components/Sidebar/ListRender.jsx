85e2b5b47ec3a5bfd8574fc3a7495ca8f7feed4c
Danilo Antonio <antoniomcs21@hotmail.com>
2023-08-05 19:02:48 -0500

    commit refres branch

--- pruebas/01-reading-list/DandevMS/01-reading-list-app/src/components/Sidebar/ListRender.jsx
+++ pruebas/01-reading-list/DandevMS/01-reading-list-app/src/components/Sidebar/ListRender.jsx
@@ -11,15 +11,6 @@ function ListRender() {
   // usando el hook personalizado
   const dataBooks = useBooks(readingList);
 
-  // obtenet la data del localStorage
-/*   useEffect(() => {
-    const storedReadingList  = JSON.parse(localStorage.getItem("readingList"));
-    if (storedReadingList ) {
-      setReadingList(storedReadingList );
-    }
-  }, []); */ //eslint-disable-line
-
-
   return (
     <div className=" mt-6 grid grid-cols-2 gap-x-6 gap-y-5 sm:grid-cols-2 ">
       <CardBooks dataBooks={dataBooks} removeBook={removeBook} addRemove={true}/>
