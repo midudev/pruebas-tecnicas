8b8158e7fc06f151f739d50b152daf0e078eb97f
sfcc5555 <sfcc.5555@gmail.com>
2023-08-08 18:29:38 -0500

    update

--- pruebas/01-reading-list/SFCC5555/src/test/ExternalLink.test.jsx
+++ pruebas/01-reading-list/SFCC5555/src/test/ExternalLink.test.jsx
@@ -1,15 +1,15 @@
 import { render, screen} from '@testing-library/react'
-import { describe, it } from 'vitest'
+import { describe, test} from 'vitest'
 import { ExternalLink } from '../components/ExternalLink';
 
 describe('ExternalLink', () => {
 
-	it('renders ExternalLink component without errors', () => {
+	test('renders ExternalLink component without errors', () => {
 		// Render the component
 		render(<ExternalLink />);
 	
-		screen.getByText('Challenge by')
-	
+		screen.getByText('Challenge by');
+
 	});
 
 })
