04a842a5c3312febe35197148f99ef034127de4b
Fernando Chazarreta <fernandochaza@gmail.com>
2023-08-12 16:08:22 -0300

    fix: Add mediaqueries

--- pruebas/01-reading-list/fernandochaza/book-reading-list/src/components/SearchForm/styles.js
+++ pruebas/01-reading-list/fernandochaza/book-reading-list/src/components/SearchForm/styles.js
@@ -2,17 +2,25 @@ import styled from 'styled-components'
 
 const StyledAside = styled.aside`
   width: 100%;
-  border: 1px solid white;
   margin: 0 auto 1rem auto;
   padding: 1rem 2rem;
   border-radius: 16px;
+  transition: all 1s ease;
+  background-color: ${(props) => props.theme.mainBg};
+  box-shadow: 0 0 20px -8px ${(props) => props.theme.accent1Color};
 
   @media screen and (min-width: 576px) {
-    .search {
-      width: 240px;
-      margin: 0 auto;
-    }
+    width: 240px;
+    margin: 0 auto;
+    box-shadow: 0 0 40px -8px ${(props) => props.theme.accent1Color};
   }
+`
+
+const StyledFormTitle = styled.h2`
+  text-align: center;
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0.5rem;
 `
 
 const StyledForm = styled.form`
@@ -29,6 +37,7 @@ const StyledTextInput = styled.input`
   border: none;
   outline: none;
   background-color: ${(props) => props.theme.black};
+  color: ${(props) => props.theme.mainTxt};
   border-radius: 4px;
   box-shadow: 0 0 2px ${(props) => props.theme.mainTxt};
   height: 40px;
@@ -39,33 +48,47 @@ const StyledTextInput = styled.input`
 
 const PagesFilterContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+
+  @media screen and (min-width: 576px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    row-gap: 12px;
+
+    :not(:first-child) {
+      place-items: center;
+    }
+  }
+`
+
+const StyledFilterTitle = styled.p`
+
+@media screen and (min-width: 576px) {
+  grid-row: 1;
+  grid-column: 1/3;
+}
 `
 
 const StyledRadioLabel = styled.label`
-  line-height: 2rem;
+  display: flex;
+  flex-direction: column;
+  line-height: 1.5;
 `
 
 const StyledRadioInput = styled.input`
-  margin-right: .5rem;
-`
 
-const StyledSubmitButton = styled.button`
-  background-color: ${(props) => props.theme.mainBg};
-  border: 1px solid ${(props) => props.theme.mainTxt};
-  color: ${(props) => props.theme.mainTxt};
-  border-radius: 4px;
-  padding: 0.5rem;
-  margin-top: 1rem;
 `
 
 export {
   StyledAside,
+  StyledFormTitle,
   StyledForm,
   StyledTextInput,
   PagesFilterContainer,
+  StyledFilterTitle,
   StyledRadioLabel,
   StyledTextLabel,
-  StyledSubmitButton,
   StyledRadioInput
 }
