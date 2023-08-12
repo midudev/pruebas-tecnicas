4b9189ac0d724dbfefa8639396ae4d5219bea782
Fernando Chazarreta <fernandochaza@gmail.com>
2023-08-12 16:14:22 -0300

    feat: Add glow banckground image

--- pruebas/01-reading-list/fernandochaza/book-reading-list/src/pages/RootLayoutStyles.js
+++ pruebas/01-reading-list/fernandochaza/book-reading-list/src/pages/RootLayoutStyles.js
@@ -2,10 +2,7 @@ import styled from 'styled-components'
 
 const StyledMainContainer = styled.div`
   height: 100vh;
-  background: ${(props) => props.theme.mainBg};
   color: ${(props) => props.theme.mainTxt};
-  /* background-image: radial-gradient(${(props) => props.theme.secondaryTxt} 1px, transparent 0);
-  background-size: 48px 48px; */
 `
 
 const StyledMain = styled.main`
@@ -16,4 +13,16 @@ const StyledMain = styled.main`
   flex-direction: column;
 `
 
-export { StyledMainContainer, StyledMain }
+const StyledImg = styled.img`
+  background: ${(props) => props.theme.mainBg};
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  max-width: 100vw;
+  height: 100%;
+  z-index: -1;
+`
+
+export { StyledMainContainer, StyledMain, StyledImg }
