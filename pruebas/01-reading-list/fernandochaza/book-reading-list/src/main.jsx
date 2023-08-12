b0f43158302ddf1eb17bce84537fc0cb0b88f102
Fernando Chazarreta <fernandochaza@gmail.com>
2023-08-11 23:48:59 -0300

    feat: Create color palette. Add theme provider

--- pruebas/01-reading-list/fernandochaza/book-reading-list/src/main.jsx
+++ pruebas/01-reading-list/fernandochaza/book-reading-list/src/main.jsx
@@ -2,6 +2,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { ThemeProvider } from 'styled-components'
+import { darkTheme } from './theme/themes'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <ThemeProvider theme={darkTheme}>
     <App />
+  </ThemeProvider>
 )
