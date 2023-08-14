cd12fd86b29d68a2ed912eb5aa23d90f8074cd50
Carlos Ochoa <uligibson@hotmail.com>
2023-08-14 13:41:08 -0600

    code refactored

--- pruebas/01-reading-list/CarlosUlisesOchoa/src/components/BookList/BookList.jsx
+++ pruebas/01-reading-list/CarlosUlisesOchoa/src/components/BookList/BookList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { Range } from 'react-range'
 import Book from '@/components/Book/Book'
 import '@/components/BookList/BookList.css'
@@ -14,18 +14,35 @@ const BookList = ({ onAddBookToReadingListClick }) => {
   const [genres, setGenres] = useState([]) // ['Fantasy', 'Horror', 'Science Fiction', etc...
   const [genreFilter, setGenreFilter] = useState('')
   const [maxPagesFilter, setMaxPagesFilter] = useState([1]) // maximum page filter
+  const [debouncedMaxPagesFilter, setDebouncedMaxPagesFilter] = useState([1])
   const [maxNumberOfPagesValue, setMaxNumberOfPagesValue] = useState(2)
+  const firstRender = useRef(true)
 
   useEffect(() => {
     // get all the genres without duplicates
     const genresArray = [...new Set(booksArray.map((book) => book.genre))]
     // get the maximum number of pages
     const maxPages = Math.max(...booksArray.map((book) => book.pages))
-    console.log(maxPages) // tetemp
-    setMaxPagesFilter([maxPages])
+    // console.log(maxPages) // tetemp
     setMaxNumberOfPagesValue(maxPages)
+    // Only set maxPagesFilter on the first render
+    if (firstRender.current) {
+      setMaxPagesFilter([maxPages])
+      firstRender.current = false
+    }
+    if (maxPagesFilter[0] > maxPages) {
+      setMaxPagesFilter([maxPages])
+    }
     setGenres(genresArray)
   }, [booksArray])
+
+  useEffect(() => {
+    const delayInputTimeoutId = setTimeout(() => {
+      setDebouncedMaxPagesFilter(maxPagesFilter)
+      // console.log(debouncedMaxPagesFilter) // tetemp
+    }, 500)
+    return () => clearTimeout(delayInputTimeoutId)
+  }, [maxPagesFilter, 500])
 
   useEffect(() => {
     const availableBooksArray = booksArray.filter((book) => {
@@ -33,13 +50,17 @@ const BookList = ({ onAddBookToReadingListClick }) => {
         (readingBook) => readingBook.ISBN === book.ISBN
       )
       const matchesGenre = genreFilter ? book.genre === genreFilter : true
-      const matchesPageRange = book.pages <= maxPagesFilter[0] // condition to filter by page range
+      const matchesPageRange = book.pages <= debouncedMaxPagesFilter[0] // condition to filter by page range
       return notInReadingList && matchesGenre && matchesPageRange
     })
     setAvailableBooks(availableBooksArray)
-    console.log(genreFilter) // tetemp
-    console.log(maxPagesFilter) // tetemp
-  }, [readingListBooksArray, genreFilter, maxPagesFilter])
+    // console.log(genreFilter) // tetemp
+    // console.log(debouncedMaxPagesFilter) // tetemp
+  }, [readingListBooksArray, genreFilter, debouncedMaxPagesFilter])
+
+  function handleMaxPagesFilterChange(e) {
+    setMaxPagesFilter(e)
+  }
 
   return (
     <>
@@ -59,20 +80,20 @@ const BookList = ({ onAddBookToReadingListClick }) => {
         </div>
         <div className='books__filters__pages'>
           {/* input field for page range filter */}
-          <span className='mr-2'>Filter by pages range</span>
+          <span className='mr-2'>Filter by pages range (1 - {maxPagesFilter})</span>
           <Range
             min={1}
             max={maxNumberOfPagesValue}
             step={1}
             values={maxPagesFilter}
-            onChange={(values) => setMaxPagesFilter(values)}
+            onChange={handleMaxPagesFilterChange}
             renderTrack={({ props, children }) => (
               <div
                 {...props}
                 style={{
                   ...props.style,
-                  height: '6px',
-                  width: '100%',
+                  height: '3px',
+                  width: '30%',
                   backgroundColor: '#ccc',
                 }}>
                 {children}
@@ -83,9 +104,9 @@ const BookList = ({ onAddBookToReadingListClick }) => {
                 {...props}
                 style={{
                   ...props.style,
-                  height: '42px',
-                  width: '42px',
-                  backgroundColor: '#999',
+                  height: '21px',
+                  width: '21px',
+                  backgroundColor: '#fff',
                 }}
               />
             )}
