032c75b84bf4fe4f025421ba385402416a290eac
Fernando Chazarreta <fernandochaza@gmail.com>
2023-08-15 17:44:21 -0300

    fix: Add attibutes to personalized button

--- pruebas/01-reading-list/fernandochaza/book-reading-list/src/components/ColoredButton/index.jsx
+++ pruebas/01-reading-list/fernandochaza/book-reading-list/src/components/ColoredButton/index.jsx
@@ -1,10 +1,10 @@
 import propTypes from 'prop-types'
 import { StyledButtonContainer, StyledButton } from './styles'
 
-const ColoredButton = ({ children }) => {
+const ColoredButton = ({ children, type, disabled }) => {
   return (
     <StyledButtonContainer>
-      <StyledButton>{children}</StyledButton>
+      <StyledButton type={type} disabled={disabled}>{children}</StyledButton>
     </StyledButtonContainer>
   )
 }
@@ -12,5 +12,7 @@ const ColoredButton = ({ children }) => {
 export default ColoredButton
 
 ColoredButton.propTypes = {
-  children: propTypes.string.isRequired
+  children: propTypes.string.isRequired,
+  type: propTypes.string,
+  disabled: propTypes.bool
 }
