96b36858acb0d50a35dfa47f9ba1d158660c38b8
Carlos Ochoa <uligibson@hotmail.com>
2023-08-16 16:37:28 -0600

    tailwind config updated

--- pruebas/01-reading-list/CarlosUlisesOchoa/tailwind.config.js
+++ pruebas/01-reading-list/CarlosUlisesOchoa/tailwind.config.js
@@ -14,11 +14,6 @@ export default {
         transparent: 'transparent',
         current: 'currentColor',
       },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
     },
   },
   plugins: [],
