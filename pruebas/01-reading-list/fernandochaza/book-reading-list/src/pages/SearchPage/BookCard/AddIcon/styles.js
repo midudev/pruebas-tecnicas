37edea347e4be0b4a678c4862bd8c95e9b92d5b6
Fernando Chazarreta <fernandochaza@gmail.com>
2023-08-16 23:41:15 -0300

    feat: Create dynamic Icon to add/remove book from list

---
+++ pruebas/01-reading-list/fernandochaza/book-reading-list/src/pages/SearchPage/BookCard/AddIcon/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const StyledAddIcon = styled.svg`
+  position: absolute;
+  right: 0;
+  top: 10px;
+`
+
+const StyledCheck = styled.svg`
+  position: absolute;
+  top: 0;
+  right: 0;
+`
+
+const StyledAddButton = styled.button`
+  border: none;
+  cursor: pointer;
+`
+
+export { StyledAddIcon, StyledCheck, StyledAddButton }
