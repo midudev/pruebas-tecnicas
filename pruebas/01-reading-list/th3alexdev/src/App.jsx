3cb423818ae79fe9daca86c6e70c8e6d7627f22e
AleKs <alexanderfranciscog@gmail.com>
2023-08-18 13:09:03 -0400

    Add UserSavedBooks component and useSidebar hook

--- pruebas/01-reading-list/th3alexdev/src/App.jsx
+++ pruebas/01-reading-list/th3alexdev/src/App.jsx
@@ -3,18 +3,22 @@ import ListOfBooks from "./components/ListOfBooks";
 import Filters from "./components/Filters";
 import FiltersProvider from "./context/contextFiltersProvider";
 import Header from "./components/Header";
+import UserSavedBooks from "./components/UserSavedBooks";
+import { useSidebar } from "./hooks/useSidebar";
 
 export default function App() {
+  const [showSidebar, toggleSidebar] = useSidebar();
 
   return (
     <>
       <FiltersProvider>
-        <Header />
-        <main className="grid grid-cols-[20%,1fr]">
-            <Filters />
-            <ListOfBooks />
+        <Header showSidebar={showSidebar} toggleSidebar={toggleSidebar} />
+        <main className="relative grid grid-cols-[20%,1fr]">
+          <UserSavedBooks showSidebar={showSidebar} />
+          <Filters />
+          <ListOfBooks />
         </main>
       </FiltersProvider>
     </>
-  )
+  );
 }
