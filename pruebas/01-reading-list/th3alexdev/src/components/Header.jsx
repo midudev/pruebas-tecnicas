164cfe4e444e0d5b6a0cea6520dceae8dd7c559d
AleKs <alexanderfranciscog@gmail.com>
2023-08-18 13:18:42 -0400

    Add saved books icon and sidebar toggle functionality

--- pruebas/01-reading-list/th3alexdev/src/components/Header.jsx
+++ pruebas/01-reading-list/th3alexdev/src/components/Header.jsx
@@ -1,31 +1,32 @@
-import { useContext, useRef } from 'react';
-import { FiltersContext } from '../context/contextFiltersProvider';
-import { SearchIcon, CloseIcon } from './Icons';
+import { useContext, useRef } from "react";
+import { FiltersContext } from "../context/contextFiltersProvider";
+import { SearchIcon, CloseIcon } from "./Icons";
+import { SavedBooksIconRegular, SavedBooksIconSolid } from "./Icons";
 
-export default function Header() {
+export default function Header({showSidebar, toggleSidebar}) {
   const searchInputRef = useRef();
   const { filters, setFilters } = useContext(FiltersContext);
 
   const { sortByTitle } = filters;
 
   const removeSortByTitle = () => {
-    setFilters({ ...filters, sortByTitle: '' });
-    searchInputRef.current.value = ''
+    setFilters({ ...filters, sortByTitle: "" });
+    searchInputRef.current.value = "";
   };
 
   return (
     <>
-      <header className='absolute right-0 w-4/5 flex flex-col items-center py-6'>
+      <header className="absolute right-0 w-4/5 flex flex-col items-center py-6 z-50">
         <form
           onSubmit={(e) => {
             e.preventDefault();
           }}
         >
-          <div className='relative'>
+          <div className="relative">
             <input
-              type='text'
-              placeholder='¿En qué libro estás pensando?'
-              className='w-[400px] px-5 pr-10 py-2 rounded-full'
+              type="text"
+              placeholder="¿En qué libro estás pensando?"
+              className="w-[400px] px-5 pr-10 py-2 rounded-full"
               onInput={(e) => {
                 setFilters({ ...filters, sortByTitle: e.target.value });
               }}
@@ -33,7 +34,7 @@ export default function Header() {
             />
             <button
               className={`absolute top-1/2 right-4 translate-y-[-50%] ${
-                sortByTitle ? '' : 'pointer-events-none'
+                sortByTitle ? "" : "pointer-events-none"
               }`}
             >
               {sortByTitle ? (
@@ -46,6 +47,18 @@ export default function Header() {
             </button>
           </div>
         </form>
+        <div className="absolute right-9 top-1/2 translate-y-[-50%]">
+          <span className="absolute right-[-0.10rem] top-[-1.5px] z-50 p-1 px-1.5 leading-3 bg-red-500 rounded-full font-semibold">
+            0
+          </span>
+          <button className="relative rotate-[30deg] p-2 z-50" onClick={toggleSidebar}>
+            <span className="relative z-50">
+              {
+                showSidebar ? <SavedBooksIconSolid /> : <SavedBooksIconRegular />
+              }
+            </span>
+          </button>
+        </div>
       </header>
     </>
   );
