c064b2f57e4c239d73acc76936fd8b0e19285a2c
Fernando Chazarreta <fernandochaza@gmail.com>
2023-08-19 14:52:13 -0300

    fix: Refactor utility function

--- pruebas/01-reading-list/fernandochaza/book-reading-list/src/Utils/updateReadingList.js
+++ pruebas/01-reading-list/fernandochaza/book-reading-list/src/Utils/updateReadingList.js
@@ -2,11 +2,11 @@ export function updateReadingList(bookData) {
   const storedData = localStorage.getItem('readingList')
   const readingList = storedData ? JSON.parse(storedData) : []
 
-  if (!storedData?.includes(bookData)) {
-    const updateReadingList = [...readingList, bookData]
-    localStorage.setItem('readingList', JSON.stringify(updateReadingList))
-  } else {
-    const updatedReadingList = readingList.filter((item) => item !== bookData)
-    localStorage.setItem('readingList', JSON.stringify(updatedReadingList))
-  }
+  const isBookInList = readingList.some((book) => book.id === bookData.id)
+
+  const updatedReadingList = isBookInList
+    ? readingList.filter((book) => book.id !== bookData.id)
+    : [...readingList, bookData]
+
+  localStorage.setItem('readingList', JSON.stringify(updatedReadingList))
 }
