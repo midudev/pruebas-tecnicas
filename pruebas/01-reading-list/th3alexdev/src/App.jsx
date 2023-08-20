1254109ebb81d1ccba445c66c9d2b31a8c0ee318
AleKs <alexanderfranciscog@gmail.com>
2023-08-19 22:19:38 -0400

    Add useEffect to control body overflow based on sidebar visibility

--- pruebas/01-reading-list/th3alexdev/src/App.jsx
+++ pruebas/01-reading-list/th3alexdev/src/App.jsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ListOfBooks from "./components/ListOfBooks";
 import Filters from "./components/Filters";
 import FiltersProvider from "./context/contextFiltersProvider";
 import Header from "./components/Header";
-import UserSavedBooks from "./components/UserSavedBooks";
+import Sidebar from "./components/Sidebar";
 import { useSidebar } from "./hooks/useSidebar";
 
 export default function App() {
   const [showSidebar, toggleSidebar] = useSidebar();
+
+  useEffect(() => {
+    if(showSidebar) document.body.style.overflowY = 'hidden';
+    else document.body.style.overflowY = 'auto';
+  }, [showSidebar]);
 
   return (
     <>
       <FiltersProvider>
         <Header showSidebar={showSidebar} toggleSidebar={toggleSidebar} />
         <main className="relative grid grid-cols-[20%,1fr]">
-          <UserSavedBooks showSidebar={showSidebar} />
+          <Sidebar showSidebar={showSidebar} toggleSidebar={toggleSidebar} />
           <Filters />
           <ListOfBooks />
         </main>
