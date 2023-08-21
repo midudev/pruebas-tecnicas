8280d501968fe8d49a544c05f93dfb14f0e74a89
Fernando Chazarreta <fernandochaza@gmail.com>
2023-08-21 16:18:03 -0300

    fea(Toaster): Add notification on data manage hooks

--- pruebas/01-reading-list/fernandochaza/book-reading-list/src/hooks/useAddToReadingList.js
+++ pruebas/01-reading-list/fernandochaza/book-reading-list/src/hooks/useAddToReadingList.js
@@ -3,6 +3,8 @@ import { useCallback, useEffect, useState } from 'react'
 import { useAtom } from 'jotai'
 import { userCompletedBooks, userReadingList } from '../context/atoms'
 
+import { toast } from 'sonner'
+
 import { updateReadingList } from '../Utils/updateReadingList'
 import { updateCompletedBooks } from '../Utils/updateCompletedBooks'
 
@@ -20,26 +22,29 @@ export function useAddToReadingList(bookData) {
 
   const handleAddBookToGlobal = useCallback(
     (bookData) => {
+      const { title } = bookData.volumeInfo
+      const { id } = bookData
+
       const isCurrentInReadingList = readingList.some(
         (book) => book.id === bookData.id
       )
 
-      const isCompleted = completedBooks.some((book) => book.id === bookData.id)
+      const isCompleted = completedBooks.some((book) => book.id === id)
 
       if (!isCurrentInReadingList) {
         setReadingList((prev) => [...prev, bookData])
         setIsInReadingList((prev) => !prev)
 
+        toast.success(`${title} was added to your reading list`)
+
         if (isCompleted) {
-          setCompletedBooks((books) =>
-            books.filter((book) => book.id !== bookData.id)
-          )
+          setCompletedBooks((books) => books.filter((book) => book.id !== id))
           updateCompletedBooks(bookData)
         }
       } else {
-        setReadingList((books) =>
-          books.filter((book) => book.id !== bookData.id)
-        )
+        setReadingList((books) => books.filter((book) => book.id !== id))
+
+        toast.error(`${title} was removed from your reading list`)
       }
     },
     [setReadingList, readingList, setCompletedBooks, completedBooks]
