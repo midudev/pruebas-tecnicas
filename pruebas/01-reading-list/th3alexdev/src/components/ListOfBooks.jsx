e72396af7954d27a246eeca55e2c5e7e04dc525a
AleKs <alexanderfranciscog@gmail.com>
2023-08-23 15:43:16 -0400

    Add book addition functionality and display error messages

--- pruebas/01-reading-list/th3alexdev/src/components/ListOfBooks.jsx
+++ pruebas/01-reading-list/th3alexdev/src/components/ListOfBooks.jsx
@@ -3,10 +3,13 @@ import { FiltersContext } from "../context/contextFiltersProvider";
 import { BooksContext } from "../context/contextBooksProvider";
 import Loader from "./Loader";
 import { AddBookIcon } from "./Icons";
+import ErrorMessage from "./ErrorMessage";
+import { UserListContext } from "../context/contextUserListProvider";
 
 export default function ListOfBooks() {
   const { filters, filteredBooks } = useContext(FiltersContext);
-  const { loading } = useContext(BooksContext);
+  const { loading, error } = useContext(BooksContext);
+  const { addBook, removeBook } = useContext(UserListContext)
 
   const { sortByPages } = filters;
 
@@ -15,46 +18,50 @@ export default function ListOfBooks() {
       <article className="py-24">
         {loading ? (
           <Loader />
+        ) : error ? (
+          <ErrorMessage error={error} />
         ) : (
           <ul className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-x-4 gap-y-5 px-2">
-            {filteredBooks?.map(
-              ({ title, cover, ISBN, author, year, pages }) => (
-                <li
-                  className="relative py-4 px-3 flex flex-nowrap flex-col items-center rounded-md hover:bg-card-hover group"
-                  key={ISBN}
+            {filteredBooks?.map((book) => (
+              <li
+                className="relative py-4 px-3 flex flex-nowrap flex-col items-center rounded-md hover:bg-card-hover group"
+                key={book.ISBN}
+                data-bookisbn={book.ISBN}
+              >
+                <button
+                  onClick={() => addBook(book)}
+                  className="absolute bg-[#64748b66]  rounded-full right-6 top-6 opacity-0 group-hover:opacity-100 hover:bg-slate-700 transition-colors"
                 >
-                  <button className="absolute bg-[#64748b66]  rounded-full right-6 top-6 opacity-0 group-hover:opacity-100 hover:bg-slate-700 transition-colors">
-                    <span className="p-2 opacity-70 hover:opacity-100 block">
-                      <AddBookIcon />
-                    </span>
-                  </button>
-                  <img
-                    src={cover}
-                    alt={title}
-                    className="w-[95%] mb-4 aspect-[9/14] object-cover rounded-sm pointer-events-none"
-                  />
-                  <div className="w-[95%]">
-                    <h1
-                      className="text-lg h-max font-bold leading-6 line-clamp-2 uppercase mb-2"
-                      title={title}
+                  <span className="p-2 opacity-70 hover:opacity-100 block">
+                    <AddBookIcon />
+                  </span>
+                </button>
+                <img
+                  src={book.cover}
+                  alt={`Cubierta de ${book.title}`}
+                  className="w-[95%] mb-4 aspect-[9/14] object-cover rounded-sm pointer-events-none"
+                />
+                <div className="w-[95%]">
+                  <h1
+                    className="text-lg h-max font-bold leading-6 line-clamp-2 uppercase mb-2"
+                    title={book.title}
+                  >
+                    {book.title}
+                  </h1>
+                  <p className="text-sm opacity-60 mb-2">{book.author.name}</p>
+                  <div className="flex justify-between">
+                    <p className="text-base opacity-60">{book.year}</p>
+                    <p
+                      className={`text-base ${
+                        sortByPages ? "text-secondary" : "opacity-60"
+                      }`}
                     >
-                      {title}
-                    </h1>
-                    <p className="text-sm opacity-60 mb-2">{author.name}</p>
-                    <div className="flex justify-between">
-                      <p className="text-base opacity-60">{year}</p>
-                      <p
-                        className={`text-base ${
-                          sortByPages ? "text-secondary" : "opacity-60"
-                        }`}
-                      >
-                        {pages} pág.
-                      </p>
-                    </div>
+                      {book.pages} pág.
+                    </p>
                   </div>
-                </li>
-              )
-            )}
+                </div>
+              </li>
+            ))}
           </ul>
         )}
       </article>
