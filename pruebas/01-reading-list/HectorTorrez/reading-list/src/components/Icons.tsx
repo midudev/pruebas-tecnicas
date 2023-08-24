3c12ccd2ba4845bd946566ccde218ca8c0e8144a
Hector <torrezhectorb@gmail.com>
2023-08-24 16:42:22 -0600

    new icon

--- pruebas/01-reading-list/HectorTorrez/reading-list/src/components/Icons.tsx
+++ pruebas/01-reading-list/HectorTorrez/reading-list/src/components/Icons.tsx
@@ -7,3 +7,12 @@ export const Delete = () => {
     </svg>
   )
 }
+
+export const Search = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+     <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+    </svg>
+  )
+}
+
