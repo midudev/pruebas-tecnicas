236c1f718e4bdaa33ba393c376cce0bb6d607318
AleKs <alexanderfranciscog@gmail.com>
2023-08-25 21:24:33 -0400

    Add custom breakpoints

--- pruebas/01-reading-list/th3alexdev/tailwind.config.js
+++ pruebas/01-reading-list/th3alexdev/tailwind.config.js
@@ -16,6 +16,21 @@ export default {
         'primary': '#009687',
         'secondary': '#FFD440',
         'sidebar': '#282e33'
+      },
+      screens: {
+        'xs': '300px',
+        '2xs': '380px',
+        '3xs': '410px',
+        '4xs': '450px',
+        '5xs': '540px',
+        '6xs': '570px',
+        '7xs': '585px',
+        'sm': '640px',
+        '2sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1440px',
       }
     },
   },
