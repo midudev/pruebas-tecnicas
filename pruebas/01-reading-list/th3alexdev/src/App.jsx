6325974102adebb06438c619a2f35d6a6496dba7
AleKs <alexanderfranciscog@gmail.com>
2023-08-29 20:48:49 -0400

    Refactor structure and components

--- pruebas/01-reading-list/th3alexdev/src/App.jsx
+++ pruebas/01-reading-list/th3alexdev/src/App.jsx
@@ -4,7 +4,8 @@ import Filters from "./components/Filters";
 import FiltersProvider from "./context/contextFiltersProvider";
 import ReadListProvider from "./context/contextUserListProvider";
 import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
+import SlideSidebar from "./components/SlideSidebar";
+import StoreInfo from "./components/StoreInfo";
 import { useSidebar } from "./hooks/useSidebar";
 
 export default function App() {
@@ -19,11 +20,19 @@ export default function App() {
     <>
       <ReadListProvider>
         <FiltersProvider>
-          <Header showSidebar={showSidebar} toggleSidebar={toggleSidebar} />
-          <main className="relative grid sm:grid-cols-[20%,1fr]">
-            <Sidebar showSidebar={showSidebar} toggleSidebar={toggleSidebar} />
-            <Filters />
-            <ListOfBooks />
+          <main className="relative bg-background sm:max-w-[1440px] sm:mx-auto overflow-hidden">
+            <Header showSidebar={showSidebar} toggleSidebar={toggleSidebar} />
+            <SlideSidebar
+              showSidebar={showSidebar}
+              toggleSidebar={toggleSidebar}
+            />
+            <section className="relative grid sm:grid-cols-[20%,1fr]">
+              <aside className="flex flex-col h-max gap-8 pt-24 items-center">
+                <Filters />
+                <StoreInfo />
+              </aside>
+              <ListOfBooks />
+            </section>
           </main>
         </FiltersProvider>
       </ReadListProvider>
