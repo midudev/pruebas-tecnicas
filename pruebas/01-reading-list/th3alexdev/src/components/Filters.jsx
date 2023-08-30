2f3746188f33b74b0bc5afcb3d056950b734828b
AleKs <alexanderfranciscog@gmail.com>
2023-08-29 20:55:05 -0400

    Refactor layout and functionality

--- pruebas/01-reading-list/th3alexdev/src/components/Filters.jsx
+++ pruebas/01-reading-list/th3alexdev/src/components/Filters.jsx
@@ -19,87 +19,82 @@ export default function Filters() {
   });
 
   return (
-    <section className="relative w-full flex flex-nowrap flex-col gap-8 pt-24 sm:pt-6">
-      <div className="w-full sm:w-max relative px-4 sm:fixed flex flex-col gap-8">
-        <div className="hidden sm:block">
-        <Brand />
-        </div>
-        <div>
-          <form onSubmit={(e) => e.preventDefault}>
-            <h3 className="sm:w-4/5 font-semibold text-lg leading-6 mb-2 line-clamp-2 sm:text-balance">
-              <label className="cursor-auto">Por cantidad de páginas</label>
-            </h3>
+    <div className="w-full relative px-4 flex flex-col gap-8">
+      <div>
+        <form onSubmit={(e) => e.preventDefault}>
+          <h3 className="sm:w-4/5 font-semibold text-lg leading-6 mb-2 line-clamp-2 sm:text-balance">
+            <label className="cursor-auto">Por cantidad de páginas</label>
+          </h3>
+          <input
+            aria-valuemin={minPages}
+            aria-valuemax={maxPosiblePages}
+            type="range"
+            min={minPages}
+            max={maxPosiblePages}
+            step="1"
+            className="p-0 whitespace-nowrap w-full h-max mb-4"
+            onChange={(e) => handleChangeRange(e)}
+            ref={numOfPagesRange}
+          />
+          <div
+            className={`flex gap-2 items-center ${
+              sortByPages ? "opacity-100" : "opacity-60"
+            }`}
+          >
+            <label htmlFor="pages" className="uppercase text-sm">
+              Max.
+            </label>
             <input
-              aria-valuemin={minPages}
-              aria-valuemax={maxPosiblePages}
-              type="range"
-              min={minPages}
+              type="number"
+              value={maxPages}
+              id="pages"
+              min={10}
               max={maxPosiblePages}
-              step="1"
-              className="p-0 whitespace-nowrap w-full h-max mb-4"
-              onChange={(e) => handleChangeRange(e)}
-              ref={numOfPagesRange}
+              className="h-[16px] w-10 bg-transparent cursor-default outline-none"
+              onChange={(e) => handleSetRange(e)}
+              ref={numOfPagesInput}
             />
-            <div
-              className={`flex gap-2 items-center ${
-                sortByPages ? "opacity-100" : "opacity-60"
-              }`}
-            >
-              <label htmlFor="pages" className="uppercase text-sm">
-                Max.
-              </label>
-              <input
-                type="number"
-                value={maxPages}
-                id="pages"
-                min={10}
-                max={maxPosiblePages}
-                className="h-[16px] w-10 bg-transparent cursor-default outline-none"
-                onChange={(e) => handleSetRange(e)}
-                ref={numOfPagesInput}
-              />
 
-              <button
-                className={`${
-                  sortByPages ? "pointer-events-auto" : "pointer-events-none"
-                }`}
-                onClick={() => removeRange()}
-                title="Quitar filtro"
-                aria-label="Quitar filtro"
-              >
-                <FilterIcon />
-              </button>
-            </div>
-          </form>
-        </div>
-        <div>
-          <h3 className="font-semibold text-lg mb-2">Por género</h3>
-          <ul className="flex flex-wrap sm:flex-nowrap sm:flex-col gap-[0.3rem]">
-            {filters.genres.map((genre) => (
-              <li key={genre} className="whitespace-nowrap">
-                <button
-                  data-value={genre}
-                  onClick={(e) =>
-                    genre === filters.genre
-                      ? setFilters({ ...filters, genre: "" })
-                      : setFilters({ ...filters, genre })
-                  }
-                >
-                  <span
-                    className={`filter-pill ${
-                      filters.genre === genre
-                        ? "bg-primary text-white opacity-100"
-                        : null
-                    }`}
-                  >
-                    {genre}
-                  </span>
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+            <button
+              className={`${
+                sortByPages ? "pointer-events-auto" : "pointer-events-none"
+              }`}
+              onClick={() => removeRange()}
+              title="Quitar filtro"
+              aria-label="Quitar filtro"
+            >
+              <FilterIcon />
+            </button>
+          </div>
+        </form>
       </div>
-    </section>
+      <div>
+        <h3 className="font-semibold text-lg mb-2">Por género</h3>
+        <ul className="flex flex-wrap sm:flex-nowrap sm:flex-col gap-[0.3rem]">
+          {filters.genres.map((genre) => (
+            <li key={genre} className="whitespace-nowrap">
+              <button
+                data-value={genre}
+                onClick={(e) =>
+                  genre === filters.genre
+                    ? setFilters({ ...filters, genre: "" })
+                    : setFilters({ ...filters, genre })
+                }
+              >
+                <span
+                  className={`filter-pill ${
+                    filters.genre === genre
+                      ? "bg-primary text-white opacity-100"
+                      : null
+                  }`}
+                >
+                  {genre}
+                </span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
