96bb8f41804d497417d9d3aaf68d5f9063ca0a99
AleKs <alexanderfranciscog@gmail.com>
2023-08-30 21:16:22 -0400

    Update styling for Brand component

--- pruebas/01-reading-list/th3alexdev/src/components/Brand.jsx
+++ pruebas/01-reading-list/th3alexdev/src/components/Brand.jsx
@@ -1,6 +1,6 @@
 export default function Brand() {
   return (
-    <h1 className="text-2xl font-extrabold">
+    <h1 className="w-max text-2xl font-extrabold uppercase px-2 bg-primary">
       <a href="#">La Bukstore</a>
     </h1>
   );
