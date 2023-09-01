4841bfeae74c1d77aa0a3a6b7cf7b6e90d25fb56
AleKs <alexanderfranciscog@gmail.com>
2023-08-31 22:12:10 -0400

    Refactor layout structure and responsive design

--- pruebas/01-reading-list/th3alexdev/src/App.jsx
+++ pruebas/01-reading-list/th3alexdev/src/App.jsx
@@ -21,17 +21,18 @@ export default function App() {
       <ReadListProvider>
         <FiltersProvider>
           <main className="relative bg-background sm:max-w-[1440px] sm:mx-auto sm:overflow-hidden">
-            <Header showSidebar={showSidebar} toggleSidebar={toggleSidebar} />
-            <SlideSidebar
-              showSidebar={showSidebar}
-              toggleSidebar={toggleSidebar}
-            />
-            <section className="w-full relative sm:grid sm:grid-cols-[20%,1fr]">
-              <aside className="sm:fixed w-1/5 max-w-[280px] flex flex-col h-max gap-8 pt-24 items-center">
+            <Header showSidebar={showSidebar} toggleSidebar={toggleSidebar}>
+              <SlideSidebar
+                showSidebar={showSidebar}
+                toggleSidebar={toggleSidebar}
+              />
+            </Header>
+            <section className="w-full relative md:grid sm:grid-cols-[20%,1fr]">
+              <aside className="md:fixed md:w-1/5 md:max-w-[280px] flex flex-col h-max gap-8 pt-24 items-center">
                 <Filters />
                 <StoreInfo />
               </aside>
-              {/* <ListOfBooks  */}
+              <ListOfBooks />
             </section>
           </main>
         </FiltersProvider>
