03715fa0099250266cb2ea4f2a28dc5fc353205f
Diego Gonzales <diegoegj14@gmail.com>
2023-09-01 10:45:59 -0500

    tailwind css added

--- pruebas/02-bazar-universal/diego-gonzales/vite.config.ts
+++ pruebas/02-bazar-universal/diego-gonzales/vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite({ devTools: { clickToSource: false } }), tsconfigPaths()],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
