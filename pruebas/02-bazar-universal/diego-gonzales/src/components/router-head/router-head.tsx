b966911cb80c19ffad36bf5fbc35a6e14ec59316
Diego Gonzales <diegoegj14@gmail.com>
2023-09-06 09:54:21 -0500

    favicon changed

--- pruebas/02-bazar-universal/diego-gonzales/src/components/router-head/router-head.tsx
+++ pruebas/02-bazar-universal/diego-gonzales/src/components/router-head/router-head.tsx
@@ -1,5 +1,5 @@
-import { component$ } from "@builder.io/qwik";
-import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik';
+import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
@@ -14,7 +14,7 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="icon" type="image/x-icon" href="/courier_car_logo.ico" />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
