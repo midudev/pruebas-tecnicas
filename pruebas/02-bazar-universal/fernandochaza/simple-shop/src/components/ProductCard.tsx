29b4054fe2fb823b08c7226eff057865d9186c78
Fernando Chazarreta <fernandochaza@gmail.com>
2023-09-07 00:17:20 -0300

    feat: Create product rating component

--- pruebas/02-bazar-universal/fernandochaza/simple-shop/src/components/ProductCard.tsx
+++ pruebas/02-bazar-universal/fernandochaza/simple-shop/src/components/ProductCard.tsx
@@ -1,19 +1,20 @@
 import AddToCartIcon from "@/icons/AddToCartIcon"
 import Image from "next/image"
+import ProductRating from "./ProductRating"
 
 const ProductCard = ({ product }: { product: Product }) => {
-  const { title, description, price } = product
+  const { title, description, price, rating } = product
 
   const capitalizedTitle = title[0].toUpperCase() + title.slice(1, -1)
 
   return (
-    <article className="bg-slate-100 grid grid-cols-2 auto-rows-min gap-y-2 gap-x-2 relative w-[316px] h-[248px] mx-auto px-4 py-4 shadow-[5px_5px_20px_#b0b2b3,-5px_-5px_20px_#ffffff] rounded-3xl">
-      <p className="col-span-full text-center text-lg font-extrabold">
+    <article className="bg-slate-50 grid grid-cols-2 auto-rows-min gap-y-2 gap-x-2 relative w-[316px] h-[248px] mx-auto px-4 py-4 shadow-[5px_5px_20px_#b0b2b3,-5px_-5px_20px_#ffffff] rounded-3xl">
+      <h2 className="col-span-full text-center text-lg font-extrabold line-clamp-1 overflow-hidden">
         {capitalizedTitle}
-      </p>
+      </h2>
       <div className="w-[120px] h-[120px] relative mx-auto row-start-2">
         <Image
-          className="absolute object-cover rounded-full border place-self-center"
+          className="absolute object-contain rounded-full border place-self-center"
           src={product.images[0]}
           alt={`Image of ${product.title}`}
           fill={true}
@@ -24,7 +25,8 @@ const ProductCard = ({ product }: { product: Product }) => {
       <p className="row-start-2 place-self-center line-clamp-4 overflow-hidden max-h-24">
         {description}
       </p>
-      <p className="my-auto col-start-2 row-start-3">$ {price}</p>
+      <p className="my-4 col-start-2 row-start-3">$ {price}</p>
+      <ProductRating rating={rating} className="place-self-center" />
       <button className="absolute right-4 bottom-4 rounded-full w-12 h-12 flex flex-col place-items-center place-content-center shadow-[3px_3px_3px_#b0b2b3,-3px_-3px_3px_#ffffff] active:border active:shadow-none transition-shadow duration-50">
         <AddToCartIcon />
       </button>
