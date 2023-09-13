e7a152ce970bebb9d6ef2aac56bfd0bfbb362910
Carlos Ochoa <uligibson@hotmail.com>
2023-09-12 18:44:05 -0600

    First steps to add tests

---
+++ pruebas/01-reading-list/CarlosUlisesOchoa/src/test/input.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+import App from '@/src/App'
+
+const mockRender = () => {
+  return render(<App />)
+}
+
+describe('testing renders', () => {
+  it('should render the app', () => {
+    mockRender()
+    const section = screen.getByRole('main')
+    expect(section).toBeInTheDocument()
+  })
+
+  it('should render the header', () => {
+    mockRender()
+    const header = screen.getByRole('main').querySelector('header')
+    expect(header).toBeInTheDocument()
+  })
+
+  it('should render a aside bar', () => {
+    mockRender()
+    const aside = screen.getByRole('main').querySelector('aside')
+    expect(aside).toBeInTheDocument()
+  })
+})
