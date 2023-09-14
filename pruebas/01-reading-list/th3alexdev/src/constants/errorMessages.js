7a8a7831101c7ab5dd07750d21a9272571d74245
AleKs <alexanderfranciscog@gmail.com>
2023-09-14 15:49:10 -0400

    Update asset paths

--- pruebas/01-reading-list/th3alexdev/src/constants/errorMessages.js
+++ pruebas/01-reading-list/th3alexdev/src/constants/errorMessages.js
@@ -1,10 +1,10 @@
 export const ERROR_MESSAGES = Object.freeze({
   NOT_FOUND: {
     message: "No han habido coincidencias con tu búsqueda ;(",
-    asset: "/public/assets/fetch-error-meme.webp"
+    asset: "/assets/fetch-error-meme.webp"
   },
   FETCH_ERROR: {
     message: "Ha habido un problema al cargar el contenido.",
-    asset: "/public/assets/not-found-meme.webp"
+    asset: "/assets/not-found-meme.webp"
   },
 });
