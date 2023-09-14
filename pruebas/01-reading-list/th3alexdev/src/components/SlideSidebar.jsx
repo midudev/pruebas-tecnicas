9381501399df23b9eacbbe469604bf5df0d0833e
AleKs <alexanderfranciscog@gmail.com>
2023-09-14 12:17:18 -0400

    Adjust for responsive design

--- pruebas/01-reading-list/th3alexdev/src/components/SlideSidebar.jsx
+++ pruebas/01-reading-list/th3alexdev/src/components/SlideSidebar.jsx
@@ -4,14 +4,14 @@ import UserReadList from "./UserReadList";
 export default function SlideSidebar({ showSidebar, toggleSidebar }) {
   return (
     <aside
-      className={`bg-sidebar sm:block absolute top-0 sm:left-[99%] 2xl:left-full px-3 py-8 sm:px-4 sm:py-7 sm:w-5/12 lg:w-4/12 sm:max-w-[420px] h-screen sm:transition-transform transition-opacity overflow-auto z-50 ${
+      className={`bg-sidebar w-full 6xs:block absolute top-0 6xs:left-[99%] 2xl:left-full px-3 py-8 6xs:px-4 6xs:py-7 6xs:w-5/12 lg:w-4/12 6xs:max-w-[420px] h-screen 6xs:transition-transform transition-opacity overflow-auto z-50 ${
         showSidebar
-          ? "opacity-100 sm:translate-x-[-100%]"
-          : "opacity-0 sm:translate-x-0"
+          ? "opacity-100 6xs:translate-x-[-100%]"
+          : "opacity-0 6xs:translate-x-0"
       }
       `}
     >
-      <header className="sticky sm:relative flex justify-between mb-4 items-center px-2">
+      <header className="sticky 6xs:relative flex justify-between mb-4 items-center px-2">
         <h1 className="text-lg font-semibold h-max">Lista de Deseos</h1>
         <button onClick={toggleSidebar}>
           <CloseIcon />
