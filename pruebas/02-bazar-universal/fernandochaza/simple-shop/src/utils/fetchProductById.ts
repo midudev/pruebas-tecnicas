27218acdae362f2bb707402197e8d5d9f984a30d
Fernando Chazarreta <fernandochaza@gmail.com>
2023-09-18 09:14:58 -0300

    feat: Add production base url

--- pruebas/02-bazar-universal/fernandochaza/simple-shop/src/utils/fetchProductById.ts
+++ pruebas/02-bazar-universal/fernandochaza/simple-shop/src/utils/fetchProductById.ts
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://www."
+    ? "https://simple-shop-nine.vercel.app/"
     : "http://localhost:3000"
 
 export async function fetchProductById(id: Number) {
