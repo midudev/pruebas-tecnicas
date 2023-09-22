ff4b403b76f024d67b9c334efc279772cbd75cc5
guzmanpdro <pedroguzman2598@gmail.com>
2023-09-22 14:59:53 -0400

    Fix print books function based it in list filtered

--- pruebas/01-reading-list/guzmanpdro/src/js/functions.js
+++ pruebas/01-reading-list/guzmanpdro/src/js/functions.js
@@ -1,44 +1,48 @@
 import { UI } from './classes/UI'
-import { $addedBooks, $booksAvailable } from './selectors'
 import { ReadingList } from './classes/ReadingList'
-import { SECTION_TITLE_ACTION, animateCSS } from './utlis'
+import { $addedBooks, $booksAvailable } from './selectors'
+import { SECTION_TITLE_ACTION, animateCSS, clearHTMl } from './utlis'
 
 const readingList = new ReadingList()
 const ui = new UI()
 
 function printBooks (data) {
-  ui.renderBooks(data)
+  if (data.length !== 0) {
+    if (ui.isOnBooksAvailable) {
+      ui.renderBooks(data)
+    } else {
+      ui.renderBooks(data)
+    }
+  } else {
+    clearHTMl(ui.booksContainer)
+    ui.isOnBooksAvailable
+      ? ui.renderEmptyLibrary('No hay libros', 'No hay libros disponibles')
+      : ui.renderEmptyLibrary('Oh, qué vacío está', 'Agrega algún libro')
+  }
 }
 
 function clickHandler (isbn, isOnBooksAvailable, event) {
-  const elementToAnimate = event.target.parentNode.parentNode
-  const animationName = 'bounceOut'
+  let book
+  let data
 
   if (isOnBooksAvailable) {
-    const book = readingList.booksAvailable.find(elem => elem.ISBN === isbn)
+    book = readingList.booksAvailable.find(elem => elem.ISBN === isbn)
     readingList.addBook(book)
-    $booksAvailable.textContent = `Libros disponibles (${readingList.booksAvailable.length})`
-    $addedBooks.textContent = `Lista de lectura (${readingList.addedBooks.length})`
-    animateCSS(elementToAnimate, animationName)
-    setTimeout(() => {
-      ui.renderBooks(readingList.booksAvailable)
-      if (!readingList.booksAvailable.length) {
-        ui.renderEmptyLibrary('Lo has agregado todo', 'No hay libros disponibles por añadir')
-      }
-    }, 750)
-    return
+    data = filterBooks(readingList.booksAvailable, ui.checkedGenre)
+  } else {
+    book = readingList.addedBooks.find(elem => elem.ISBN === isbn)
+    readingList.removeBook(book)
+    data = filterBooks(readingList.addedBooks, ui.checkedGenre)
   }
 
-  const book = readingList.addedBooks.find(elem => elem.ISBN === isbn)
-  readingList.removeBook(book)
+  const elementToAnimate = event.target.parentNode.parentNode
+  const animationName = 'bounceOut'
   $booksAvailable.textContent = `Libros disponibles (${readingList.booksAvailable.length})`
   $addedBooks.textContent = `Lista de lectura (${readingList.addedBooks.length})`
   animateCSS(elementToAnimate, animationName)
+
   setTimeout(() => {
-    ui.renderBooks(readingList.addedBooks)
-    if (!readingList.addedBooks.length) {
-      ui.renderEmptyLibrary('!Qué vacío está esto!', 'Prueba agregando alguno de los libros disponibles')
-    }
+    printBooks(data, ui.checkedGenre)
   }, 750)
 }
 
@@ -46,25 +50,41 @@ function renderAddedBooks () {
   $booksAvailable.classList.remove(SECTION_TITLE_ACTION.ACTIVE)
   $addedBooks.classList.add(SECTION_TITLE_ACTION.ACTIVE)
   ui.isOnBooksAvailable = false
-  ui.renderBooks(readingList.addedBooks)
-  if (!readingList.addedBooks.length) {
-    ui.renderEmptyLibrary('!Qué vacío está esto!', 'Prueba agregando alguno de los libros disponibles')
-  }
+  printBooks(filterBooks(readingList.addedBooks, ui.checkedGenre))
 }
 
 function backToBooksAvailable () {
   $booksAvailable.classList.add(SECTION_TITLE_ACTION.ACTIVE)
   $addedBooks.classList.remove(SECTION_TITLE_ACTION.ACTIVE)
   ui.isOnBooksAvailable = true
-  ui.renderBooks(readingList.booksAvailable)
-  if (!readingList.booksAvailable.length) {
-    ui.renderEmptyLibrary('Lo has agregado todo', 'No hay libros disponibles por añadir')
+  printBooks(filterBooks(readingList.booksAvailable, ui.checkedGenre))
+}
+
+function filterBooks (list, genre) {
+  let filteredBooks = list
+
+  if (genre) {
+    if (genre === 'Todos') {
+      return filteredBooks
+    }
+    filteredBooks = list.filter(elem => elem.genre === genre)
   }
+
+  return filteredBooks
+}
+
+function onGenreChange (event) {
+  const list = ui.isOnBooksAvailable ? readingList.booksAvailable : readingList.addedBooks
+  ui.checkedGenre = event.target.value
+
+  printBooks(filterBooks(list, ui.checkedGenre))
 }
 
 export {
   printBooks,
   clickHandler,
   renderAddedBooks,
-  backToBooksAvailable
+  backToBooksAvailable,
+  onGenreChange,
+  filterBooks
 }
