2acf330e470e73d0d5e43d16f71f745bc05ab21a
Orloxx23 <madorlix@gmail.com>
2023-09-28 13:49:09 -0500

    Domain Changed

--- pruebas/01-reading-list/orloxx23/src/utils/constants/pageConfig.js
+++ pruebas/01-reading-list/orloxx23/src/utils/constants/pageConfig.js
@@ -6,7 +6,7 @@ const description =
   "Inkfinity es una increíble aplicación web diseñada para todos los amantes de la lectura. Con esta innovadora herramienta, podrás llevar un control detallado de todos los libros que lees y disfrutas. ¡Sumérgete en el fascinante mundo de la lectura con Inkfinity y aprovecha al máximo cada página que leas!";
 const themeColor = "#b08968";
 const author = "Orloxx23";
-const authorLink = "https://orlandomm.me";
+const authorLink = "https://orlandomm.net";
 const keywords =
   "Inkfinity, libros, lectura, leer, control, lista, lista de lectura, lista de libros";
 
