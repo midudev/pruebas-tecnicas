0a0b315999599614fcc61d01519ab45d42bc8c42
Jes015 <jes.18.sin.15@gmail.com>
2023-10-11 16:34:08 -0500

    feat(02-bazar-universal): add Product and SectionLayout components

---
+++ pruebas/02-bazar-universal/Jes015/src/components/CustomImage/CustomImage.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useRef, type LegacyRef } from "react"
+import styles from './customImage.module.css'
+
+interface Props {
+    src: string,
+    width: string,
+    height: string,
+    alt: string
+}
+
+export const CustomImage: React.FC<Props> = ({ src, width, height, alt }) => {
+    const img = useRef<HTMLImageElement>()
+    const imgContainer = useRef<HTMLDivElement>()
+
+    // useEffect by astro compilation
+    useEffect(() => {
+        if (img.current?.complete) {
+            imgContainer.current?.classList.remove(styles["img--loading"])
+        }
+
+        console.log('loading')
+
+        img.current?.addEventListener('load', () => {
+            img.current?.classList.add(styles["img--loaded"])
+            imgContainer.current?.classList.remove(styles["img--loading"])
+        })
+
+    }, [])
+
+    return (
+        <div
+            className={["overflow-hidden rounded-md", width, height].join(' ')}
+            ref={imgContainer as LegacyRef<HTMLDivElement>}
+        >
+            <img
+                className={["w-full h-full object-cover", styles["img--loading"]].join(' ')}
+                ref={img as LegacyRef<HTMLImageElement>}
+                {...{ src, alt }}
+            />
+        </div>
+    )
+}
---
+++ pruebas/02-bazar-universal/Jes015/src/models/product.model.ts
@@ -0,0 +1,13 @@
+export interface Product {
+    id:                 number;
+    title:              string;
+    description:        string;
+    price:              number;
+    discountPercentage: number;
+    rating:             number;
+    stock:              number;
+    brand:              string;
+    category:           string;
+    thumbnail:          string;
+    images:             string[];
+}
