06b31680025c82e9be9ffc9822aa220f5e39847f
Yuri <jdsanchez19@esei.uvigo.es>
2023-10-11 05:13:48 +0200

    Removed template files

--- pruebas/02-bazar-universal/juanyuri/src/views/HomeView.vue
+++ pruebas/02-bazar-universal/juanyuri/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <h3>Found it!</h3>
 </template>
+
+<script setup></script>
