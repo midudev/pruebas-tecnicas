6d1dd6709ea68aef0a5c1f6a8828339f8c76e8a3
Alvaro <alvaromagu2002@gmail.com>
2023-10-11 10:11:34 +0200

    fix strict bool expresion

--- pruebas/02-bazar-universal/aiv4ro/src/constants/env.ts
+++ pruebas/02-bazar-universal/aiv4ro/src/constants/env.ts
@@ -1,2 +1,2 @@
-const vercelUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : null
+const vercelUrl = process.env.VERCEL_URL != null ? `https://${process.env.VERCEL_URL}` : null
 export const hostUrl = vercelUrl ?? 'http://localhost:3000'
