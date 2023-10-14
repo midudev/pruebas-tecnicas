ee12b0f6f09f570074e9d9c7a968c008e97db308
Jes015 <jes.18.sin.15@gmail.com>
2023-10-14 14:25:02 -0500

    fix(02-bazar-universal): add vercel integration

--- pruebas/02-bazar-universal/Jes015/astro.config.mjs
+++ pruebas/02-bazar-universal/Jes015/astro.config.mjs
@@ -1,15 +1,16 @@
-import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
-import { defineConfig } from 'astro/config'
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
 
-import node from "@astrojs/node"
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'hybrid',
-  adapter: node({
-    mode: "standalone"
-  }),
-  build: { format: 'file' }
+  adapter: vercel(),
+  build: {
+    format: 'file'
+  }
 });
