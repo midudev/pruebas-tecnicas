9c3b944b983d7a364890563fcdd355485548c1de
Jes015 <jes.18.sin.15@gmail.com>
2023-10-14 16:41:29 -0500

    feat(02-bazar-universal): add netlify adapter

--- pruebas/02-bazar-universal/Jes015/astro.config.mjs
+++ pruebas/02-bazar-universal/Jes015/astro.config.mjs
@@ -2,13 +2,13 @@ import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from 'astro/config'
 
-import vercel from "@astrojs/vercel/serverless"
+import netlify from "@astrojs/netlify/functions"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel(),
+  output: 'hybrid',
+  adapter: netlify(),
   build: {
     format: 'file'
   }
