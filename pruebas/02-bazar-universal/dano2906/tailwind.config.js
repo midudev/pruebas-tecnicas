a8b5d765fa377471c9c1f66d10667316b2f2eb61
dano2906 <dlg9806@gmail.com>
2023-10-15 12:15:04 +0200

    feat: Main page and items page build

--- pruebas/02-bazar-universal/dano2906/tailwind.config.js
+++ pruebas/02-bazar-universal/dano2906/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'tilt':['TiltNeon','system-ui']
+      }
+    },
   },
   plugins: [],
 }
