2d1e681685d7769c0593ee7bf256221d35facd2c
Dano <dlg9806@gmail.com>
2023-10-16 14:23:43 -0400

    feat: Implement navigation and pagination

--- pruebas/02-bazar-universal/dano2906/src/pages/api/items/data.json.js
+++ pruebas/02-bazar-universal/dano2906/src/pages/api/items/data.json.js
@@ -3,13 +3,16 @@ import { products } from "@utils/products.json";
 export async function GET({ params, request }) {
     const url = new URL(request.url)
     const query = url.searchParams.get('q')
-    const page = url.searchParams.get('p')
+    const page = Number(url.searchParams.get('p'))
 
     const data = products.filter((product)=> product.title.toLowerCase().includes(query.toLowerCase()))
     const paginatedData = data.slice(page*3,page*3+3)
+    const nextPage = page+1;
+    const prevPage = page === 0 ? 0 : page -1;
+    const isLastPage = paginatedData.length < 3 ? true : false
 
     return new Response(
-      JSON.stringify({paginatedData}), {
+      JSON.stringify({paginatedData,nextPage,prevPage,isLastPage}), {
         status: 200,
         headers: {
           "Content-Type": "application/json"
