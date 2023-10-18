ef488d22219992196032858c4f55caf64f7f4cce
dano2906 <dlg9806@gmail.com>
2023-10-18 09:23:21 +0200

    refactor: Fix transition name bugs

--- pruebas/02-bazar-universal/dano2906/src/components/Cart.jsx
+++ pruebas/02-bazar-universal/dano2906/src/components/Cart.jsx
@@ -7,17 +7,19 @@ export default function Cart() {
         setCartItems(cart)
     },[])
   return (
-    <section className="max-w-4xl mx-auto min-h-screen p-2">
-        <ul className="grid col-span-1 sm:col-span-2 md:col-span-3 gap-2">
+    <section className="max-w-4xl min-h-screen p-2 mx-auto">
+        <ul className="grid col-span-1 gap-2 sm:col-span-2 md:col-span-3">
             {
-                cartItems.map(({title,description,category,price,rating},index) => (
-                    <article key={index} class="w-full h-auto p-3 flex flex-col items-start justify-start gap-y-1 bg-gradient-to-br from-blue-300 via-pink-300 to-orange-300 shadow-lg rounded-lg">
-                            <div class="flex flex-col items-start justify-start">
-                                <h2 class="text-2xl font-tilt font-bold text-purple-950" transition:name="title">{title}</h2>
-                                <p class="text-base font-tilt font-light text-purple-950 [text-wrap:balance]" transition:name="desc">{description}</p>
-                                <span class="text-sm font-tilt font-normal text-purple-950" transition:name="category">{category}</span>
-                                <span class="text-sm font-tilt font-light text-purple-950" transition:name="price">Price: ${price}</span>
-                                <span class="text-sm font-tilt font-light text-purple-950" transition:name="rating">Rating: {rating}/5</span>
+                cartItems.map(({id,title,description,category,price,rating},index) => (
+                    <article key={index} className="flex flex-col items-start justify-start w-full h-auto p-3 rounded-lg shadow-lg gap-y-1 bg-gradient-to-br from-blue-300 via-pink-300 to-orange-300">
+                        <img src="/placeholder-product-image.png" alt="Imagen del producto" width={128} height={128} transition:name={`image--${id}`}/>
+                            <div className="flex flex-col items-start justify-start">
+                                <h2 className="text-2xl font-bold font-tilt text-purple-950" transition:name={`title--${id}`}>{title}</h2>
+                                <p className="text-base font-tilt font-light text-purple-950 [text-wrap:balance]" transition:name={`desc--${id}`}>{description}</p>
+                                <span className="text-sm font-normal font-tilt text-purple-950" transition:name={`category--${id}`}>{category}</span>
+                                <span className="text-sm font-light font-tilt text-purple-950" transition:name={`price--${id}`}>Price: ${price}</span>
+                                <span className="text-sm font-light font-tilt text-purple-950" transition:name={`rating--${id}`}>Rating: {rating}/5</span>
+                                <a href={`/items/${id}`} className="p-1 my-2 text-sm font-light rounded-lg md:text-lg font-tilt text-purple-950 bg-gradient-to-br from-blue-200 via-pink-200 to-orange-200 ">Detalles</a>
                             </div>     
                     </article>
                 ))
